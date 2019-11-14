f3f9bdccb660c8b00dd841fd2aef98e88cc015a4
Fabrice V <fabrice@mobilitydata.org>
2019-11-14 16:54:19 -0500

    Conversion from CSV to protobuf binary
    -add general class with specialized convert methods
      -take their path as parameters
    -use Jackson library to retrieve CSV content
    
    -remove specific conversion classes

--- src/main/java/org/mobilitydata/Main.java
+++ src/main/java/org/mobilitydata/Main.java
@@ -4,16 +4,17 @@ package org.mobilitydata;
  * Copyright (c) 2019. MobilityData IO. All rights reserved
  */
 
-import org.mobilitydata.conversion.PathwaysConverter;
-import org.mobilitydata.conversion.StopTimesConverter;
-import org.mobilitydata.conversion.StopsConverter;
-import org.mobilitydata.conversion.TripsConverter;
+import org.mobilitydata.conversion.*;
+import org.mobilitydata.gtfsproto.PathwaysProto;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Comparator;
 
 public class Main {
 
@@ -22,10 +23,11 @@ public class Main {
         long timeBeforeMillis = System.currentTimeMillis();
 
         // download data source from URL and unzip
-        //TODO: configurable through command line options: url, zip path, extraction path
+        //TODO: configurable through command line options: url, zip path, extraction path, output path
         String url = "https://transitfeeds.com/p/mbta/64/latest/download";
         String zipInputPath = "input.zip";
         String zipExtractTargetPath = "input";
+        String outputPath = "output";
 
         try {
             Files.copy(
@@ -40,11 +42,28 @@ public class Main {
             e.printStackTrace();
         }
 
-        // convert GTFS text files to .proto files on disk
-        StopsConverter.convert();
-        PathwaysConverter.convert();
-        TripsConverter.convert();
-        StopTimesConverter.convert();
+        CSVtoProtoConverter pathwaysConverter = new CSVtoProtoConverter();
+
+        try {
+
+            Path out = Path.of(outputPath);
+
+            // to empty any already existing directory
+            if(Files.exists(out)){
+                //noinspection ResultOfMethodCallIgnored
+                Files.walk(out).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+                Files.createDirectory(out);
+            } else {
+                Files.createDirectory(out);
+            }
+
+            // convert GTFS text files to .proto files on disk
+            pathwaysConverter.convert("input/pathways.txt",
+                    "output/pathways.pb",
+                    PathwaysProto.pathwayCollection.newBuilder());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Took " + (System.currentTimeMillis() - timeBeforeMillis) + "ms");
     }
---
+++ src/main/java/org/mobilitydata/conversion/CSVtoProtoConverter.java
@@ -0,0 +1,154 @@
+package org.mobilitydata.conversion;
+
+/*
+ * Copyright (c) 2019. MobilityData IO. All rights reserved
+ */
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.mobilitydata.gtfsproto.PathwaysProto;
+import org.mobilitydata.gtfsproto.StopTimesProto;
+import org.mobilitydata.gtfsproto.StopsProto;
+import org.mobilitydata.gtfsproto.TripsProto;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+public class CSVtoProtoConverter {
+
+    public void convert(String csvInFilePath, String protoBinOutFilePath,
+                        PathwaysProto.pathwayCollection.Builder protoCollectionBuilder)
+            throws IOException {
+
+        var it = getRows(csvInFilePath);
+
+        while (it.hasNext()){
+            Map<String, String> rowAsMap = it.next();
+
+            PathwaysProto.Pathway.Builder toAddBuilder = PathwaysProto.Pathway.newBuilder();
+
+            toAddBuilder.setPathwayId(rowAsMap.get("pathway_id"));
+            toAddBuilder.setFromStopId(rowAsMap.get("from_stop_id"));
+            toAddBuilder.setToStopId(rowAsMap.get("to_stop_id"));
+
+            toAddBuilder.setIsBidirectionalValue(Integer.parseInt(rowAsMap.get("is_bidirectional")));
+
+            try {
+                //TODO: mandatory but mtba dataset don't provide them
+                toAddBuilder.setPathwayModeValue(Integer.parseInt(rowAsMap.get("pathway_mode")));
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                //e.printStackTrace();
+            }
+
+            try{
+                toAddBuilder.setLength(Float.parseFloat(rowAsMap.get("length")));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            try{
+                toAddBuilder.setTraversalTime(Integer.parseInt(rowAsMap.get("traversal_time")));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            try{
+                toAddBuilder.setStairCount(Integer.parseInt(rowAsMap.get("stair_count")));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            try{
+                toAddBuilder.setMaxSlope(Float.parseFloat(rowAsMap.get("max_slope")));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            try{
+                toAddBuilder.setMinWidth(Float.parseFloat(rowAsMap.get("min_width")));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            try{
+                toAddBuilder.setSignpostedAs(rowAsMap.get("signposted_as"));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            try{
+                toAddBuilder.setReversedSignpostedAs(rowAsMap.get("reversed_signposted_as"));
+
+            } catch (NumberFormatException e) {
+                //TODO: collect errors
+            }
+            catch (NullPointerException e){
+                //not an error - optional value not provided
+            }
+
+            protoCollectionBuilder.addPathways(toAddBuilder);
+        }
+
+        // Write the pathway collection to disk.
+        FileOutputStream output = new FileOutputStream(protoBinOutFilePath);
+        protoCollectionBuilder.build().writeTo(output);
+        output.close();
+    }
+
+    public void convert(String csvInFilePath, String protoBinOutFilePath,
+                        StopTimesProto.stopTimeCollection.Builder protoCollectionBuilder)
+            throws IOException {
+        //TODO
+    }
+
+    public void convert(String csvInFilePath, String protoBinOutFilePath,
+                        StopsProto.stopCollection.Builder protoCollectionBuilder)
+            throws IOException {
+        //TODO
+    }
+
+    public void convert(String csvInFilePath, String protoBinOutFilePath,
+                        TripsProto.tripCollection.Builder protoCollectionBuilder)
+            throws IOException {
+        //TODO
+    }
+
+    private MappingIterator<Map<String, String>> getRows(String csvInFilePath) throws IOException {
+        //use jackson to open the file and see what API we have
+        File csvFile = new File(csvInFilePath);
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = CsvSchema.emptySchema().withHeader();
+
+        return mapper.readerFor(Map.class)
+                .with(schema)
+                .readValues(csvFile);
+
+    }
+}
