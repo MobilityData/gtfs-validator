6f3f4246f1a86892cc033176158dd9ecb8ba62c0
fabrice-v <55884852+fabrice-v@users.noreply.github.com>
2019-12-05 15:43:42 -0500

    Stops.txt type validation protobuf conversion (#26)
    * GTFS types validation. Using Apache commons
    
    -for Float and Integer types. Including range validation.
    -refactor String validation to expose only GTFS types to client code (id and text)
    
    * Stops.txt -- GTFS types validation + protobuf conversion
    
    * GTFS types validation. time
    
    -add validation code
      -specified format HH:MM:SS or H:MM:SS
    -add unit testing of method
    
    * GTFS types validation. time
    
    -add test to check for HHH:MM:SS rejection
    
    * GTFS types validation. time
    
    -required changes after merging PR #19 to master
    
    * Stops.txt -- GTFS types validation + protobuf conversion
    
    -required changes after merging PR #19

--- src/main/java/org/mobilitydata/gtfsvalidator/conversion/CSVtoProtoConverter.java
+++ src/main/java/org/mobilitydata/gtfsvalidator/conversion/CSVtoProtoConverter.java
@@ -19,6 +19,7 @@ package org.mobilitydata.gtfsvalidator.conversion;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.type.LatLng;
 import org.mobilitydata.gtfsvalidator.model.OccurrenceModel;
 import org.mobilitydata.gtfsvalidator.proto.PathwaysProto;
 import org.mobilitydata.gtfsvalidator.proto.StopTimesProto;
@@ -47,7 +48,7 @@ public class CSVtoProtoConverter {
         try {
             rawEntities = getRows(csvInFilePath);
         } catch (IOException e) {
-            RuleUtils.addOccurrence(E001, "File Pathways.txt", errorAndWarningList);
+            RuleUtils.addOccurrence(E001, "File pathways.txt", errorAndWarningList);
             return errorAndWarningList;
         }
 
@@ -63,11 +64,10 @@ public class CSVtoProtoConverter {
 
             if (pathwayId != null) {
                 toAddBuilder.setPathwayId(pathwayId);
+                pathwayId = "pathway_id: " + pathwayId;
             } else {
                 pathwayId = "pathway_id: null";
             }
-
-            pathwayId = "pathway_id: " + pathwayId;
 
             String fromStopId = GTFSTypeValidationUtils.validateId("from_stop_id",
                     rawEntity.get("from_stop_id"),
@@ -187,16 +187,204 @@ public class CSVtoProtoConverter {
         return errorAndWarningList;
     }
 
-    public void convert(String csvInFilePath, String protoBinOutFilePath,
+    public List<OccurrenceModel> convert(String csvInFilePath, String protoBinOutFilePath,
                         StopTimesProto.stopTimeCollection.Builder protoCollectionBuilder)
             throws IOException {
-        //TODO
+
+        List<OccurrenceModel> errorAndWarningList = new ArrayList<>();
+
+        MappingIterator<Map<String, String>> rawEntities;
+
+        try {
+            rawEntities = getRows(csvInFilePath);
+        } catch (IOException e) {
+            RuleUtils.addOccurrence(E001, "File stop_times.txt", errorAndWarningList);
+            return errorAndWarningList;
+        }
+
+
+        while (rawEntities.hasNext()) {
+            Map<String, String> rawEntity = rawEntities.next();
+
+            StopTimesProto.StopTime.Builder toAddBuilder = StopTimesProto.StopTime.newBuilder();
+
+
+            protoCollectionBuilder.addStopTimes(toAddBuilder);
+        }
+
+        // Write the stop collection to disk.
+        FileOutputStream output = new FileOutputStream(protoBinOutFilePath);
+        protoCollectionBuilder.build().writeTo(output);
+        output.close();
+
+        return errorAndWarningList;
     }
 
-    public void convert(String csvInFilePath, String protoBinOutFilePath,
+    public List<OccurrenceModel> convert(String csvInFilePath, String protoBinOutFilePath,
                         StopsProto.stopCollection.Builder protoCollectionBuilder)
             throws IOException {
-        //TODO
+        List<OccurrenceModel> errorAndWarningList = new ArrayList<>();
+
+        MappingIterator<Map<String, String>> rawEntities;
+
+        try {
+            rawEntities = getRows(csvInFilePath);
+        } catch (IOException e) {
+            RuleUtils.addOccurrence(E001, "File stops.txt", errorAndWarningList);
+            return errorAndWarningList;
+        }
+
+
+        while (rawEntities.hasNext()) {
+            Map<String, String> rawEntity = rawEntities.next();
+
+            StopsProto.Stop.Builder toAddBuilder = StopsProto.Stop.newBuilder();
+
+            String stopId = GTFSTypeValidationUtils.validateId("stop_id",
+                    rawEntity.get("stop_id"),
+                    false,
+                    errorAndWarningList);
+
+            if (stopId != null) {
+                toAddBuilder.setId(stopId);
+                stopId = "stop_id: " + stopId;
+            } else {
+                stopId = "stop_id: null";
+            }
+
+            String stopCode = GTFSTypeValidationUtils.validateText(stopId,
+                    "stop_code",
+                    rawEntity.get("stop_code"),
+                    true,
+                    errorAndWarningList);
+
+            if (stopCode != null) {
+                toAddBuilder.setCode(stopCode);
+            }
+
+            String stopName = GTFSTypeValidationUtils.validateText(stopId,
+                    "stop_name",
+                    rawEntity.get("stop_name"),
+                    true,
+                    errorAndWarningList);
+
+            if (stopName != null) {
+                toAddBuilder.setName(stopName);
+            }
+
+            String stopDesc = GTFSTypeValidationUtils.validateText(stopId,
+                    "stop_desc",
+                    rawEntity.get("stop_desc"),
+                    true,
+                    errorAndWarningList);
+
+            if (stopDesc != null) {
+                toAddBuilder.setDesc(stopDesc);
+            }
+
+            Float stopLatitude = GTFSTypeValidationUtils.parseAndValidateLatitude(stopId,
+                    "stop_lat",
+                    rawEntity.get("stop_lat"),
+                    true,
+                    errorAndWarningList);
+
+            Float stopLongitude = GTFSTypeValidationUtils.parseAndValidateLongitude(stopId,
+                    "stop_lon",
+                    rawEntity.get("stop_lon"),
+                    true,
+                    errorAndWarningList);
+
+            if (stopLatitude != null && stopLongitude != null) {
+                toAddBuilder.setLatLng(LatLng.newBuilder().setLatitude(stopLatitude).setLongitude(stopLongitude).build());
+            }
+
+            String zoneId = GTFSTypeValidationUtils.validateId("zone_id",
+                    rawEntity.get("zone_id"),
+                    true,
+                    errorAndWarningList);
+
+            if (zoneId != null) {
+                toAddBuilder.setZoneId(zoneId);
+            }
+
+            String stopUrl = GTFSTypeValidationUtils.validateUrl(stopId,
+                    "stop_url",
+                    rawEntity.get("stop_url"),
+                    true,
+                    errorAndWarningList);
+
+            if (stopUrl != null) {
+                toAddBuilder.setUrl(stopUrl);
+            }
+
+            Integer locationType = GTFSTypeValidationUtils.parseAndValidateInteger(stopId,
+                    "location_type",
+                    rawEntity.get("location_type"),
+                    true,
+                    false,
+                    errorAndWarningList);
+
+            if (locationType != null) {
+                toAddBuilder.setLocationTypeValue(locationType);
+            }
+
+            String parentStationId = GTFSTypeValidationUtils.validateId("parent_station",
+                    rawEntity.get("parent_station"),
+                    true,
+                    errorAndWarningList);
+
+            if (parentStationId != null) {
+                toAddBuilder.setParentStation(parentStationId);
+            }
+
+            String timezone = GTFSTypeValidationUtils.parseAndValidateTimeZone(stopId,
+                    "stop_timezone",
+                    rawEntity.get("stop_timezone"),
+                    errorAndWarningList);
+
+            if (timezone != null) {
+                toAddBuilder.setTimezone(timezone);
+            }
+
+            Integer wheelchairBoarding = GTFSTypeValidationUtils.parseAndValidateInteger(stopId,
+                    "wheelchair_boarding",
+                    rawEntity.get("wheelchair_boarding"),
+                    true,
+                    false,
+                    errorAndWarningList);
+
+            if (wheelchairBoarding != null) {
+                toAddBuilder.setWheelchairBoardingValue(wheelchairBoarding);
+            }
+
+            String levelId = GTFSTypeValidationUtils.validateId("level_id",
+                    rawEntity.get("level_id"),
+                    true,
+                    errorAndWarningList);
+
+            if (levelId != null) {
+                toAddBuilder.setLevelId(levelId);
+            }
+
+            String platformCode = GTFSTypeValidationUtils.validateText(stopId,
+                    "platform_code",
+                    rawEntity.get("platform_code"),
+                    true,
+                    errorAndWarningList);
+
+            if (platformCode != null) {
+                toAddBuilder.setPlatformCode(platformCode);
+            }
+
+            protoCollectionBuilder.addStops(toAddBuilder);
+        }
+
+        // Write the stop collection to disk.
+        FileOutputStream output = new FileOutputStream(protoBinOutFilePath);
+        protoCollectionBuilder.build().writeTo(output);
+        output.close();
+
+        return errorAndWarningList;
     }
 
     public void convert(String csvInFilePath, String protoBinOutFilePath,
