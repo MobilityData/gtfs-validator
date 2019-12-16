11503ba4e41c8e169031a91068fe2077f5ffbaac
Lionel N <lionel@mobilitydata.org>
2019-12-16 14:11:40 -0500

    Fix for #28 - Improve command line arguments parsing
    Provide 'help'command to describe available options

--- src/main/java/org/mobilitydata/gtfsvalidator/Main.java
+++ src/main/java/org/mobilitydata/gtfsvalidator/Main.java
@@ -19,9 +19,9 @@ package org.mobilitydata.gtfsvalidator;
 import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.mobilitydata.gtfsvalidator.conversion.CSVtoProtoConverter;
 import org.mobilitydata.gtfsvalidator.model.OccurrenceModel;
 import org.mobilitydata.gtfsvalidator.proto.PathwaysProto;
-import org.mobilitydata.gtfsvalidator.conversion.CSVtoProtoConverter;
 import org.mobilitydata.gtfsvalidator.proto.StopsProto;
 import org.mobilitydata.gtfsvalidator.util.FileUtils;
 
@@ -38,16 +38,24 @@ public class Main {
         final Options options = new Options();
 
         options.addOption("u", "url", true, "URL to GTFS zipped archive");
-        options.addRequiredOption("z", "zip", true, "if -url is used, where to place the downloaded archive," +
+        options.addOption("z", "zip", true, "if -url is used, where to place the downloaded archive," +
                 "otherwise must point to a valid GTFS zipped archive on disk");
-        options.addRequiredOption("i", "input", true, "Relative path where to extract the zip content");
-        options.addRequiredOption("o", "output", true, "Relative path where to place output files");
+        options.addOption("i", "input", true, "Relative path where to extract the zip content");
+        options.addOption("o", "output", true, "Relative path where to place output files");
+        options.addOption("h", "help", false, "Print this message");
+
         //TODO: add configurable warning threshold for GTFS time type validation
 
         final CommandLineParser parser = new DefaultParser();
 
+
         try {
             final CommandLine cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("h")) {
+                printHelp(options);
+                return;
+            }
 
             String url = cmd.getOptionValue("u");
             String zipInputPath = cmd.getOptionValue("z");
@@ -77,6 +85,7 @@ public class Main {
             logger.debug("validation result: " + result);
 
         } catch (ParseException e) {
+
             logger.error("Could not parse command line arguments: " + e.getMessage());
         } catch (IOException e) {
             logger.error("An exception occurred: " + e.getMessage());
@@ -84,4 +93,15 @@ public class Main {
 
         logger.info("Took " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime) + "ms");
     }
+
+    private static void printHelp(Options options) {
+        final String HELP = String.join("\n",
+                "Loads input GTFS feed from url or disk.",
+                "Checks files integrity, and converts CSV to proto file on disk");
+        HelpFormatter formatter = new HelpFormatter();
+        System.out.println(); // blank line for legibility
+        formatter.printHelp(HELP, options);
+        System.out.println(); // blank line for legibility
+    }
+
 }
