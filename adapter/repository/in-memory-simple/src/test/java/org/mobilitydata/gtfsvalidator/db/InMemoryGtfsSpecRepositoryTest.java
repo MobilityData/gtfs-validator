c2be5355c59569a707b7518e5380e85fab893d92
Fabrice V <fabrice@mobilitydata.org>
2020-02-14 15:20:58 -0500

    InMemoryGtfsSpecRepository -- tests
    -add test class

---
+++ adapter/repository/in-memory-simple/src/test/java/org/mobilitydata/gtfsvalidator/db/InMemoryGtfsSpecRepositoryTest.java
@@ -0,0 +1,12 @@
+package org.mobilitydata.gtfsvalidator.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryGtfsSpecRepositoryTest {
+
+    @Test
+    void getRequiredFilenameList() {
+    }
+}
