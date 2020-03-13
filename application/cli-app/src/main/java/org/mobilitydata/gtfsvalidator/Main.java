de2fc0d7c23c1834a40f4889998c1b82e4368495
Fabrice V <fabrice@mobilitydata.org>
2020-03-13 15:37:27 -0400

    Basic validation - exclude frequencies.txt
    -contain unsupported date type fields

--- application/cli-app/src/main/java/org/mobilitydata/gtfsvalidator/Main.java
+++ application/cli-app/src/main/java/org/mobilitydata/gtfsvalidator/Main.java
@@ -122,6 +122,7 @@ public class Main {
             filenameList.remove("calendar.txt");
             filenameList.remove("calendar_dates.txt");
             filenameList.remove("stop_times.txt");
+            filenameList.remove("frequencies.txt");
 
             // base validation
             filenameList.forEach(filename -> {
