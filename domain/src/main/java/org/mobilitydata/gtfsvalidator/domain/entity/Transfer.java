a3ca7c6a12c8b42f683b0e087ee65c612cda9aef
Lionel N <lionel@mobilitydata.org>
2020-03-19 15:46:51 -0400

    Shape entities -- implement new class Shape
    - implement new class Transfer representing a transfer defined as detailed by https://gtfs.org/reference/static#transferstxt
    - implement getters
    - implement builder pattern for immutable object construction
    - add licence header
    - modify gradle.build to allow use of annotations

---
+++ domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/Transfer.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2020. MobilityData IO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator.domain.entity;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Transfer {
+
+    @NotNull
+    private final String fromStopId;
+
+    @NotNull
+    private final String toStopId;
+
+    @NotNull
+    private final TransferType transferType;
+
+    private final int minTransferTime;
+
+    public Transfer(@NotNull String fromStopId,
+                    @NotNull String toStopId,
+                    @NotNull TransferType transferType,
+                    int minTransferTime) {
+        this.fromStopId = fromStopId;
+        this.toStopId = toStopId;
+        this.transferType = transferType;
+        this.minTransferTime = minTransferTime;
+    }
+
+    @NotNull
+    public String getFromStopId() {
+        return fromStopId;
+    }
+
+    @NotNull
+    public String getToStopId() {
+        return toStopId;
+    }
+
+    @NotNull
+    public TransferType getTransferType() {
+        return transferType;
+    }
+
+    public int getMinTransferTime() {
+        return minTransferTime;
+    }
+
+    public static class TransferBuilder {
+        private String fromStopId;
+        private String toStopId;
+        private TransferType transferType;
+        private Integer minTransferTime;
+
+        public TransferBuilder(@NotNull String fromStopId,
+                               @NotNull String toStopId,
+                               @NotNull TransferType transferType) {
+            this.fromStopId = fromStopId;
+            this.toStopId = toStopId;
+            this.transferType = transferType;
+        }
+
+        public TransferBuilder minTransferTime(@Nullable Integer minTransferTime) {
+            this.minTransferTime = minTransferTime;
+            return this;
+        }
+
+        public Transfer build() {
+            return new Transfer(fromStopId, toStopId, transferType, minTransferTime);
+        }
+    }
+}
