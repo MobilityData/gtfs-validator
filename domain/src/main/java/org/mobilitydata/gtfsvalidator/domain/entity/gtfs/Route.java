199ad982c3f85d6af0a9bb730ddfc9eb91f2e479
Lionel N <lionel@mobilitydata.org>
2020-04-02 12:11:54 -0400

    route entity -- move entity to org.mobilitydata.gtfsvalidator.domain.entity.gtfs package

--- domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/Route.java
+++ domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/Route.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.mobilitydata.gtfsvalidator.domain.entity;
+package org.mobilitydata.gtfsvalidator.domain.entity.gtfs;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
