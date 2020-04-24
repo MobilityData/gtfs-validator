a23db8672a31dc44e9819c7c7f83e69b2dab38ae
Lionel N <lionel@mobilitydata.org>
2020-04-24 11:34:51 -0400

    GtfsDataRepository -- add javadoc comments to class InMemoryGtfsDataRepository

--- adapter/repository/in-memory-simple/src/main/java/org/mobilitydata/gtfsvalidator/db/InMemoryGtfsDataRepository.java
+++ adapter/repository/in-memory-simple/src/main/java/org/mobilitydata/gtfsvalidator/db/InMemoryGtfsDataRepository.java
@@ -24,9 +24,22 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This holds an internal representation of gtfs entities: each row of each file from a GTFS dataset is represented here
+ */
 public class InMemoryGtfsDataRepository implements GtfsDataRepository {
     private final Map<String, Agency> agencyCollection = new HashMap<>();
 
+    /**
+     * Add an Agency representing a row from agency.txt to this. Return the entity added to the repository. This method
+     * throws SQLIntegrityConstraintViolationException if the uniqueness constraint of agency based on
+     * agency_id is not respected.
+     *
+     * @param newAgency the internal representation of a row from agency.txt to be added to the repository.
+     * @return the entity added to the repository
+     * @throws SQLIntegrityConstraintViolationException if the uniqueness constraint of agency based on agency_id is not
+     *                                                  respected.
+     */
     @Override
     public Agency addAgency(final Agency newAgency) throws SQLIntegrityConstraintViolationException {
         final String agencyId = newAgency.getAgencyId();
@@ -38,6 +51,12 @@ public class InMemoryGtfsDataRepository implements GtfsDataRepository {
         }
     }
 
+    /**
+     * Return the Agency representing a row from agency.txt related to the id provided as parameter
+     *
+     * @param agencyId the key from agency.txt related to the Agency to be returned
+     * @return the Agency representing a row from agency.txt related to the id provided as parameter
+     */
     @Override
     public Agency getAgencyById(final String agencyId) {
         return agencyCollection.get(agencyId);
@@ -45,11 +64,27 @@ public class InMemoryGtfsDataRepository implements GtfsDataRepository {
 
     private final Map<String, Route> routeCollection = new HashMap<>();
 
+    /**
+     * Return the Routes representing a row from routes.txt related to the id provided as parameter
+     *
+     * @param routeId the key from routes.txt related to the Route to be returned
+     * @return the Agency representing a row from routes.txt related to the id provided as parameter
+     */
     @Override
     public Route getRouteById(final String routeId) {
         return routeCollection.get(routeId);
     }
 
+    /**
+     * Add an Route representing a row from routes.txt to this. Return the entity added to the repository. This method
+     * throws SQLIntegrityConstraintViolationException if the uniqueness constraint of route based on
+     * routeId is not respected.
+     *
+     * @param newRoute the internal representation of a row from routes.txt to be added to the repository.
+     * @return the entity added to the repository
+     * @throws SQLIntegrityConstraintViolationException if the uniqueness constraint on table routes based on routeId
+     *                                                  is not respected.
+     */
     @Override
     public Route addRoute(final Route newRoute) throws SQLIntegrityConstraintViolationException {
         if (routeCollection.containsKey(newRoute.getRouteId())) {
