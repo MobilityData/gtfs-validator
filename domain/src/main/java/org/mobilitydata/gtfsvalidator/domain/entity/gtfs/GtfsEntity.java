71c433dc4225d7c10008394c4ad44c5ddc8cbfae
Lionel N <lionel@mobilitydata.org>
2020-05-01 17:50:28 -0400

    Gtfs entities -- Implement superclass GtfsEntity to ease code maintenance
    - make Agency and Route entities inherit from class GtfsEntity
    - implement generic constructor for EntityBuildResult

---
+++ domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/GtfsEntity.java
@@ -0,0 +1,8 @@
+package org.mobilitydata.gtfsvalidator.domain.entity.gtfs;
+
+/**
+ * This is the super class from which all gtfs entities inherit. At present, this class is empty. We will add fields
+ * and methods according to the progress of the project.
+ */
+public abstract class GtfsEntity {
+}
