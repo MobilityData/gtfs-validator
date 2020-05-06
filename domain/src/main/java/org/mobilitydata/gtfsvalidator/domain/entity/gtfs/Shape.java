1234cf38abd7dbb425354cf865968677e602de14
Lionel N <lionel@mobilitydata.org>
2020-05-06 16:58:06 -0400

    Shape entities -- rework entity
    - make noticeCollection a field of entity builder for future null check
    - rework null check in entity builder build method to generate notices
    - adapt tests
    - add javadoc

--- domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/Shape.java
+++ domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/Shape.java
@@ -18,18 +18,37 @@ package org.mobilitydata.gtfsvalidator.domain.entity.gtfs;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.base.Notice;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.FloatFieldValueOutOfRangeNotice;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.IntegerFieldValueOutOfRangeNotice;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.MissingRequiredValueNotice;
 
-public class Shape implements Comparable<Shape> {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Class for all entities defined in shapes.txt. Can not be directly instantiated: user must use the
+ * {@link Shape.ShapeBuilder} to create this.
+ */
+public class Shape extends GtfsEntity implements Comparable<Shape> {
     @NotNull
     private final String shapeId;
     private final float shapePtLat;
     private final float shapePtLon;
     private final int shapePtSequence;
-
     @Nullable
     private final Float shapeDistTraveled;
 
+    /**
+     * Class for all entities defined in shapes.txt
+     *
+     * @param shapeId           identifies a shape
+     * @param shapePtLat        latitude of a shape point
+     * @param shapePtLon        longitude of a shape point
+     * @param shapePtSequence   sequence in which the shape points connect to form the shape
+     * @param shapeDistTraveled actual distance traveled along the shape from the first shape point to the point
+     *                          specified in this record
+     */
     private Shape(@NotNull final String shapeId,
                   float shapePtLat,
                   float shapePtLon,
@@ -65,70 +84,153 @@ public class Shape implements Comparable<Shape> {
         return shapeDistTraveled;
     }
 
+    /**
+     * Implement compareTo method from {@code Comparable}. Return the value {@code 0} if this {@code Shape} field
+     * shape_pt_sequence is equal to field shape_pt_sequence of argument; a value less than {@code 0} if this field
+     * shape_pt_sequence is numerically less than field shape_pt_sequence of argument; and a value greater
+     * than {@code 0} if this field shape_pt_sequence is numerically greater than field shape_pt_sequence of argument
+     *
+     * @param shape shape to compare to
+     * @return the value {@code 0} if this {@code Shape} field shape_pt_sequence is equal to field shape_pt_sequence of
+     * argument; a value less than {@code 0} if this field shape_pt_sequence is numerically less than field
+     * shape_pt_sequence of argument; and a value greater than {@code 0} if this field shape_pt_sequence is numerically
+     * greater than field shape_pt_sequence of argument
+     */
     @Override
     public int compareTo(@NotNull final Shape shape) {
         return getShapePtSequence().compareTo(shape.getShapePtSequence());
     }
 
+    /**
+     * Return true if this {@link Shape} has field shape_pt_sequence greater than {@param otherShape}
+     * field shape_pt_sequence; otherwise return false
+     *
+     * @param otherShape shape to compare
+     * @return true if this {@link Shape} has field shape_pt_sequence greater than {@param otherShape}
+     * field shape_pt_sequence; otherwise return false
+     */
     public boolean isGreaterThan(final Shape otherShape) {
         return compareTo(otherShape) > 0;
     }
 
+    /**
+     * Builder class to create {@link Shape} objects. Allows an unordered definition of the different attributes of
+     * {@link Shape}.
+     */
     public static class ShapeBuilder {
         private String shapeId;
-        private float shapePtLat;
-        private float shapePtLon;
-        private int shapePtSequence;
-
+        private Float shapePtLat;
+        private Float shapePtLon;
+        private Integer shapePtSequence;
+        private final List<Notice> noticeCollection = new ArrayList<>();
         @Nullable
         private Float shapeDistTraveled;
 
+        /**
+         * Sets field shapeId value and returns this
+         *
+         * @param shapeId identifies a shape
+         * @return builder for future object creation
+         */
         public ShapeBuilder shapeId(@NotNull final String shapeId) {
             this.shapeId = shapeId;
             return this;
         }
 
-        public ShapeBuilder shapePtLat(final float shapePtLat) {
+        /**
+         * Sets field shapePtLat value and returns this
+         *
+         * @param shapePtLat latitude of a shape point
+         * @return builder for future object creation
+         */
+        public ShapeBuilder shapePtLat(@NotNull final Float shapePtLat) {
             this.shapePtLat = shapePtLat;
             return this;
         }
 
-        public ShapeBuilder shapePtLon(final float shapePtLon) {
+        /**
+         * Sets field shapePtLon value and returns this
+         *
+         * @param shapePtLon longitude of a shape point
+         * @return builder for future object creation
+         */
+        public ShapeBuilder shapePtLon(@NotNull final Float shapePtLon) {
             this.shapePtLon = shapePtLon;
             return this;
         }
 
-        public ShapeBuilder shapePtSequence(final int shapePtSequence) {
+        /**
+         * Sets field shapePtSequence value and returns this
+         *
+         * @param shapePtSequence sequence in which the shape points connect to form the shape
+         * @return builder for future object creation
+         */
+        public ShapeBuilder shapePtSequence(@NotNull final Integer shapePtSequence) {
             this.shapePtSequence = shapePtSequence;
             return this;
         }
 
+        /**
+         * Sets field shapeDistTraveled value and returns this
+         *
+         * @param shapeDistTraveled actual distance traveled along the shape from the first shape point to the point
+         *                          specified in this record
+         * @return builder for future object creation
+         */
         public ShapeBuilder shapeDistTraveled(@Nullable final Float shapeDistTraveled) {
             this.shapeDistTraveled = shapeDistTraveled;
             return this;
         }
 
-        public Shape build() {
-            if (shapeId == null) {
-                throw new IllegalArgumentException("field shape_id can not be null in file shapes.txt");
-            }
+        /**
+         * Entity representing a row from shapes.txt if the requirements from the official GTFS specification
+         * are met. Otherwise, method returns an entity representing a list of notices.
+         *
+         * @return Entity representing a row from shapes.txt if the requirements from the official GTFS specification
+         * are met. Otherwise, method returns an entity representing a list of notices.
+         */
+        public EntityBuildResult<?> build() {
+            noticeCollection.clear();
 
-            if (shapePtLat < -90.0f || shapePtLat > 90.0f) {
-                throw new IllegalArgumentException("invalid value for field shape_latitude");
+            //noinspection ConstantConditions to avoid lint
+            if (shapeId == null ||
+                    shapePtLat == null || (shapePtLat != null && (shapePtLat < -90.0f || shapePtLat > 90.0f)) ||
+                    shapePtLon == null || (shapePtLon != null && (shapePtLon < -180.0f || shapePtLon > 180.0f)) ||
+                    shapePtSequence == null || (shapePtSequence != null && shapePtSequence < 0) ||
+                    (shapeDistTraveled != null && shapeDistTraveled < 0)) {
+                if (shapeId == null) {
+                    noticeCollection.add(new MissingRequiredValueNotice("shapes.txt", "shape_id",
+                            shapeId));
+                }
+                if (shapePtLat == null) {
+                    noticeCollection.add(new MissingRequiredValueNotice("shapes.txt", "shape_pt_lat",
+                            shapeId));
+                } else if (shapePtLat < -90.0f || shapePtLat > 90.0f) {
+                    noticeCollection.add(new FloatFieldValueOutOfRangeNotice("shapes.txt",
+                            "shape_pt_lat", shapeId, -90.0f, 90.0f, shapePtLat));
+                }
+                if (shapePtLon == null) {
+                    noticeCollection.add(new MissingRequiredValueNotice("shapes.txt", "shape_pt_lon",
+                            shapeId));
+                } else if (shapePtLon < -180.0f || shapePtLon > 180.0f) {
+                    noticeCollection.add(new FloatFieldValueOutOfRangeNotice("shapes.txt",
+                            "shape_pt_lon", shapeId, -180.0f, 180.0f, shapePtLon));
+                }
+                if (shapePtSequence == null) {
+                    noticeCollection.add(new MissingRequiredValueNotice("shapes.txt", "shape_pt_sequence",
+                            shapeId));
+                } else if (shapePtSequence < 0) {
+                    noticeCollection.add(new IntegerFieldValueOutOfRangeNotice("shapes.txt",
+                            "shape_pt_sequence", shapeId, 0, Integer.MAX_VALUE, shapePtSequence));
+                }
+                if (shapeDistTraveled != null && shapeDistTraveled < 0) {
+                    noticeCollection.add(new FloatFieldValueOutOfRangeNotice("shapes.txt",
+                            "shape_dist_traveled", shapeId, 0, Float.MAX_VALUE, shapeDistTraveled));
+                }
+                return new EntityBuildResult<>(noticeCollection);
+            } else {
+                return new EntityBuildResult<>(new Shape(shapeId, shapePtLat, shapePtLon, shapePtSequence, shapeDistTraveled));
             }
-
-            if (shapePtLon < -180.0f || shapePtLon > 180.0f) {
-                throw new IllegalArgumentException("invalid value for field shape_longitude");
-            }
-
-            if (shapePtSequence < 0) {
-                throw new IllegalArgumentException("invalid value for field shape_pt_sequence");
-            }
-
-            if (!(shapeDistTraveled == null) && shapeDistTraveled < 0) {
-                throw new IllegalArgumentException("invalid value for field shape_dist_traveled");
-            }
-            return new Shape(shapeId, shapePtLat, shapePtLon, shapePtSequence, shapeDistTraveled);
         }
     }
 }
--- domain/src/test/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/ShapeTest.java
+++ domain/src/test/java/org/mobilitydata/gtfsvalidator/domain/entity/gtfs/ShapeTest.java
@@ -16,135 +16,276 @@
 package org.mobilitydata.gtfsvalidator.domain.entity.gtfs;
 
 import org.junit.jupiter.api.Test;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.FloatFieldValueOutOfRangeNotice;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.IntegerFieldValueOutOfRangeNotice;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.MissingRequiredValueNotice;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShapeTest {
 
     // Field shapeId is annotated as `@NonNull` but test require this field to be null. Therefore annotation
     // "@SuppressWarnings("ConstantConditions")" is used here to suppress lint.
     @Test
-    public void createShapeWithNullIdShouldThrowException() {
+    public void createShapeWithNullIdShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
         //noinspection ConstantConditions
-        underTest.shapeId(null)
-                .shapePtLat(112)
-                .shapePtLon(110)
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId(null)
+                .shapePtLat(112f)
+                .shapePtLon(110f)
                 .shapePtSequence(2)
-                .shapeDistTraveled(2.0f);
+                .shapeDistTraveled(2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<MissingRequiredValueNotice> noticeCollection =
+                (List<MissingRequiredValueNotice>) entityBuildResult.getData();
+        final MissingRequiredValueNotice notice = noticeCollection.get(0);
 
-        assertEquals("field shape_id can not be null in file shapes.txt", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_id", notice.getFieldName());
+        assertEquals("no id", notice.getEntityId());
     }
 
     @Test
-    public void createShapeWithTooBigLatitudeShouldThrowException() {
+    void createShapeWithNullLatitudeShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(120)
-                .shapePtLon(0)
+        //noinspection ConstantConditions
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(null)
+                .shapePtLon(110f)
                 .shapePtSequence(2)
-                .shapeDistTraveled(2.0f);
+                .shapeDistTraveled(2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<MissingRequiredValueNotice> noticeCollection =
+                (List<MissingRequiredValueNotice>) entityBuildResult.getData();
+        final MissingRequiredValueNotice notice = noticeCollection.get(0);
 
-        assertEquals("invalid value for field shape_latitude", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_pt_lat", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
     }
 
     @Test
-    public void createShapeWithTooSmallLatitudeShouldThrowException() {
+    void createShapeWithNullLongitudeShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(-120)
-                .shapePtLon(0)
+        //noinspection ConstantConditions
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(null)
                 .shapePtSequence(2)
-                .shapeDistTraveled(2.0f);
+                .shapeDistTraveled(2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<MissingRequiredValueNotice> noticeCollection =
+                (List<MissingRequiredValueNotice>) entityBuildResult.getData();
+        final MissingRequiredValueNotice notice = noticeCollection.get(0);
 
-        assertEquals("invalid value for field shape_latitude", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_pt_lon", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
     }
 
     @Test
-    public void createShapeWithTooSmallLongitudeShouldThrowException() {
+    void createShapeWithNullShapePtSequenceShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(0)
-                .shapePtLon(-270)
-                .shapePtSequence(2)
-                .shapeDistTraveled(2.0f);
+        //noinspection ConstantConditions
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(0f)
+                .shapePtSequence(null)
+                .shapeDistTraveled(2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked  to avoid lint
+        final List<MissingRequiredValueNotice> noticeCollection =
+                (List<MissingRequiredValueNotice>) entityBuildResult.getData();
+        final MissingRequiredValueNotice notice = noticeCollection.get(0);
 
-        assertEquals("invalid value for field shape_longitude", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_pt_sequence", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
     }
 
     @Test
-    public void createShapeWithTooBigLongitudeShouldThrowException() {
+    public void createShapeWithTooBigLatitudeShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(0)
-                .shapePtLon(270)
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(120f)
+                .shapePtLon(0f)
                 .shapePtSequence(2)
-                .shapeDistTraveled(2.0f);
+                .shapeDistTraveled(2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<FloatFieldValueOutOfRangeNotice> noticeCollection =
+                (List<FloatFieldValueOutOfRangeNotice>) entityBuildResult.getData();
+        final FloatFieldValueOutOfRangeNotice notice = noticeCollection.get(0);
 
-        assertEquals("invalid value for field shape_longitude", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape id", notice.getEntityId());
+        assertEquals("shape_pt_lat", notice.getFieldName());
+        assertEquals(-90f, notice.getRangeMin());
+        assertEquals(120f, notice.getActualValue());
+        assertEquals(90f, notice.getRangeMax());
     }
 
     @Test
-    public void createShapeWithInvalidShapePtSequenceShouldThrowException() {
+    public void createShapeWithTooSmallLatitudeShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(0)
-                .shapePtLon(0)
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(-120f)
+                .shapePtLon(0f)
+                .shapePtSequence(2)
+                .shapeDistTraveled(2.0f)
+                .build();
+
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<FloatFieldValueOutOfRangeNotice> noticeCollection =
+                (List<FloatFieldValueOutOfRangeNotice>) entityBuildResult.getData();
+        final FloatFieldValueOutOfRangeNotice notice = noticeCollection.get(0);
+
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_pt_lat", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
+        assertEquals(-90f, notice.getRangeMin());
+        assertEquals(-120f, notice.getActualValue());
+        assertEquals(90f, notice.getRangeMax());
+    }
+
+    @Test
+    public void createShapeWithTooSmallLongitudeShouldGenerateNotice() {
+        final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
+
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(-270f)
+                .shapePtSequence(2)
+                .shapeDistTraveled(2.0f)
+                .build();
+
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<FloatFieldValueOutOfRangeNotice> noticeCollection =
+                (List<FloatFieldValueOutOfRangeNotice>) entityBuildResult.getData();
+        final FloatFieldValueOutOfRangeNotice notice = noticeCollection.get(0);
+
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape id", notice.getEntityId());
+        assertEquals("shape_pt_lon", notice.getFieldName());
+        assertEquals(-180f, notice.getRangeMin());
+        assertEquals(-270f, notice.getActualValue());
+        assertEquals(180f, notice.getRangeMax());
+    }
+
+    @Test
+    public void createShapeWithTooBigLongitudeShouldGenerateNotice() {
+        final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
+
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(270f)
+                .shapePtSequence(2)
+                .shapeDistTraveled(2.0f)
+                .build();
+
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<FloatFieldValueOutOfRangeNotice> noticeCollection =
+                (List<FloatFieldValueOutOfRangeNotice>) entityBuildResult.getData();
+        final FloatFieldValueOutOfRangeNotice notice = noticeCollection.get(0);
+
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_pt_lon", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
+        assertEquals(-180f, notice.getRangeMin());
+        assertEquals(270f, notice.getActualValue());
+        assertEquals(180f, notice.getRangeMax());
+    }
+
+    @Test
+    public void createShapeWithInvalidShapePtSequenceShouldGenerateNotice() {
+        final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
+
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(0f)
                 .shapePtSequence(-3)
-                .shapeDistTraveled(2.0f);
+                .shapeDistTraveled(2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<IntegerFieldValueOutOfRangeNotice> noticeCollection =
+                (List<IntegerFieldValueOutOfRangeNotice>) entityBuildResult.getData();
+        final IntegerFieldValueOutOfRangeNotice notice = noticeCollection.get(0);
 
-        assertEquals("invalid value for field shape_pt_sequence", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_pt_sequence", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
+        assertEquals(0, notice.getRangeMin());
+        assertEquals(-3, notice.getActualValue());
+        assertEquals(Integer.MAX_VALUE, notice.getRangeMax());
     }
 
-    @SuppressWarnings("unused")
     @Test
-    public void createShapeWithInvalidShapeDistTraveledShouldThrowException() {
+    public void createShapeWithInvalidShapeDistTraveledShouldGenerateNotice() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(0)
-                .shapePtLon(0)
+        final EntityBuildResult<?> entityBuildResult = underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(0f)
                 .shapePtSequence(0)
-                .shapeDistTraveled(-2.0f);
+                .shapeDistTraveled(-2.0f)
+                .build();
 
-        final Exception exception = assertThrows(IllegalArgumentException.class, underTest::build);
+        assertTrue(entityBuildResult.getData() instanceof List);
+        //noinspection unchecked to avoid lint
+        final List<FloatFieldValueOutOfRangeNotice> noticeCollection =
+                (List<FloatFieldValueOutOfRangeNotice>) entityBuildResult.getData();
+        final FloatFieldValueOutOfRangeNotice notice = noticeCollection.get(0);
 
-        assertEquals("invalid value for field shape_dist_traveled", exception.getMessage());
+        assertEquals("shapes.txt", notice.getFilename());
+        assertEquals("shape_dist_traveled", notice.getFieldName());
+        assertEquals("shape id", notice.getEntityId());
+        assertEquals(0, notice.getRangeMin());
+        assertEquals(-2.0f, notice.getActualValue());
+        assertEquals(Float.MAX_VALUE, notice.getRangeMax());
     }
 
     @Test
     public void shapeShouldBeAbleToBeComparedToOtherShape() {
         final Shape.ShapeBuilder underTest = new Shape.ShapeBuilder();
 
-        underTest.shapeId("test")
-                .shapePtLat(0)
-                .shapePtLon(0)
+        underTest.shapeId("shape id")
+                .shapePtLat(0f)
+                .shapePtLon(0f)
                 .shapePtSequence(0)
                 .shapeDistTraveled(0f);
 
-        final Shape firstShapeInSequence = underTest.build();
+        final Shape firstShapeInSequence = (Shape) underTest.build().getData();
 
         underTest.shapePtSequence(2);
 
-        final Shape secondShapeInSequence = underTest.build();
+        final Shape secondShapeInSequence = (Shape) underTest.build().getData();
 
         assertTrue(secondShapeInSequence.isGreaterThan(firstShapeInSequence));
     }
