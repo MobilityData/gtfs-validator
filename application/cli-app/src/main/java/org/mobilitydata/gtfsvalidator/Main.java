78f13e26ae03bc3e132a88931b4d62982f18d198
Lionel Nébot Janvier <35747326+lionel-nj@users.noreply.github.com>
2020-05-06 17:10:18 -0400

    Implement #182 - Load GTFS entities into memory (#183)
    * Load GTFS entities into memory -- implement use case
    
    - implement new use case LoadAllEntityIntoMemory
    - adapt main method to load gtfs entities into memory using an internal representation
      - create class LoadAllEntityIntoMemory in DefaultConfig to execute use case LoadAllEntityIntoMemory in main method
    - remove blank line
    - implement test to check to verify new use case LoadAllEntityIntoMemory behavior
    
    * Build GTFS entities -- rework use case usage
    
    - remove use case LoadAllEntityIntoMemory.java to avoid cross dependency among use cases
      - remove associated test class
    - instantiate use case ProcessParsedXXX for each entity: agency and route, via method in DefaultConfig
    - retrieve use cases from config once for performance instead of instantiating use cases for each processed parsed entity
    
    * Touch: remove blank lines
    
    * Build GTFS entities -- use filename as equivalent expression of parsedEntity.getRawFileInfo().getFilename()
    
    * Gtfs entities -- rework methods processParsedAgency and processParsedRoute return
    
    - use default constructor for gtfs entities builder
    
    * Load GTFS entities into memory -- implement use case to create the filename list on which the GTFS semantic validation be processed
    
    - implement new use case CreateGtfsSemanticValidationFilenameList
    - implement method createGtfsSemanticValidationFilenameList in DefaultConfig
    - instantiate use case CreateGtfsSemanticValidationFilenameList in main method
    - write tests to verify new use case behavior

--- application/cli-app/src/main/java/org/mobilitydata/gtfsvalidator/Main.java
+++ application/cli-app/src/main/java/org/mobilitydata/gtfsvalidator/Main.java
@@ -19,7 +19,11 @@ package org.mobilitydata.gtfsvalidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.mobilitydata.gtfsvalidator.config.DefaultConfig;
+import org.mobilitydata.gtfsvalidator.domain.entity.ParsedEntity;
 import org.mobilitydata.gtfsvalidator.usecase.ParseSingleRowForFile;
+import org.mobilitydata.gtfsvalidator.usecase.ProcessParsedAgency;
+import org.mobilitydata.gtfsvalidator.usecase.ProcessParsedRoute;
+import org.mobilitydata.gtfsvalidator.usecase.ValidateGtfsTypes;
 import org.mobilitydata.gtfsvalidator.usecase.port.ExecParamRepository;
 
 import java.io.IOException;
@@ -29,7 +33,6 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-
         final long startTime = System.nanoTime();
         final Logger logger = LogManager.getLogger();
         final DefaultConfig config = new DefaultConfig(logger);
@@ -40,7 +43,8 @@ public class Main {
             // use case will inspect parameters and decide if help menu should be displayed or not
             if (!config.printHelp().execute()) {
 
-                // use case will inspect parameters and display relevant information about the validation execution process
+                // use case will inspect parameters and display relevant information about the validation execution
+                // process
                 config.logExecutionInfo().execute();
 
                 // use case will inspect parameters and decide if GTFS dataset should be downloaded or not
@@ -51,25 +55,57 @@ public class Main {
                         .execute();
 
                 final List<String> filenameList = config.validateAllRequiredFilePresence().execute();
-
                 filenameList.addAll(config.validateAllOptionalFileName().execute());
 
-                // base validation
+                // to be replaced by the call to a future use case that will create the list of files to exclude from
+                // command line option or configuration file.
+                final List<String> toExcludeFromGtfsSemanticValidation = List.of("stops.txt", "trips.txt",
+                        "stop_times.txt", "calendar.txt", "calendar_dates.txt", "fare_attributes.txt", "fare_rules.txt",
+                        "shapes.txt", "frequencies.txt", "transfers.txt", "pathways.txt", "levels.txt",
+                        "translations.txt", "feed_info.txt", "attributions.txt");
+
+                final List<String> filenameListToProcess = config.createGtfsSemanticValidationFilenameList(filenameList)
+                        .execute(toExcludeFromGtfsSemanticValidation);
+
+                // retrieve use case to be used multiple times
+                final ValidateGtfsTypes validateGtfsTypes = config.validateGtfsTypes();
+                final ProcessParsedAgency processParsedAgency = config.processParsedAgency();
+                final ProcessParsedRoute processParsedRoute = config.processParsedRoute();
+
+                // base validation + build gtfs entities
                 filenameList.forEach(filename -> {
                     config.validateHeadersForFile(filename).execute();
                     config.validateAllRowLengthForFile(filename).execute();
 
-                    ParseSingleRowForFile parseSingleRowForFile = config.parseSingleRowForFile(filename);
+                    final ParseSingleRowForFile parseSingleRowForFile = config.parseSingleRowForFile(filename);
                     while (parseSingleRowForFile.hasNext()) {
-                        config.validateGtfsTypes().execute(parseSingleRowForFile.execute());
+                        final ParsedEntity parsedEntity = parseSingleRowForFile.execute();
+                        validateGtfsTypes.execute(parsedEntity);
+
+                        // load gtfs entities into memory
+                        // in the future all filename in filenameList will be processed. For now focusing on routes.txt
+                        // and agency.txt.
+                        // filenames in filenameList will be determined using a dependency tree defined by a JSON file,
+                        // and command lines or configuration file will be used to exclude files from the validation
+                        // process.
+                        if (filenameListToProcess.contains(filename)) {
+                            switch (filename) {
+                                case "agency.txt": {
+                                    processParsedAgency.execute(parsedEntity);
+                                    break;
+                                }
+                                case "routes.txt": {
+                                    processParsedRoute.execute(parsedEntity);
+                                    break;
+                                }
+                            }
+                        }
                     }
                 });
-
                 config.cleanOrCreatePath().execute(ExecParamRepository.OUTPUT_KEY);
 
                 config.exportResultAsFile().execute();
             }
-
         } catch (IOException e) {
             if (e.getMessage().contains("execution-parameters.json")) {
                 config.printHelp().execute();
