7d1e020518c2394d27dfb911bd65e7647667d0a7
lionel-nj <lionel@mobilitydata.org>
2020-06-02 11:36:54 -0400

    FareRule entities -- Add FareRule entities to GtfsDataRepo
    - Add use case to config
    - Execute use case ProcessParsedFareRule inside main method

--- config/src/main/java/org/mobilitydata/gtfsvalidator/config/DefaultConfig.java
+++ config/src/main/java/org/mobilitydata/gtfsvalidator/config/DefaultConfig.java
@@ -22,6 +22,7 @@ import org.apache.logging.log4j.Logger;
 import org.mobilitydata.gtfsvalidator.db.*;
 import org.mobilitydata.gtfsvalidator.domain.entity.RawFileInfo;
 import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.Agency;
+import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.FareRule;
 import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.routes.Route;
 import org.mobilitydata.gtfsvalidator.usecase.*;
 import org.mobilitydata.gtfsvalidator.usecase.port.*;
@@ -168,6 +169,10 @@ public class DefaultConfig {
 
     public ProcessParsedRoute processParsedRoute() {
         return new ProcessParsedRoute(resultRepo, gtfsDataRepository, new Route.RouteBuilder());
+    }
+
+    public ProcessParsedFareRule processParsedFareRule() {
+        return new ProcessParsedFareRule(resultRepo, gtfsDataRepository, new FareRule.FareRuleBuilder());
     }
 
     public GenerateExclusionFilenameList generateExclusionFilenameList() {
