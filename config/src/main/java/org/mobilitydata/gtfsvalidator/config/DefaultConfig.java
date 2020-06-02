b0ad1b41e7f21802d13dab3a67b9384a3074eb15
lionel-nj <lionel@mobilitydata.org>
2020-06-02 12:38:45 -0400

    Attribution entities -- Add Attribution entities to GtfsDataRepo
    - Add use case to config
    - Execute use case ProcessParsedAttribution inside main method

--- config/src/main/java/org/mobilitydata/gtfsvalidator/config/DefaultConfig.java
+++ config/src/main/java/org/mobilitydata/gtfsvalidator/config/DefaultConfig.java
@@ -22,6 +22,7 @@ import org.apache.logging.log4j.Logger;
 import org.mobilitydata.gtfsvalidator.db.*;
 import org.mobilitydata.gtfsvalidator.domain.entity.RawFileInfo;
 import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.Agency;
+import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.Attribution;
 import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.routes.Route;
 import org.mobilitydata.gtfsvalidator.usecase.*;
 import org.mobilitydata.gtfsvalidator.usecase.port.*;
@@ -180,6 +181,10 @@ public class DefaultConfig {
 
     public ProcessParsedRoute processParsedRoute() {
         return new ProcessParsedRoute(resultRepo, gtfsDataRepository, new Route.RouteBuilder());
+    }
+
+    public ProcessParsedAttribution processParsedAttribution() {
+        return new ProcessParsedAttribution(resultRepo, gtfsDataRepository, new Attribution.AttributionBuilder());
     }
 
     public GenerateExclusionFilenameList generateExclusionFilenameList() {
