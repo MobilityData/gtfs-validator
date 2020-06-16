59cebcf5b878ee41d21274e7896516039851c640
lionel-nj <lionel@mobilitydata.org>
2020-06-16 11:24:32 -0400

    Re-establish notice identifier

--- domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/notice/error/UnexpectedEnumValueNotice.java
+++ domain/src/main/java/org/mobilitydata/gtfsvalidator/domain/entity/notice/error/UnexpectedEnumValueNotice.java
@@ -25,7 +25,7 @@ public class UnexpectedEnumValueNotice extends ErrorNotice {
 
     public UnexpectedEnumValueNotice(final String filename, final String fieldName, final String entityId,
                                      final Integer enumValue) {
-        super(filename, E_020,
+        super(filename, E_021,
                 "Unexpected enum value",
                 "Invalid value :`" + enumValue + "` - for field:`" + fieldName + "` in file:`" + filename +
                         "` for entity with id:`" + entityId + "`", entityId);
