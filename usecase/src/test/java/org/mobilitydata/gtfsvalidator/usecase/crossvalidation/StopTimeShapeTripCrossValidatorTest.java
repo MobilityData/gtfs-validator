1f493f693d342a168734491fadae4495c6b94ba4
lionel-nj <lionel@mobilitydata.org>
2020-07-15 14:16:42 -0400

    Rework test -- Verify execution of StopTimeShapeTripCrossValidatorTest.execute method

--- usecase/src/test/java/org/mobilitydata/gtfsvalidator/usecase/crossvalidation/StopTimeShapeTripCrossValidatorTest.java
+++ usecase/src/test/java/org/mobilitydata/gtfsvalidator/usecase/crossvalidation/StopTimeShapeTripCrossValidatorTest.java
@@ -18,58 +18,26 @@ package org.mobilitydata.gtfsvalidator.usecase.crossvalidation;
 
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
-import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.ShapePoint;
-import org.mobilitydata.gtfsvalidator.domain.entity.gtfs.stoptimes.StopTime;
 import org.mobilitydata.gtfsvalidator.usecase.port.GtfsDataRepository;
 import org.mobilitydata.gtfsvalidator.usecase.port.ValidationResultRepository;
-import org.mockito.ArgumentMatchers;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
 
 import static org.mockito.Mockito.*;
 
 class StopTimeShapeTripCrossValidatorTest {
 
-    // suppress warning regarding ignored result of method since it is not necessary here.
-    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
-    void notFoundTripIdShouldNotGenerateNotice() {
+    void execute() {
         final ValidationResultRepository mockResultRepo = mock(ValidationResultRepository.class);
-        final StopTime mockStopTime = mock(StopTime.class);
-        when(mockStopTime.getShapeDistTraveled()).thenReturn(340f);
-
-        final Map<String, TreeMap<Integer, StopTime>> stopTimeCollection = new HashMap<>();
-        final TreeMap<Integer, StopTime> innerMap = new TreeMap<>();
-        innerMap.put(3, mockStopTime);
-        stopTimeCollection.put("non existing trip id", innerMap);
-
         final GtfsDataRepository mockDataRepo = mock(GtfsDataRepository.class);
-        when(mockDataRepo.getStopTimeAll()).thenReturn(stopTimeCollection);
-        when(mockDataRepo.getTripById("non existing trip id")).thenReturn(null);
-
-        // suppressed warning regarding unchecked type since it is not required here
-        // noinspection unchecked
-        final Map<Integer, ShapePoint> mockShape = mock(TreeMap.class);
-        when(mockDataRepo.getShapeById("shape id")).thenReturn(mockShape);
         final Logger mockLogger = mock(Logger.class);
 
         final StopTimeShapeTripCrossValidator underTest =
-                new StopTimeShapeTripCrossValidator(mockDataRepo, mockResultRepo, mockLogger);
+                spy(new StopTimeShapeTripCrossValidator(mockDataRepo, mockResultRepo, mockLogger));
 
         underTest.execute();
 
         verify(mockLogger, times(1)).info("Validating rule 'E034 - `shape_id` not found" +
                 System.lineSeparator());
-
-        verify(mockDataRepo, times(1)).getStopTimeAll();
-        verify(mockDataRepo, times(1)).getTripById(
-                ArgumentMatchers.eq("non existing trip id"));
-        verify(mockDataRepo, times(1)).getShapeById(ArgumentMatchers.eq(null));
-
-        verify(mockStopTime, times(1)).getShapeDistTraveled();
-        verifyNoInteractions(mockResultRepo);
-        verifyNoMoreInteractions(mockDataRepo, mockLogger, mockStopTime, mockShape);
+        verify(underTest, times(1)).checkE034();
     }
 }
