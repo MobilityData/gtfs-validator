1e7b9cffcfa25ab116526849f3b313adcdb2ddbc
Fabrice V <fabrice@mobilitydata.org>
2020-08-13 09:57:23 -0400

    Fast Travel
    - add notice
    - add notice output in repository
      - accumulate stop sequence list to provide it in notice
    - add tests
    - update javadoc

--- usecase/src/main/java/org/mobilitydata/gtfsvalidator/usecase/ValidateTripTravelSpeed.java
+++ usecase/src/main/java/org/mobilitydata/gtfsvalidator/usecase/ValidateTripTravelSpeed.java
@@ -17,11 +17,14 @@
 package org.mobilitydata.gtfsvalidator.usecase;
 
 import org.apache.logging.log4j.Logger;
-import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.MissingTripEdgeStopTimeNotice;
+import org.mobilitydata.gtfsvalidator.domain.entity.notice.error.FastTravelBetweenStopsNotice;
 import org.mobilitydata.gtfsvalidator.domain.entity.stops.LocationBase;
 import org.mobilitydata.gtfsvalidator.usecase.port.GtfsDataRepository;
 import org.mobilitydata.gtfsvalidator.usecase.port.ValidationResultRepository;
 import org.mobilitydata.gtfsvalidator.usecase.utils.GeospatialUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Use case for E046 to validate that all records of `trips.txt` refer to an a collection of {@code StopTime} from file
@@ -33,6 +36,7 @@ public class ValidateTripTravelSpeed {
     private final ValidationResultRepository resultRepo;
     private final GeospatialUtils geoUtils;
     private final Logger logger;
+    private final static float METER_PER_SECOND_TO_KMH_CONVERSION_FACTOR = 3.6f;
 
     /**
      * @param dataRepo   a repository storing the data of a GTFS dataset
@@ -50,9 +54,9 @@ public class ValidateTripTravelSpeed {
     }
 
     /**
-     * Use case execution method: Checks if first and last stop in sequence associated with a trip define
-     * both fields `departure_time` and `arrival_time`.
-     * A new {@link MissingTripEdgeStopTimeNotice} notice is generated each time this condition is false.
+     * Use case execution method: Checks all records of `trips.txt` refer to an a collection of {@code StopTime} from file
+     * `stop_times.txt` of which -when sorted by `stop_sequence`- the travel speed between each stop is not above 150 km/h
+     * A new {@link FastTravelBetweenStopsNotice} notice is generated each time this condition is false.
      * This notice is then added to the {@link ValidationResultRepository} provided in the constructor.
      */
     public void execute() {
@@ -66,6 +70,7 @@ public class ValidateTripTravelSpeed {
 
                 // used to accumulate distance between stops with same arrival and departure times
                 int accumulatedDistanceMeter = 0;
+                final List<Integer> accumulatedStopSequence = new ArrayList<>();
             };
 
             dataRepo.getStopTimeByTripId(tripId).forEach((stopSequence, stopTime) -> {
@@ -75,7 +80,7 @@ public class ValidateTripTravelSpeed {
                 double currentStopLat = currentStop.getStopLat();
                 double currentStopLon = currentStop.getStopLon();
 
-                Integer currentStopArrivalTime = stopTime.getArrivalTime();
+                Integer currentArrivalTime = stopTime.getArrivalTime();
                 int distanceFromPreviousStopMeter = geoUtils.distanceBetweenMeter(
                         previousStopsData.latitude,
                         previousStopsData.longitude,
@@ -84,54 +89,40 @@ public class ValidateTripTravelSpeed {
                 );
                 boolean sameArrivalAndDeparture = false;
 
-                if (previousStopsData.departureTime != null && currentStopArrivalTime != null) {
-                    sameArrivalAndDeparture = currentStopArrivalTime.equals(previousStopsData.departureTime);
+                if (previousStopsData.departureTime != null && currentArrivalTime != null) {
+                    sameArrivalAndDeparture = currentArrivalTime.equals(previousStopsData.departureTime);
 
                     if (!sameArrivalAndDeparture) {
-                        int durationSecond = currentStopArrivalTime - previousStopsData.departureTime;
+                        int durationSecond = currentArrivalTime - previousStopsData.departureTime;
                         int distanceMeter = distanceFromPreviousStopMeter + previousStopsData.accumulatedDistanceMeter;
                         int speedMeterPerSecond = distanceMeter / durationSecond;
 
                         if (speedMeterPerSecond > 42) { // roughly 150 km per hour. Put it in default parameters
+                            previousStopsData.accumulatedStopSequence.add(stopSequence);
 
-                            if (previousStopsData.accumulatedDistanceMeter == 0) {
-                                // contiguous stop
-                                //Add notice
-                                /*resultRepo.addNotice(
-                                        new MissingTripEdgeStopTimeNotice("arrival_time",
-                                                tripId,
-                                                stopTime.getStopSequence()
-                                        )
-                                );*/
-                            } else {
-                                // far stops
-                                //Add notice
-                                /*resultRepo.addNotice(
-                                        new MissingTripEdgeStopTimeNotice("arrival_time",
-                                                tripId,
-                                                stopTime.getStopSequence()
-                                        )
-                                );*/
-                            }
+                            resultRepo.addNotice(
+                                    new FastTravelBetweenStopsNotice(
+                                            tripId,
+                                            speedMeterPerSecond * METER_PER_SECOND_TO_KMH_CONVERSION_FACTOR,
+                                            new ArrayList<>(previousStopsData.accumulatedStopSequence)
+                                    )
+                            );
                         }
                     }
                 }
 
                 // Prepare data for next iteration
                 if (sameArrivalAndDeparture) {
-                    previousStopsData.accumulatedDistanceMeter += geoUtils.distanceBetweenMeter(
-                            previousStopsData.latitude,
-                            previousStopsData.longitude,
-                            currentStopLat,
-                            currentStopLon
-                    );
+                    previousStopsData.accumulatedDistanceMeter += distanceFromPreviousStopMeter;
                 } else {
                     previousStopsData.accumulatedDistanceMeter = 0;
+                    previousStopsData.accumulatedStopSequence.clear();
                 }
 
                 previousStopsData.departureTime = stopTime.getDepartureTime();
                 previousStopsData.latitude = currentStopLat;
                 previousStopsData.longitude = currentStopLon;
+                previousStopsData.accumulatedStopSequence.add(stopSequence);
             });
         });
     }
