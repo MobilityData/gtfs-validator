1becb3cdd5a9c0637e33483ad78cdae629c5792f
lionel-nj <lionel@mobilitydata.org>
2020-08-31 19:15:18 -0400

    Redesign UI
    - add MobilityData's logo
    - add input field
    - add validation button
    - install semantic-react-ui dependency

--- gtfs-validator-web-ui/src/App.js
+++ gtfs-validator-web-ui/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import InputField from "./components/input";
+import FittedButton from "./components/button";
 
 function App() {
   return (
@@ -8,16 +10,33 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Add text bar here for command line
           </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>
+            <InputField placeHolderText="Enter command line here" id="command-line-input"/>
+          </p>
+          <p>
+            <FittedButton description="Lauch validation process"/>
+          </p>
+          <p>
+            <a
+                className="App-link"
+                href="https://mobilitydata.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              MobilityData
+            </a></p>
+          <p>
+            <a
+                className="App-link"
+                href="https://github.com/MobilityData/gtfs-validator"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Project documentation
+            </a>
+          </p>
         </header>
       </div>
   );
---
+++ gtfs-validator-web-ui/src/components/button.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const FittedButton = (props) => <Button fluid>{props.description}</Button>
+
+export default FittedButton
