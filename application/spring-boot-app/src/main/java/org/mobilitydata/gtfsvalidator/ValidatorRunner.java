cf4bc41a022f67b712c1ff770070cd0bf49ee2a4
lionel-nj <lionel@mobilitydata.org>
2020-09-18 09:24:05 -0400

    Springbootify project

---
+++ application/spring-boot-app/src/main/java/org/mobilitydata/gtfsvalidator/ValidatorRunner.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2020. MobilityData IO.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorRunner implements ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ValidatorRunner.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
