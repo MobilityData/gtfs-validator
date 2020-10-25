beb5161bfd9f2999cb116b7ba4e9c13ee1d26a80
fabrice-v <55884852+fabrice-v@users.noreply.github.com>
2020-10-25 02:24:49 -0400

    Breaking Java code

--- application/cli-app/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
+++ application/cli-app/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.mobilitydata.gtfsvalidator.cli;
+package org.mobilitydata.gtfsvalidator.cli
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
