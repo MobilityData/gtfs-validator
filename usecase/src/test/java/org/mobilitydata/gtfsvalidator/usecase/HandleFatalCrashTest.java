ab1f32244f6cafe75a8d57c9749764aea6600641
lionel-nj <lionel@mobilitydata.org>
2020-11-11 12:23:16 -0500

    draft unit tests for HandleFatalCrash use case

---
+++ usecase/src/test/java/org/mobilitydata/gtfsvalidator/usecase/HandleFatalCrashTest.java
@@ -0,0 +1,18 @@
+package org.mobilitydata.gtfsvalidator.usecase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandleFatalCrashTest {
+
+    @Test
+    void shouldGenerateAndAddOutOfMemoryNoticeWhenOutOfMemoryErrorIsThrown() {
+        /// TODO
+    }
+
+    @Test
+    void shouldGenerateAndAddValidatorCrashNoticeWhenExceptionIsThrown() {
+        /// TODO
+    }
+}
