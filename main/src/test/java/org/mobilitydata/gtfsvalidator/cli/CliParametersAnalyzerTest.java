db91b40d21d44184a8c804a3d117f07e92a5946d
lionel-nj <lionel@mobilitydata.org>
2020-12-07 16:21:56 -0400

    test(CliParametersAnalyzer): write unit tests

--- main/src/test/java/org/mobilitydata/gtfsvalidator/cli/CliParametersAnalyzerTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/cli/CliParametersAnalyzerTest.java
@@ -1,19 +1,83 @@
 package org.mobilitydata.gtfsvalidator.cli;
 
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(JUnit4.class)
 public class CliParametersAnalyzerTest {
 
     @Test
-    public void provideUrlAndInputCliParametersShouldThrowException() {
+    public void provideUrlAndInputCliParametersShouldReturnFalse() {
+        Logger mockLogger = mock(Logger.class);
+        Arguments mockArguments = mock(Arguments.class);
+        when(mockArguments.getUrl()).thenReturn("url to dataset");
+        when(mockArguments.getInput()).thenReturn("path to dataset");
+
+        CliParametersAnalyzer underTest = new CliParametersAnalyzer(mockLogger);
+        underTest.isValid(mockArguments);
+        assertThat(underTest.isValid(mockArguments)).isFalse();
+        // TODO: verify logger.error has been called with the correct set of parameters
     }
 
     @Test
-    public void bothUrlAndInputCliParametersNotProvidedShouldThrowException() {}
+    public void bothUrlAndInputCliParametersNotProvidedShouldReturnFalse() {
+        Logger mockLogger = mock(Logger.class);
+        Arguments mockArguments = mock(Arguments.class);
+        when(mockArguments.getUrl()).thenReturn(null);
+        when(mockArguments.getInput()).thenReturn(null);
+
+        CliParametersAnalyzer underTest = new CliParametersAnalyzer(mockLogger);
+        underTest.isValid(mockArguments);
+        assertThat(underTest.isValid(mockArguments)).isFalse();
+        // TODO: verify logger.error has been called with the correct set of parameters
+    }
 
     @Test
-    public void provideUrlWithoutSpecifyingStorageDirectoryCliParameterShouldNotThrowException() {}
+    public void provideUrlWithoutSpecifyingStorageDirectoryCliParameterShouldReturnFalse() {
+        Logger mockLogger = mock(Logger.class);
+        Arguments mockArguments = mock(Arguments.class);
+        when(mockArguments.getUrl()).thenReturn("url to dataset");
+        when(mockArguments.getInput()).thenReturn(null);
+        when(mockArguments.getStorageDirectory()).thenReturn(null);
+
+        CliParametersAnalyzer underTest = new CliParametersAnalyzer(mockLogger);
+        underTest.isValid(mockArguments);
+        assertThat(underTest.isValid(mockArguments)).isFalse();
+        // TODO: verify logger.error has been called with the correct set of parameters
+    }
 
     @Test
-    public void provideStorageDirectoryCliParameterWithoutSpecifyingUrlShouldThrowException() {}
+    public void provideStorageDirectoryCliParameterWithoutSpecifyingUrlShouldReturnFalse() {
+        Logger mockLogger = mock(Logger.class);
+        Arguments mockArguments = mock(Arguments.class);
+        when(mockArguments.getUrl()).thenReturn(null);
+        when(mockArguments.getInput()).thenReturn(null);
+        when(mockArguments.getStorageDirectory()).thenReturn("storage.zip");
+
+        CliParametersAnalyzer underTest = new CliParametersAnalyzer(mockLogger);
+        underTest.isValid(mockArguments);
+        assertThat(underTest.isValid(mockArguments)).isFalse();
+        // TODO: verify logger.error has been called with the correct set of parameters
+    }
+
+    @Test
+    public void provideUrlStorageDirectoryAndNoInputCliParameterShouldReturnTrue() {
+        Logger mockLogger = mock(Logger.class);
+        Arguments mockArguments = mock(Arguments.class);
+        when(mockArguments.getUrl()).thenReturn("url to dataset");
+        when(mockArguments.getInput()).thenReturn(null);
+        when(mockArguments.getStorageDirectory()).thenReturn("storage.zip");
+
+        CliParametersAnalyzer underTest = new CliParametersAnalyzer(mockLogger);
+        underTest.isValid(mockArguments);
+        assertThat(underTest.isValid(mockArguments)).isTrue();
+        // TODO: verify logger.error has been called with the correct set of parameters
+    }
 }
