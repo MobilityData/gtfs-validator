8f697b9374097c7e7238876c2150e3039e5b2aca
Alexej Ababilov <aababilov@google.com>
2020-12-10 01:47:41 +1100

    feat: Validate presence of location_type and the type of referenced location (#500)
    * `LocationWithoutParentStationNotice` - A location that must have `parent_station` field does not have it. The following location types must have `parent_station`: entrance, generic node, boarding area.
    * `PlatformWithoutParentStationNotice` - A platform has no `parent_station` field set. This is different from {@code LocationWithoutParentStationNotice} since it is less severe. Here we treat `location_type=0` with `platform_code` as a platform.
    * `StationWithParentStationNotice` - A station has `parent_station` field set.
    * `WrongParentLocationTypeNotice` - incorrect type of the parent location (e.g., parent for a stop must be a station).

---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/notice/LocationWithoutParentStationNotice.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator.notice;
+
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * A location that must have `parent_station` field does not have it.
+ * <p>
+ * The following location types must have `parent_station`: entrance, generic node, boarding area.
+ */
+public class LocationWithoutParentStationNotice extends Notice {
+    public LocationWithoutParentStationNotice(String stopId, long csvRowNumber, int locationType) {
+        super(ImmutableMap.of(
+                "stopId", stopId,
+                "csvRowNumber", csvRowNumber,
+                "locationType", locationType));
+    }
+
+    @Override
+    public String getCode() {
+        return "location_without_parent_station";
+    }
+}
