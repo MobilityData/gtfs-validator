1f9aa519146f2871b17ae121bde35f34c264ef68
Sean Barbeau <sjbarbeau@gmail.com>
2020-12-09 11:37:18 -0500

    chore: Replace with single class imports

--- main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsTranslationSchema.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsTranslationSchema.java
@@ -16,7 +16,11 @@
 
 package org.mobilitydata.gtfsvalidator.table;
 
-import org.mobilitydata.gtfsvalidator.annotation.*;
+import org.mobilitydata.gtfsvalidator.annotation.ConditionallyRequired;
+import org.mobilitydata.gtfsvalidator.annotation.FieldType;
+import org.mobilitydata.gtfsvalidator.annotation.FieldTypeEnum;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsTable;
+import org.mobilitydata.gtfsvalidator.annotation.Required;
 
 import java.util.Locale;
 
