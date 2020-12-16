3049baf71aa8af76c60c533b2d66dbde81018bba
lionel-nj <lionel@mobilitydata.org>
2020-12-16 18:43:33 -0400

    test: unit test for GtfsTransferTableContainer class

---
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsTransferTableContainerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 Google LLC, MobilityData IO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator.table;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+
+@RunWith(JUnit4.class)
+public class GtfsTransferTableContainerTest {
+    @Test
+    public void forEmptyFile() {
+        GtfsTransferTableContainer tableContainer = GtfsTransferTableContainer.forEmptyFile();
+        assertThat(tableContainer.getEntities()).isEmpty();
+        assertThat(tableContainer.gtfsFilename()).matches("transfers.txt");
+        assertThat(tableContainer.isEmptyFile()).isTrue();
+        assertThat(tableContainer.hasInvalidHeaders()).isFalse();
+        assertThat(tableContainer.isMissingFile()).isFalse();
+    }
+
+    @Test
+    public void forMissingFile() {
+        GtfsTransferTableContainer tableContainer = GtfsTransferTableContainer.forMissingFile();
+        assertThat(tableContainer.getEntities()).isEmpty();
+        assertThat(tableContainer.gtfsFilename()).matches("transfers.txt");
+        assertThat(tableContainer.isEmptyFile()).isFalse();
+        assertThat(tableContainer.hasInvalidHeaders()).isFalse();
+        assertThat(tableContainer.isMissingFile()).isTrue();
+    }
+
+    @Test
+    public void forInvalidHeaders() {
+        GtfsTransferTableContainer tableContainer = GtfsTransferTableContainer.forInvalidHeaders();
+        assertThat(tableContainer.getEntities()).isEmpty();
+        assertThat(tableContainer.gtfsFilename()).matches("transfers.txt");
+        assertThat(tableContainer.isEmptyFile()).isFalse();
+        assertThat(tableContainer.hasInvalidHeaders()).isTrue();
+        assertThat(tableContainer.isMissingFile()).isFalse();
+    }
+
+    @Test
+    public void forEntities() {
+        NoticeContainer noticeContainer = new NoticeContainer();
+        List<GtfsTransfer> transferCollection = new ArrayList<>();
+        GtfsTransfer mockTransfer = mock(GtfsTransfer.class);
+        transferCollection.add(mockTransfer);
+        GtfsTransferTableContainer tableContainer =
+                GtfsTransferTableContainer.forEntities(transferCollection, noticeContainer);
+        assertThat(tableContainer.getEntities().isEmpty()).isFalse();
+        assertThat(tableContainer.isEmptyFile()).isFalse();
+        assertThat(tableContainer.gtfsFilename()).matches("transfers.txt");
+        assertThat(tableContainer.hasInvalidHeaders()).isFalse();
+        assertThat(tableContainer.isMissingFile()).isFalse();
+    }
+}
