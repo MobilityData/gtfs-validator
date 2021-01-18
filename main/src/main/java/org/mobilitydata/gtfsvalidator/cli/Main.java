555f04efc1d6cce89347c870f479aa0cb0039d02
lionel-nj <lionel@mobilitydata.org>
2021-01-18 13:33:05 -0400

    add system errors to noticeContainer
    - create new systemError to describe system error at GtfsInput creation
    - split generation and export of validation/system error reports from main code block

--- main/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
@@ -28,76 +28,83 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
 import org.mobilitydata.gtfsvalidator.input.GtfsInput;
+import org.mobilitydata.gtfsvalidator.notice.GtfsInputCreationError;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedLoader;
 import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
 
-/** The main entry point for GTFS Validator CLI. */
+/**
+ * The main entry point for GTFS Validator CLI.
+ */
 public class Main {
-  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-  public static void main(String[] argv) {
-    Arguments args = new Arguments();
-    CliParametersAnalyzer cliParametersAnalyzer = new CliParametersAnalyzer();
-    new JCommander(args).parse(argv);
-    if (!cliParametersAnalyzer.isValid(args)) {
-      System.exit(1);
-    }
-
-    ValidatorLoader validatorLoader = new ValidatorLoader();
-    GtfsFeedLoader feedLoader = new GtfsFeedLoader();
-
-    GtfsFeedName feedName = GtfsFeedName.parseString(args.getFeedName());
-    System.out.println("Feed name: " + feedName.getCountryFirstName());
-    System.out.println("Input: " + args.getInput());
-    System.out.println("URL: " + args.getUrl());
-    System.out.println("Output: " + args.getOutputBase());
-    System.out.println("Path to archive storage directory: " + args.getStorageDirectory());
-    System.out.println("Table loaders: " + feedLoader.listTableLoaders());
-    System.out.println("Validators:");
-    System.out.println(validatorLoader.listValidators());
-
-    final long startNanos = System.nanoTime();
-    // Input.
-    feedLoader.setNumThreads(args.getNumThreads());
-    NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedContainer feedContainer;
-    GtfsInput gtfsInput;
-    try {
-      if (args.getInput() == null) {
-        if (Strings.isNullOrEmpty(args.getStorageDirectory())) {
-          gtfsInput = GtfsInput.createFromUrlInMemory(new URL(args.getUrl()));
-        } else {
-          gtfsInput = GtfsInput.createFromUrl(new URL(args.getUrl()), args.getStorageDirectory());
+    public static void main(String[] argv) {
+        Arguments args = new Arguments();
+        CliParametersAnalyzer cliParametersAnalyzer = new CliParametersAnalyzer();
+        new JCommander(args).parse(argv);
+        if (!cliParametersAnalyzer.isValid(args)) {
+            System.exit(1);
         }
-      } else {
-        gtfsInput = GtfsInput.createFromPath(Paths.get(args.getInput()));
-      }
-    } catch (IOException | URISyntaxException | InterruptedException e) {
-      // FIXME: Add system errors to noticeContainer here.
-      logger.atSevere().withCause(e).log("Cannot load GTFS feed");
-      return;
-    }
-    feedContainer =
-        feedLoader.loadAndValidate(gtfsInput, feedName, validatorLoader, noticeContainer);
 
-    // Output.
-    new File(args.getOutputBase()).mkdirs();
-    try {
-      Files.write(
-          Paths.get(args.getOutputBase(), "report.json"),
-          noticeContainer.exportValidationNotices().getBytes(StandardCharsets.UTF_8));
-      Files.write(
-          Paths.get(args.getOutputBase(), "system_errors.json"),
-          noticeContainer.exportSystemErrors().getBytes(StandardCharsets.UTF_8));
-    } catch (IOException e) {
-      logger.atSevere().withCause(e).log("Cannot store report files");
+        ValidatorLoader validatorLoader = new ValidatorLoader();
+        GtfsFeedLoader feedLoader = new GtfsFeedLoader();
+
+        GtfsFeedName feedName = GtfsFeedName.parseString(args.getFeedName());
+        System.out.println("Feed name: " + feedName.getCountryFirstName());
+        System.out.println("Input: " + args.getInput());
+        System.out.println("URL: " + args.getUrl());
+        System.out.println("Output: " + args.getOutputBase());
+        System.out.println("Path to archive storage directory: " + args.getStorageDirectory());
+        System.out.println("Table loaders: " + feedLoader.listTableLoaders());
+        System.out.println("Validators:");
+        System.out.println(validatorLoader.listValidators());
+
+        final long startNanos = System.nanoTime();
+        // Input.
+        feedLoader.setNumThreads(args.getNumThreads());
+        NoticeContainer noticeContainer = new NoticeContainer();
+        GtfsFeedContainer feedContainer;
+        GtfsInput gtfsInput;
+        try {
+            if (args.getInput() == null) {
+                if (Strings.isNullOrEmpty(args.getStorageDirectory())) {
+                    gtfsInput = GtfsInput.createFromUrlInMemory(new URL(args.getUrl()));
+                } else {
+                    gtfsInput = GtfsInput.createFromUrl(new URL(args.getUrl()), args.getStorageDirectory());
+                }
+            } else {
+                gtfsInput = GtfsInput.createFromPath(Paths.get(args.getInput()));
+            }
+        } catch (IOException | URISyntaxException | InterruptedException e) {
+            noticeContainer.addSystemError(
+                    new GtfsInputCreationError(e.getClass().getCanonicalName(), e.getMessage()));
+            logger.atSevere().withCause(e).log("Cannot load GTFS feed");
+            generateAndExportReports(args, noticeContainer);
+            return;
+        }
+        feedContainer = feedLoader.loadAndValidate(gtfsInput, feedName, validatorLoader, noticeContainer);
+        // Output.
+        generateAndExportReports(args, noticeContainer);
+        final long endNanos = System.nanoTime();
+        System.out.printf("Validation took %.3f seconds%n", (endNanos - startNanos) / 1e9);
+        System.out.println(feedContainer.tableTotals());
     }
 
-    final long endNanos = System.nanoTime();
-    System.out.println(
-        String.format("Validation took %.3f seconds", (endNanos - startNanos) / 1e9));
-    System.out.println(feedContainer.tableTotals());
-  }
+    /**
+     * Generates and exports reports for both validation notices and system errors reports
+     */
+    private static void generateAndExportReports(final Arguments args,
+                                                 final NoticeContainer noticeContainer) {
+        new File(args.getOutputBase()).mkdirs();
+        try {
+            Files.write(Paths.get(args.getOutputBase(), "report.json"),
+                    noticeContainer.exportValidationNotices().getBytes(StandardCharsets.UTF_8));
+            Files.write(Paths.get(args.getOutputBase(), "system_errors.json"),
+                    noticeContainer.exportSystemErrors().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            logger.atSevere().withCause(e).log("Cannot store report files");
+        }
+    }
 }
