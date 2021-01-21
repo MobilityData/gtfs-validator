ee783ccc752cd27a6df28cc5cf0b412759ce41fb
lionel-nj <lionel@mobilitydata.org>
2021-01-21 15:14:44 -0400

    test: unit test for GtfsTime

--- core/src/test/java/org/mobilitydata/gtfsvalidator/type/GtfsTimeTest.java
+++ core/src/test/java/org/mobilitydata/gtfsvalidator/type/GtfsTimeTest.java
@@ -16,28 +16,85 @@
 
 package org.mobilitydata.gtfsvalidator.type;
 
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
 @RunWith(JUnit4.class)
 public class GtfsTimeTest {
-  @Test
-  public void fromString() {
-    assertThat(GtfsTime.fromString("12:20:30").getSecondsSinceMidnight())
-        .isEqualTo(12 * 3600 + 20 * 60 + 30);
-    assertThat(GtfsTime.fromString("2:34:12").getSecondsSinceMidnight())
-        .isEqualTo(2 * 3600 + 34 * 60 + 12);
-    assertThat(GtfsTime.fromString("101:34:12").getSecondsSinceMidnight())
-        .isEqualTo(101 * 3600 + 34 * 60 + 12);
+    @Test
+    public void fromString() {
+        assertThat(GtfsTime.fromString("12:20:30").getSecondsSinceMidnight()).isEqualTo(12 * 3600 + 20 * 60 + 30);
+        assertThat(GtfsTime.fromString("2:34:12").getSecondsSinceMidnight()).isEqualTo(2 * 3600 + 34 * 60 + 12);
+        assertThat(GtfsTime.fromString("101:34:12").getSecondsSinceMidnight()).isEqualTo(101 * 3600 + 34 * 60 + 12);
 
-    assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("0"));
-    assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("qwerty"));
-    assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("midnight"));
-    assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("1234:00:12"));
-    assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("prefix4:00:12suffix"));
-  }
+        assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("0"));
+        assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("qwerty"));
+        assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("midnight"));
+        assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("1234:00:12"));
+        assertThrows(IllegalArgumentException.class, () -> GtfsTime.fromString("prefix4:00:12suffix"));
+    }
+
+    @Test
+    public void fromHourMinuteSecondShouldReturnEntityWithCorrectData() {
+        assertThat(GtfsTime.fromHourMinuteSecond(12,20,20).getSecond()).isEqualTo(20);
+        assertThat(GtfsTime.fromHourMinuteSecond(12,20,20).getMinute()).isEqualTo(20);
+        assertThat(GtfsTime.fromHourMinuteSecond(12,20,20).getHour()).isEqualTo(12);
+    }
+
+    @Test
+    public void fromSecondsSinceMidnightShouldReturnEntityWithCorrectData() {
+        assertThat(GtfsTime.fromSecondsSinceMidnight(20 * 60 + 20).getSecond()).isEqualTo(20);
+        assertThat(GtfsTime.fromSecondsSinceMidnight(20 * 60 + 20).getMinute()).isEqualTo(20);
+        assertThat(GtfsTime.fromSecondsSinceMidnight(20 * 60 + 20).getHour()).isEqualTo(0);
+    }
+
+    @Test
+    public void toHHMMSSShouldReturnFormattedRepresentationOfGtfsTime() {
+        assertThat(GtfsTime.fromHourMinuteSecond(12,20,20).toHHMMSS()).matches("12:20:20");
+        assertThat(GtfsTime.fromHourMinuteSecond(25,20,20).toHHMMSS()).matches("25:20:20");
+    }
+
+    @Test
+    public void gtfsTimeShouldBeComparable() {
+        GtfsTime firstGtfsTime = GtfsTime.fromSecondsSinceMidnight(18614);
+        GtfsTime secondGtfsTime = GtfsTime.fromSecondsSinceMidnight(20614);
+
+        assertThat(firstGtfsTime.compareTo(secondGtfsTime)).isLessThan(0);
+        assertThat(secondGtfsTime.compareTo(firstGtfsTime)).isAtLeast(1);
+        assertThat(firstGtfsTime.compareTo(firstGtfsTime)).isEqualTo(0);
+    }
+
+    @Test
+    public void firstTimeShouldBeIdentifiedAsBeforeSecondTime() {
+        GtfsTime firstGtfsTime = GtfsTime.fromSecondsSinceMidnight(18614);
+        GtfsTime secondGtfsTime = GtfsTime.fromSecondsSinceMidnight(20614);
+
+        assertThat(firstGtfsTime.isBefore(secondGtfsTime)).isTrue();
+        assertThat(firstGtfsTime.isAfter(secondGtfsTime)).isFalse();
+    }
+
+    @Test
+    public void secondTimeShouldBeIdentifiedAsAfterFirstTime() {
+        GtfsTime firstGtfsTime = GtfsTime.fromSecondsSinceMidnight(18614);
+        GtfsTime secondGtfsTime = GtfsTime.fromSecondsSinceMidnight(20614);
+
+        assertThat(secondGtfsTime.isAfter(firstGtfsTime)).isTrue();
+        assertThat(secondGtfsTime.isBefore(firstGtfsTime)).isFalse();
+    }
+
+    @Test
+    public void gtfsTimeShouldNotBeIdentifiedAsBeforeItself() {
+        GtfsTime underTest = GtfsTime.fromSecondsSinceMidnight(18614);
+        assertThat(underTest.isBefore(underTest)).isFalse();
+    }
+
+    @Test
+    public void gtfsTimeShouldNotBeIdentifiedAsAfterItself() {
+        GtfsTime underTest = GtfsTime.fromSecondsSinceMidnight(18614);
+        assertThat(underTest.isAfter(underTest)).isFalse();
+    }
 }
