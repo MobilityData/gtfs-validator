9379376f8fa4fb30622a3d23079ed290525f1e06
lionel-nj <lionel@mobilitydata.org>
2021-01-21 15:22:23 -0400

    test: unit test for TableHeaderValidator

--- core/src/test/java/org/mobilitydata/gtfsvalidator/validator/TableHeaderValidatorTest.java
+++ core/src/test/java/org/mobilitydata/gtfsvalidator/validator/TableHeaderValidatorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2020 Google LLC, MobilityData IO
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
 
 package org.mobilitydata.gtfsvalidator.validator;
 
-import static com.google.common.truth.Truth.assertThat;
-
 import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,11 +25,13 @@ import org.mobilitydata.gtfsvalidator.notice.MissingRequiredColumnError;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.UnknownColumnNotice;
 
+import static com.google.common.truth.Truth.assertThat;
+
 @RunWith(JUnit4.class)
 public class TableHeaderValidatorTest {
-  @Test
-  public void expectedColumns() {
-    NoticeContainer container = new NoticeContainer();
+    @Test
+    public void expectedColumns() {
+        NoticeContainer container = new NoticeContainer();
 
     assertThat(
             new TableHeaderValidator()
@@ -45,26 +45,20 @@ public class TableHeaderValidatorTest {
     assertThat(container.getValidationNotices().isEmpty()).isTrue();
   }
 
-  @Test
-  public void unknownColumn() {
-    NoticeContainer container = new NoticeContainer();
+    @Test
+    public void unknownColumnShouldGenerateNotice() {
+        NoticeContainer container = new NoticeContainer();
 
-    assertThat(
-            new TableHeaderValidator()
-                .validate(
-                    "stops.txt",
-                    new String[] {"stop_id", "stop_name", "stop_extra"},
-                    ImmutableSet.of("stop_id", "stop_name"),
-                    ImmutableSet.of("stop_id"),
-                    container))
-        .isTrue();
-    assertThat(container.getValidationNotices())
-        .containsExactly(new UnknownColumnNotice("stops.txt", "stop_extra", 3));
-  }
+        assertThat(new TableHeaderValidator()
+                .validate("stops.txt", new String[]{"stop_id", "stop_name", "stop_extra"},
+                        ImmutableSet.of("stop_id", "stop_name"), ImmutableSet.of("stop_id"), container)).isTrue();
+        assertThat(container.getValidationNotices()).containsExactly(
+                new UnknownColumnNotice("stops.txt", "stop_extra", 3));
+    }
 
-  @Test
-  public void missingRequiredColumn() {
-    NoticeContainer container = new NoticeContainer();
+    @Test
+    public void missingRequiredColumnShouldGenerateNotice() {
+        NoticeContainer container = new NoticeContainer();
 
     assertThat(
             new TableHeaderValidator()
@@ -79,9 +73,9 @@ public class TableHeaderValidatorTest {
         .containsExactly(new MissingRequiredColumnError("stops.txt", "stop_id"));
   }
 
-  @Test
-  public void duplicatedColumn() {
-    NoticeContainer container = new NoticeContainer();
+    @Test
+    public void duplicatedColumnShouldGenerateNotice() {
+        NoticeContainer container = new NoticeContainer();
 
     assertThat(
             new TableHeaderValidator()
@@ -96,9 +90,9 @@ public class TableHeaderValidatorTest {
         .containsExactly(new DuplicatedColumnNotice("stops.txt", "stop_id", 1, 3));
   }
 
-  @Test
-  public void emptyFile() {
-    NoticeContainer container = new NoticeContainer();
+    @Test
+    public void emptyFileShouldNotGenerateNotice() {
+        NoticeContainer container = new NoticeContainer();
 
     assertThat(
             new TableHeaderValidator()
