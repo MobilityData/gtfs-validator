c592ef7dd2abcd326a3baa5fdbc30a724d897c10
lionel-nj <lionel@mobilitydata.org>
2021-01-21 15:27:44 -0400

    test: unit test for ValidatorLoader class

---
+++ core/src/test/java/org/mobilitydata/gtfsvalidator/validator/ValidatorLoaderTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Google LLC, MobilityData IO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator.validator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//TODO: implement test for ValidatorLoader class
+@RunWith(JUnit4.class)
+public class ValidatorLoaderTest {
+    @Test
+    public void shouldReturnAllSingleEntityValidatorsForClass() {}
+
+    @Test
+    public void shouldInvokeValidateMethodOfSingleEntityValidators() {}
+
+    @Test
+    public void shouldInvokeValidateMethodOfSingleFileValidators() {}
+
+    @Test
+    public void shouldCreateMultiFileValidators() {}
+
+    @Test
+    public void createMultiFileValidatorsShouldThrowExceptionOnErrorWhenAddingValidator() {}
+
+    @Test
+    public void shouldListAllValidators() {}
+}
