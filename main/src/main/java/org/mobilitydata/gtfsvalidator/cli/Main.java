6679fefe8842e3994ce8aaefaf5c93576baabfb7
lionel-nj <lionel@mobilitydata.org>
2021-01-21 16:50:13 -0400

    style: reformat code

--- main/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
@@ -19,13 +19,6 @@ package org.mobilitydata.gtfsvalidator.cli;
 import com.beust.jcommander.JCommander;
 import com.google.common.base.Strings;
 import com.google.common.flogger.FluentLogger;
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
 import org.mobilitydata.gtfsvalidator.input.GtfsInput;
 import org.mobilitydata.gtfsvalidator.notice.GtfsInputCreationError;
@@ -34,78 +27,90 @@ import org.mobilitydata.gtfsvalidator.table.GtfsFeedContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedLoader;
 import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
 
-/** The main entry point for GTFS Validator CLI. */
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * The main entry point for GTFS Validator CLI.
+ */
 public class Main {
-    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-    public static void main(String[] argv) {
-        Arguments args = new Arguments();
-        CliParametersAnalyzer cliParametersAnalyzer = new CliParametersAnalyzer();
-        new JCommander(args).parse(argv);
-        if (!cliParametersAnalyzer.isValid(args)) {
-            System.exit(1);
-        }
-
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        GtfsFeedLoader feedLoader = new GtfsFeedLoader();
-
-        GtfsFeedName feedName = GtfsFeedName.parseString(args.getFeedName());
-        System.out.println("Feed name: " + feedName.getCountryFirstName());
-        System.out.println("Input: " + args.getInput());
-        System.out.println("URL: " + args.getUrl());
-        System.out.println("Output: " + args.getOutputBase());
-        System.out.println("Path to archive storage directory: " + args.getStorageDirectory());
-        System.out.println("Table loaders: " + feedLoader.listTableLoaders());
-        System.out.println("Validators:");
-        System.out.println(validatorLoader.listValidators());
-
-        final long startNanos = System.nanoTime();
-        // Input.
-        feedLoader.setNumThreads(args.getNumThreads());
-        NoticeContainer noticeContainer = new NoticeContainer();
-        GtfsFeedContainer feedContainer;
-        GtfsInput gtfsInput;
-        try {
-            if (args.getInput() == null) {
-                if (Strings.isNullOrEmpty(args.getStorageDirectory())) {
-                    gtfsInput = GtfsInput.createFromUrlInMemory(new URL(args.getUrl()));
-                } else {
-                    gtfsInput = GtfsInput.createFromUrl(new URL(args.getUrl()), args.getStorageDirectory());
-                }
-            } else {
-                gtfsInput = GtfsInput.createFromPath(Paths.get(args.getInput()));
-            }
-        } catch (IOException | URISyntaxException | InterruptedException e) {
-            noticeContainer.addSystemError(
-                    new GtfsInputCreationError(e.getClass().getCanonicalName(), e.getMessage()));
-            logger.atSevere().withCause(e).log("Cannot load GTFS feed");
-            generateAndExportReports(args, noticeContainer);
-            return;
-        }
-        feedContainer =
-                feedLoader.loadAndValidate(gtfsInput, feedName, validatorLoader, noticeContainer);
-
-        // Output.
-        generateAndExportReports(args, noticeContainer);
-        final long endNanos = System.nanoTime();
-        System.out.println(
-                String.format("Validation took %.3f seconds", (endNanos - startNanos) / 1e9));
-        System.out.println(feedContainer.tableTotals());
+  public static void main(String[] argv) {
+    Arguments args = new Arguments();
+    CliParametersAnalyzer cliParametersAnalyzer = new CliParametersAnalyzer();
+    new JCommander(args).parse(argv);
+    if (!cliParametersAnalyzer.isValid(args)) {
+      System.exit(1);
     }
 
-    /**
-     * Generates and exports reports for both validation notices and system errors reports
-     */
-    private static void generateAndExportReports(final Arguments args,
-                                                 final NoticeContainer noticeContainer) {
-        new File(args.getOutputBase()).mkdirs();
-        try {
-            Files.write(Paths.get(args.getOutputBase(), "report.json"),
-                    noticeContainer.exportValidationNotices().getBytes(StandardCharsets.UTF_8));
-            Files.write(Paths.get(args.getOutputBase(), "system_errors.json"),
-                    noticeContainer.exportSystemErrors().getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            logger.atSevere().withCause(e).log("Cannot store report files");
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    GtfsFeedLoader feedLoader = new GtfsFeedLoader();
+
+    GtfsFeedName feedName = GtfsFeedName.parseString(args.getFeedName());
+    System.out.println("Feed name: " + feedName.getCountryFirstName());
+    System.out.println("Input: " + args.getInput());
+    System.out.println("URL: " + args.getUrl());
+    System.out.println("Output: " + args.getOutputBase());
+    System.out.println("Path to archive storage directory: " + args.getStorageDirectory());
+    System.out.println("Table loaders: " + feedLoader.listTableLoaders());
+    System.out.println("Validators:");
+    System.out.println(validatorLoader.listValidators());
+
+    final long startNanos = System.nanoTime();
+    // Input.
+    feedLoader.setNumThreads(args.getNumThreads());
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsFeedContainer feedContainer;
+    GtfsInput gtfsInput;
+    try {
+      if (args.getInput() == null) {
+        if (Strings.isNullOrEmpty(args.getStorageDirectory())) {
+          gtfsInput = GtfsInput.createFromUrlInMemory(new URL(args.getUrl()));
+        } else {
+          gtfsInput = GtfsInput.createFromUrl(new URL(args.getUrl()), args.getStorageDirectory());
         }
+      } else {
+        gtfsInput = GtfsInput.createFromPath(Paths.get(args.getInput()));
+      }
+    } catch (IOException | URISyntaxException | InterruptedException e) {
+      noticeContainer.addSystemError(
+              new GtfsInputCreationError(e.getClass().getCanonicalName(), e.getMessage()));
+      logger.atSevere().withCause(e).log("Cannot load GTFS feed");
+      generateAndExportReports(args, noticeContainer);
+      return;
     }
+    feedContainer =
+            feedLoader.loadAndValidate(gtfsInput, feedName, validatorLoader, noticeContainer);
+
+    // Output.
+    generateAndExportReports(args, noticeContainer);
+    final long endNanos = System.nanoTime();
+    System.out.println(
+            String.format("Validation took %.3f seconds", (endNanos - startNanos) / 1e9));
+    System.out.println(feedContainer.tableTotals());
+  }
+
+  /**
+   * Generates and exports reports for both validation notices and system errors reports
+   */
+  private static void generateAndExportReports(
+          final Arguments args, final NoticeContainer noticeContainer) {
+    new File(args.getOutputBase()).mkdirs();
+    try {
+      Files.write(
+              Paths.get(args.getOutputBase(), "report.json"),
+              noticeContainer.exportValidationNotices().getBytes(StandardCharsets.UTF_8));
+      Files.write(
+              Paths.get(args.getOutputBase(), "system_errors.json"),
+              noticeContainer.exportSystemErrors().getBytes(StandardCharsets.UTF_8));
+    } catch (IOException e) {
+      logger.atSevere().withCause(e).log("Cannot store report files");
+    }
+  }
 }
--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/BlockTripsWithOverlappingStopTimesValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/BlockTripsWithOverlappingStopTimesValidatorTest.java
@@ -1,227 +1,223 @@
 package org.mobilitydata.gtfsvalidator.validator;
 
-import static com.google.common.truth.Truth.assertThat;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Set;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mobilitydata.gtfsvalidator.notice.BlockTripsWithOverlappingStopTimesNotice;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
-import org.mobilitydata.gtfsvalidator.table.GtfsCalendarDateTableContainer;
-import org.mobilitydata.gtfsvalidator.table.GtfsCalendarTableContainer;
-import org.mobilitydata.gtfsvalidator.table.GtfsStopTime;
-import org.mobilitydata.gtfsvalidator.table.GtfsStopTimeTableContainer;
-import org.mobilitydata.gtfsvalidator.table.GtfsTrip;
-import org.mobilitydata.gtfsvalidator.table.GtfsTripTableContainer;
+import org.mobilitydata.gtfsvalidator.table.*;
 import org.mobilitydata.gtfsvalidator.type.GtfsDate;
 import org.mobilitydata.gtfsvalidator.type.GtfsTime;
 import org.mobilitydata.gtfsvalidator.util.CalendarUtilTest;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Set;
+
+import static com.google.common.truth.Truth.assertThat;
+
 @RunWith(JUnit4.class)
 public class BlockTripsWithOverlappingStopTimesValidatorTest {
 
-    private static GtfsCalendarTableContainer createCalendarTable(NoticeContainer noticeContainer) {
-        final Set<DayOfWeek> weekDays =
-                ImmutableSet.of(
-                        DayOfWeek.MONDAY,
-                        DayOfWeek.TUESDAY,
-                        DayOfWeek.WEDNESDAY,
-                        DayOfWeek.THURSDAY,
-                        DayOfWeek.FRIDAY);
-        return GtfsCalendarTableContainer.forEntities(
-                ImmutableList.of(
-                        CalendarUtilTest.createGtfsCalendar(
-                                "WEEK", LocalDate.of(2021, 1, 4), LocalDate.of(2021, 4, 4), weekDays),
-                        CalendarUtilTest.createGtfsCalendar(
-                                "WEEK-ALT", LocalDate.of(2021, 1, 4), LocalDate.of(2021, 4, 4), weekDays),
-                        CalendarUtilTest.createGtfsCalendar(
-                                "SAT",
-                                LocalDate.of(2021, 1, 4),
-                                LocalDate.of(2021, 4, 4),
-                                ImmutableSet.of(DayOfWeek.SATURDAY)),
-                        CalendarUtilTest.createGtfsCalendar(
-                                "SUN",
-                                LocalDate.of(2021, 1, 4),
-                                LocalDate.of(2021, 4, 4),
-                                ImmutableSet.of(DayOfWeek.SUNDAY))),
-                noticeContainer);
+  private static GtfsCalendarTableContainer createCalendarTable(NoticeContainer noticeContainer) {
+    final Set<DayOfWeek> weekDays =
+            ImmutableSet.of(
+                    DayOfWeek.MONDAY,
+                    DayOfWeek.TUESDAY,
+                    DayOfWeek.WEDNESDAY,
+                    DayOfWeek.THURSDAY,
+                    DayOfWeek.FRIDAY);
+    return GtfsCalendarTableContainer.forEntities(
+            ImmutableList.of(
+                    CalendarUtilTest.createGtfsCalendar(
+                            "WEEK", LocalDate.of(2021, 1, 4), LocalDate.of(2021, 4, 4), weekDays),
+                    CalendarUtilTest.createGtfsCalendar(
+                            "WEEK-ALT", LocalDate.of(2021, 1, 4), LocalDate.of(2021, 4, 4), weekDays),
+                    CalendarUtilTest.createGtfsCalendar(
+                            "SAT",
+                            LocalDate.of(2021, 1, 4),
+                            LocalDate.of(2021, 4, 4),
+                            ImmutableSet.of(DayOfWeek.SATURDAY)),
+                    CalendarUtilTest.createGtfsCalendar(
+                            "SUN",
+                            LocalDate.of(2021, 1, 4),
+                            LocalDate.of(2021, 4, 4),
+                            ImmutableSet.of(DayOfWeek.SUNDAY))),
+            noticeContainer);
+  }
+
+  public static GtfsStopTime createStopTime(
+          long csvRowNumber, String tripId, String time, String stopId, int stopSequence) {
+    return new GtfsStopTime.Builder()
+            .setCsvRowNumber(csvRowNumber)
+            .setTripId(tripId)
+            .setArrivalTime(GtfsTime.fromString(time))
+            .setDepartureTime(GtfsTime.fromString(time))
+            .setStopSequence(stopSequence)
+            .setStopId(stopId)
+            .build();
+  }
+
+  public static GtfsTrip createTrip(
+          long csvRowNumber, String tripId, String serviceId, String blockId) {
+    return new GtfsTrip.Builder()
+            .setCsvRowNumber(csvRowNumber)
+            .setTripId(tripId)
+            .setServiceId(serviceId)
+            .setBlockId(blockId)
+            .build();
+  }
+
+  private static GtfsTripTableContainer createTripTable(
+          String[] tripIds, String[] serviceIds, String blockId, NoticeContainer noticeContainer) {
+    Preconditions.checkArgument(
+            tripIds.length == serviceIds.length, "tripIds.length must be equal to serviceIds.length");
+
+    ArrayList<GtfsTrip> trips = new ArrayList<>();
+    trips.ensureCapacity(tripIds.length);
+    for (int i = 0; i < tripIds.length; ++i) {
+      trips.add(createTrip(trips.size() + 1, tripIds[i], serviceIds[i], blockId));
     }
 
-    public static GtfsStopTime createStopTime(
-            long csvRowNumber, String tripId, String time, String stopId, int stopSequence) {
-        return new GtfsStopTime.Builder()
-                .setCsvRowNumber(csvRowNumber)
-                .setTripId(tripId)
-                .setArrivalTime(GtfsTime.fromString(time))
-                .setDepartureTime(GtfsTime.fromString(time))
-                .setStopSequence(stopSequence)
-                .setStopId(stopId)
-                .build();
+    return GtfsTripTableContainer.forEntities(trips, noticeContainer);
+  }
+
+  private static GtfsStopTimeTableContainer createStopTimeTable(
+          String[] tripIds, String[] stopIds, String[][] times, NoticeContainer noticeContainer) {
+    Preconditions.checkArgument(
+            tripIds.length == times.length, "tripIds.length must be equal to times.length");
+
+    ArrayList<GtfsStopTime> stopTimes = new ArrayList<>();
+    stopTimes.ensureCapacity(tripIds.length * stopIds.length);
+    for (int i = 0; i < tripIds.length; ++i) {
+      Preconditions.checkArgument(
+              stopIds.length == times[i].length, "stopIds.length must be equal to times[%d].length", i);
+      for (int j = 0; j < stopIds.length; ++j) {
+        stopTimes.add(createStopTime(stopTimes.size() + 1, tripIds[i], times[i][j], stopIds[j], j));
+      }
     }
 
-    public static GtfsTrip createTrip(
-            long csvRowNumber, String tripId, String serviceId, String blockId) {
-        return new GtfsTrip.Builder()
-                .setCsvRowNumber(csvRowNumber)
-                .setTripId(tripId)
-                .setServiceId(serviceId)
-                .setBlockId(blockId)
-                .build();
-    }
+    return GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
+  }
 
-    private static GtfsTripTableContainer createTripTable(
-            String[] tripIds, String[] serviceIds, String blockId, NoticeContainer noticeContainer) {
-        Preconditions.checkArgument(
-                tripIds.length == serviceIds.length, "tripIds.length must be equal to serviceIds.length");
+  @Test
+  public void goodFeed() {
+    final NoticeContainer noticeContainer = new NoticeContainer();
 
-        ArrayList<GtfsTrip> trips = new ArrayList<>();
-        trips.ensureCapacity(tripIds.length);
-        for (int i = 0; i < tripIds.length; ++i) {
-            trips.add(createTrip(trips.size() + 1, tripIds[i], serviceIds[i], blockId));
-        }
+    BlockTripsWithOverlappingStopTimesValidator validator =
+            new BlockTripsWithOverlappingStopTimesValidator();
+    validator.calendarTable = createCalendarTable(noticeContainer);
+    validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
+    validator.tripTable =
+            createTripTable(
+                    new String[]{"t0", "t1", "t2", "t3", "t4", "t5"},
+                    new String[]{"WEEK", "WEEK", "WEEK", "SAT", "SAT", "SAT"},
+                    "b1",
+                    noticeContainer);
+    validator.stopTimeTable =
+            createStopTimeTable(
+                    new String[]{"t0", "t1", "t2", "t3", "t4", "t5"},
+                    new String[]{"s0", "s1"},
+                    new String[][]{
+                            new String[]{"08:00:00", "09:00:00"},
+                            new String[]{"10:00:00", "11:00:00"},
+                            new String[]{"12:00:00", "13:00:00"},
+                            new String[]{"08:00:00", "09:00:00"},
+                            new String[]{"10:00:00", "11:00:00"},
+                            new String[]{"12:00:00", "13:00:00"},
+                    },
+                    noticeContainer);
 
-        return GtfsTripTableContainer.forEntities(trips, noticeContainer);
-    }
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices()).isEmpty();
+  }
 
-    private static GtfsStopTimeTableContainer createStopTimeTable(
-            String[] tripIds, String[] stopIds, String[][] times, NoticeContainer noticeContainer) {
-        Preconditions.checkArgument(
-                tripIds.length == times.length, "tripIds.length must be equal to times.length");
+  @Test
+  public void overlapWithSameServiceId() {
+    final NoticeContainer noticeContainer = new NoticeContainer();
 
-        ArrayList<GtfsStopTime> stopTimes = new ArrayList<>();
-        stopTimes.ensureCapacity(tripIds.length * stopIds.length);
-        for (int i = 0; i < tripIds.length; ++i) {
-            Preconditions.checkArgument(
-                    stopIds.length == times[i].length, "stopIds.length must be equal to times[%d].length", i);
-            for (int j = 0; j < stopIds.length; ++j) {
-                stopTimes.add(createStopTime(stopTimes.size() + 1, tripIds[i], times[i][j], stopIds[j], j));
-            }
-        }
+    BlockTripsWithOverlappingStopTimesValidator validator =
+            new BlockTripsWithOverlappingStopTimesValidator();
+    validator.calendarTable = createCalendarTable(noticeContainer);
+    validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
+    validator.tripTable =
+            createTripTable(
+                    new String[]{"t0", "t1", "t2"},
+                    new String[]{"WEEK", "WEEK", "WEEK"},
+                    "b1",
+                    noticeContainer);
+    validator.stopTimeTable =
+            createStopTimeTable(
+                    new String[]{"t0", "t1", "t2"},
+                    new String[]{"s0", "s1"},
+                    new String[][]{
+                            new String[]{"08:00:00", "09:00:00"},
+                            new String[]{"08:30:00", "09:30:00"},
+                            new String[]{"12:00:00", "13:00:00"},
+                    },
+                    noticeContainer);
 
-        return GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer);
-    }
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices())
+            .containsExactly(
+                    new BlockTripsWithOverlappingStopTimesNotice(
+                            1, "t0", "WEEK", 2, "t1", "WEEK", "b1", GtfsDate.fromString("20210104")));
+  }
 
-    @Test
-    public void goodFeed() {
-        final NoticeContainer noticeContainer = new NoticeContainer();
+  @Test
+  public void overlapWithDifferentServiceId() {
+    final NoticeContainer noticeContainer = new NoticeContainer();
 
-        BlockTripsWithOverlappingStopTimesValidator validator =
-                new BlockTripsWithOverlappingStopTimesValidator();
-        validator.calendarTable = createCalendarTable(noticeContainer);
-        validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
-        validator.tripTable =
-                createTripTable(
-                        new String[] {"t0", "t1", "t2", "t3", "t4", "t5"},
-                        new String[] {"WEEK", "WEEK", "WEEK", "SAT", "SAT", "SAT"},
-                        "b1",
-                        noticeContainer);
-        validator.stopTimeTable =
-                createStopTimeTable(
-                        new String[] {"t0", "t1", "t2", "t3", "t4", "t5"},
-                        new String[] {"s0", "s1"},
-                        new String[][] {
-                                new String[] {"08:00:00", "09:00:00"},
-                                new String[] {"10:00:00", "11:00:00"},
-                                new String[] {"12:00:00", "13:00:00"},
-                                new String[] {"08:00:00", "09:00:00"},
-                                new String[] {"10:00:00", "11:00:00"},
-                                new String[] {"12:00:00", "13:00:00"},
-                        },
-                        noticeContainer);
+    BlockTripsWithOverlappingStopTimesValidator validator =
+            new BlockTripsWithOverlappingStopTimesValidator();
+    validator.calendarTable = createCalendarTable(noticeContainer);
+    validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
+    validator.tripTable =
+            createTripTable(
+                    new String[]{"t0", "t1"}, new String[]{"WEEK", "WEEK-ALT"}, "b1", noticeContainer);
+    validator.stopTimeTable =
+            createStopTimeTable(
+                    new String[]{"t0", "t1"},
+                    new String[]{"s0", "s1"},
+                    new String[][]{
+                            new String[]{"08:00:00", "09:00:00"},
+                            new String[]{"08:30:00", "09:30:00"},
+                    },
+                    noticeContainer);
 
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getValidationNotices()).isEmpty();
-    }
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices())
+            .containsExactly(
+                    new BlockTripsWithOverlappingStopTimesNotice(
+                            1, "t0", "WEEK", 2, "t1", "WEEK-ALT", "b1", GtfsDate.fromString("20210104")));
+  }
 
-    @Test
-    public void overlapWithSameServiceId() {
-        final NoticeContainer noticeContainer = new NoticeContainer();
+  @Test
+  public void tripsWith0or1Stop() {
+    // Trips with 0 or 1 stop are not useful to end users and should be
+    // gracefully handled by this validator.
+    final NoticeContainer noticeContainer = new NoticeContainer();
 
-        BlockTripsWithOverlappingStopTimesValidator validator =
-                new BlockTripsWithOverlappingStopTimesValidator();
-        validator.calendarTable = createCalendarTable(noticeContainer);
-        validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
-        validator.tripTable =
-                createTripTable(
-                        new String[] {"t0", "t1", "t2"},
-                        new String[] {"WEEK", "WEEK", "WEEK"},
-                        "b1",
-                        noticeContainer);
-        validator.stopTimeTable =
-                createStopTimeTable(
-                        new String[] {"t0", "t1", "t2"},
-                        new String[] {"s0", "s1"},
-                        new String[][] {
-                                new String[] {"08:00:00", "09:00:00"},
-                                new String[] {"08:30:00", "09:30:00"},
-                                new String[] {"12:00:00", "13:00:00"},
-                        },
-                        noticeContainer);
+    BlockTripsWithOverlappingStopTimesValidator validator =
+            new BlockTripsWithOverlappingStopTimesValidator();
+    validator.calendarTable = createCalendarTable(noticeContainer);
+    validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
+    validator.tripTable =
+            createTripTable(
+                    new String[]{"t0", "t1"}, new String[]{"WEEK", "WEEK"}, "b1", noticeContainer);
+    // t0 has 1 stop (s0), t1 has no stops at all.
+    validator.stopTimeTable =
+            createStopTimeTable(
+                    new String[]{"t0"},
+                    new String[]{"s0"},
+                    new String[][]{new String[]{"08:00:00"}},
+                    noticeContainer);
 
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getValidationNotices())
-                .containsExactly(
-                        new BlockTripsWithOverlappingStopTimesNotice(
-                                1, "t0", "WEEK", 2, "t1", "WEEK", "b1", GtfsDate.fromString("20210104")));
-    }
-
-    @Test
-    public void overlapWithDifferentServiceId() {
-        final NoticeContainer noticeContainer = new NoticeContainer();
-
-        BlockTripsWithOverlappingStopTimesValidator validator =
-                new BlockTripsWithOverlappingStopTimesValidator();
-        validator.calendarTable = createCalendarTable(noticeContainer);
-        validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
-        validator.tripTable =
-                createTripTable(
-                        new String[] {"t0", "t1"}, new String[] {"WEEK", "WEEK-ALT"}, "b1", noticeContainer);
-        validator.stopTimeTable =
-                createStopTimeTable(
-                        new String[] {"t0", "t1"},
-                        new String[] {"s0", "s1"},
-                        new String[][] {
-                                new String[] {"08:00:00", "09:00:00"},
-                                new String[] {"08:30:00", "09:30:00"},
-                        },
-                        noticeContainer);
-
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getValidationNotices())
-                .containsExactly(
-                        new BlockTripsWithOverlappingStopTimesNotice(
-                                1, "t0", "WEEK", 2, "t1", "WEEK-ALT", "b1", GtfsDate.fromString("20210104")));
-    }
-
-    @Test
-    public void tripsWith0or1Stop() {
-        // Trips with 0 or 1 stop are not useful to end users and should be
-        // gracefully handled by this validator.
-        final NoticeContainer noticeContainer = new NoticeContainer();
-
-        BlockTripsWithOverlappingStopTimesValidator validator =
-                new BlockTripsWithOverlappingStopTimesValidator();
-        validator.calendarTable = createCalendarTable(noticeContainer);
-        validator.calendarDateTable = GtfsCalendarDateTableContainer.forMissingFile();
-        validator.tripTable =
-                createTripTable(
-                        new String[] {"t0", "t1"}, new String[] {"WEEK", "WEEK"}, "b1", noticeContainer);
-        // t0 has 1 stop (s0), t1 has no stops at all.
-        validator.stopTimeTable =
-                createStopTimeTable(
-                        new String[] {"t0"},
-                        new String[] {"s0"},
-                        new String[][] {new String[] {"08:00:00"}},
-                        noticeContainer);
-
-        validator.validate(noticeContainer);
-        assertThat(noticeContainer.getValidationNotices()).isEmpty();
-    }
+    validator.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices()).isEmpty();
+  }
 }
