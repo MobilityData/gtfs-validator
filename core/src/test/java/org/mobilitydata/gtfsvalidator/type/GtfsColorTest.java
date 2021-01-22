9fd1791be5340062dd98e8242388857434a92899
lionel-nj <lionel@mobilitydata.org>
2021-01-22 11:07:55 -0400

    style: reformat code

--- core/src/test/java/org/mobilitydata/gtfsvalidator/type/GtfsColorTest.java
+++ core/src/test/java/org/mobilitydata/gtfsvalidator/type/GtfsColorTest.java
@@ -16,12 +16,12 @@
 
 package org.mobilitydata.gtfsvalidator.type;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
 
 @RunWith(JUnit4.class)
 public class GtfsColorTest {
