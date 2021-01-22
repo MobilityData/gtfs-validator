4785b53cd5ee5bf69c8ba17fd26bbd64e93aba4c
lionel-nj <lionel@mobilitydata.org>
2021-01-22 15:06:44 -0400

    reorganize code to avoid mocking objects

--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/FeedServiceDateValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/FeedServiceDateValidatorTest.java
@@ -1,150 +1,133 @@
 package org.mobilitydata.gtfsvalidator.validator;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-import java.util.ArrayList;
+import com.google.common.collect.ImmutableList;
 import java.util.List;
-import org.junit.Before;
+import java.util.Locale;
 import org.junit.Test;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.StartAndEndDateOutOfOrderNotice;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfo;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfoTableContainer;
 import org.mobilitydata.gtfsvalidator.type.GtfsDate;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 public class FeedServiceDateValidatorTest {
-  @Mock final GtfsFeedInfoTableContainer mockFeedInfoTable = mock(GtfsFeedInfoTableContainer.class);
-  @InjectMocks final FeedServiceDateValidator underTest = new FeedServiceDateValidator();
 
-  @Before
-  public void openMocks() {
-    MockitoAnnotations.openMocks(this);
+  private static GtfsFeedInfoTableContainer createFeedInfoTable(
+      NoticeContainer noticeContainer, List<GtfsFeedInfo> entities) {
+    return GtfsFeedInfoTableContainer.forEntities(entities, noticeContainer);
+  }
+
+  public static GtfsFeedInfo createFeedInfo(
+      long csvRowNumber,
+      String feedPublisherName,
+      String feedPublisherUrl,
+      Locale feedLang,
+      GtfsDate feedStartDate,
+      GtfsDate feedEndDate) {
+    return new GtfsFeedInfo.Builder()
+        .setCsvRowNumber(csvRowNumber)
+        .setFeedPublisherName(feedPublisherName)
+        .setFeedPublisherUrl(feedPublisherUrl)
+        .setFeedLang(feedLang)
+        .setDefaultLang(null)
+        .setFeedStartDate(feedStartDate)
+        .setFeedEndDate(feedEndDate)
+        .setFeedVersion(null)
+        .setFeedContactEmail(null)
+        .setFeedContactUrl(null)
+        .build();
   }
 
   @Test
   public void startDateBeforeEndDateShouldNotGenerateNotice() {
-    NoticeContainer mockNoticeContainer = mock(NoticeContainer.class);
-    List<GtfsFeedInfo> feedInfoCollection = new ArrayList<>();
-    GtfsFeedInfo mockFeedInfo = mock(GtfsFeedInfo.class);
-    when(mockFeedInfo.hasFeedEndDate()).thenReturn(true);
-    when(mockFeedInfo.hasFeedStartDate()).thenReturn(true);
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
+        createFeedInfoTable(
+            noticeContainer,
+            ImmutableList.of(
+                createFeedInfo(
+                    1,
+                    "name value",
+                    "url value",
+                    Locale.CANADA,
+                    GtfsDate.fromEpochDay(340),
+                    GtfsDate.fromEpochDay(450))));
 
-    GtfsDate mockStartDate = mock(GtfsDate.class);
-    GtfsDate mockEndDate = mock(GtfsDate.class);
+    FeedServiceDateValidator underTest = new FeedServiceDateValidator();
+    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    when(mockStartDate.isAfter(mockEndDate)).thenReturn(false);
-    when(mockFeedInfo.feedStartDate()).thenReturn(mockStartDate);
-    when(mockFeedInfo.feedEndDate()).thenReturn(mockEndDate);
-    feedInfoCollection.add(mockFeedInfo);
-    when(mockFeedInfoTable.getEntities()).thenReturn(feedInfoCollection);
-
-    underTest.validate(mockNoticeContainer);
-
-    verifyNoInteractions(mockNoticeContainer);
-    verify(mockFeedInfo, times(1)).hasFeedEndDate();
-    verify(mockFeedInfo, times(1)).feedEndDate();
-    verify(mockFeedInfo, times(1)).hasFeedStartDate();
-    verify(mockFeedInfo, times(1)).feedStartDate();
-    verify(mockStartDate, times(1)).isAfter(mockEndDate);
-    verifyNoMoreInteractions(mockFeedInfo, mockEndDate, mockStartDate);
+    underTest.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices().isEmpty());
   }
 
   @Test
   public void startDateAfterEndDateShouldGenerateNotice() {
-    NoticeContainer mockNoticeContainer = mock(NoticeContainer.class);
-    List<GtfsFeedInfo> feedInfoCollection = new ArrayList<>();
-    GtfsFeedInfo mockFeedInfo = mock(GtfsFeedInfo.class);
-    when(mockFeedInfo.hasFeedEndDate()).thenReturn(true);
-    when(mockFeedInfo.hasFeedStartDate()).thenReturn(true);
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
+        createFeedInfoTable(
+            noticeContainer,
+            ImmutableList.of(
+                createFeedInfo(
+                    1,
+                    "name value",
+                    "url value",
+                    Locale.CANADA,
+                    GtfsDate.fromEpochDay(450),
+                    GtfsDate.fromEpochDay(340))));
 
-    GtfsDate mockStartDate = mock(GtfsDate.class);
-    GtfsDate mockEndDate = mock(GtfsDate.class);
+    FeedServiceDateValidator underTest = new FeedServiceDateValidator();
+    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    when(mockStartDate.isAfter(mockEndDate)).thenReturn(true);
-    when(mockFeedInfo.feedStartDate()).thenReturn(mockStartDate);
-    when(mockStartDate.toYYYYMMDD()).thenReturn("start date value");
-    when(mockEndDate.toYYYYMMDD()).thenReturn("end date value");
-    when(mockFeedInfo.feedEndDate()).thenReturn(mockEndDate);
-    when(mockFeedInfo.csvRowNumber()).thenReturn(2L);
-    feedInfoCollection.add(mockFeedInfo);
-    when(mockFeedInfoTable.getEntities()).thenReturn(feedInfoCollection);
-    when(mockFeedInfoTable.gtfsFilename()).thenReturn("feed_info.txt");
-
-    underTest.validate(mockNoticeContainer);
-
-    final ArgumentCaptor<StartAndEndDateOutOfOrderNotice> captor =
-        ArgumentCaptor.forClass(StartAndEndDateOutOfOrderNotice.class);
-
-    verify(mockNoticeContainer, times(1)).addValidationNotice(captor.capture());
-    StartAndEndDateOutOfOrderNotice notice = captor.getValue();
-
-    assertThat(notice.getCode()).matches("start_and_end_date_out_of_order");
-    assertThat(notice.getContext()).containsEntry("filename", "feed_info.txt");
-    assertThat(notice.getContext()).containsEntry("csvRowNumber", 2L);
-    assertThat(notice.getContext()).containsEntry("startDate", "start date value");
-    assertThat(notice.getContext()).containsEntry("endDate", "end date value");
-
-    verify(mockFeedInfo, times(1)).hasFeedEndDate();
-    verify(mockFeedInfo, times(1)).hasFeedStartDate();
-    verify(mockFeedInfoTable, times(1)).gtfsFilename();
-    verify(mockFeedInfo, times(1)).csvRowNumber();
-    verify(mockFeedInfo, times(2)).feedEndDate();
-    verify(mockFeedInfo, times(2)).feedStartDate();
-    verify(mockStartDate, times(1)).isAfter(mockEndDate);
-    verify(mockStartDate, times(1)).toYYYYMMDD();
-    verify(mockEndDate, times(1)).toYYYYMMDD();
-    verifyNoMoreInteractions(mockFeedInfo, mockEndDate, mockStartDate);
+    underTest.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices())
+        .containsExactly(
+            new StartAndEndDateOutOfOrderNotice(
+                "feed_info.txt", 1L, GtfsDate.fromEpochDay(450), GtfsDate.fromEpochDay(340)));
   }
 
   @Test
   public void noStartDateShouldNotGenerateNotice() {
-    NoticeContainer mockNoticeContainer = mock(NoticeContainer.class);
-    List<GtfsFeedInfo> feedInfoCollection = new ArrayList<>();
-    GtfsFeedInfo mockFeedInfo = mock(GtfsFeedInfo.class);
-    when(mockFeedInfo.hasFeedEndDate()).thenReturn(true);
-    when(mockFeedInfo.hasFeedStartDate()).thenReturn(false);
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
+        createFeedInfoTable(
+            noticeContainer,
+            ImmutableList.of(
+                createFeedInfo(
+                    1,
+                    "name value",
+                    "url value",
+                    Locale.CANADA,
+                    null,
+                    GtfsDate.fromEpochDay(450))));
 
-    GtfsDate mockStartDate = mock(GtfsDate.class);
-    GtfsDate mockEndDate = mock(GtfsDate.class);
+    FeedServiceDateValidator underTest = new FeedServiceDateValidator();
+    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    when(mockStartDate.isAfter(mockEndDate)).thenReturn(false);
-    when(mockFeedInfo.feedEndDate()).thenReturn(mockEndDate);
-    feedInfoCollection.add(mockFeedInfo);
-    when(mockFeedInfoTable.getEntities()).thenReturn(feedInfoCollection);
-
-    underTest.validate(mockNoticeContainer);
-
-    verifyNoInteractions(mockNoticeContainer);
-    verify(mockFeedInfo, times(1)).hasFeedStartDate();
-    verifyNoMoreInteractions(mockFeedInfo, mockEndDate, mockStartDate);
+    underTest.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices().isEmpty());
   }
 
   @Test
   public void noEndDateShouldNotGenerateNotice() {
-    NoticeContainer mockNoticeContainer = mock(NoticeContainer.class);
-    List<GtfsFeedInfo> feedInfoCollection = new ArrayList<>();
-    GtfsFeedInfo mockFeedInfo = mock(GtfsFeedInfo.class);
-    when(mockFeedInfo.hasFeedEndDate()).thenReturn(false);
-    when(mockFeedInfo.hasFeedStartDate()).thenReturn(true);
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
+        createFeedInfoTable(
+            noticeContainer,
+            ImmutableList.of(
+                createFeedInfo(
+                    1,
+                    "name value",
+                    "url value",
+                    Locale.CANADA,
+                    GtfsDate.fromEpochDay(450),
+                    null)));
 
-    GtfsDate mockStartDate = mock(GtfsDate.class);
-    GtfsDate mockEndDate = mock(GtfsDate.class);
+    FeedServiceDateValidator underTest = new FeedServiceDateValidator();
+    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    when(mockStartDate.isAfter(mockEndDate)).thenReturn(false);
-    feedInfoCollection.add(mockFeedInfo);
-    when(mockFeedInfoTable.getEntities()).thenReturn(feedInfoCollection);
-
-    underTest.validate(mockNoticeContainer);
-
-    verifyNoInteractions(mockNoticeContainer);
-    verify(mockFeedInfo, times(1)).hasFeedEndDate();
-    verify(mockFeedInfo, times(1)).hasFeedStartDate();
-    verifyNoMoreInteractions(mockFeedInfo, mockEndDate, mockStartDate);
+    underTest.validate(noticeContainer);
+    assertThat(noticeContainer.getValidationNotices().isEmpty());
   }
 }
