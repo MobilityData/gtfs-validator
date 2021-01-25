2035f6fe691f2539447a608972bc1a9287532fa5
lionel-nj <lionel@mobilitydata.org>
2021-01-25 11:49:17 -0400

    test: GtfsRouteTableLoader class

--- main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsRouteTableLoaderTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsRouteTableLoaderTest.java
@@ -16,88 +16,84 @@
 
 package org.mobilitydata.gtfsvalidator.table;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
-import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
-import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
+import static com.google.common.truth.Truth.assertThat;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.math.BigDecimal;
-import java.util.Currency;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
+import org.mobilitydata.gtfsvalidator.notice.EmptyFileNotice;
+import org.mobilitydata.gtfsvalidator.notice.MissingRequiredFieldError;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
 
-import static com.google.common.truth.Truth.assertThat;
-
-/**
- * Runs GtfsRouteTableLoader on test CSV data.
- */
+/** Runs GtfsRouteTableLoader on test CSV data. */
 @RunWith(JUnit4.class)
 public class GtfsRouteTableLoaderTest {
-    private static final GtfsFeedName FEED_NAME = GtfsFeedName.parseString("au-sydney-buses");
+  private static final GtfsFeedName FEED_NAME = GtfsFeedName.parseString("au-sydney-buses");
 
-    @Test
-    public void validFileShouldNotGenerateNotice() throws IOException {
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        Reader reader =
-                new StringReader(
-                        "route_id,agency_id,route_short_name,route_long_name,route_type"
-                                + System.lineSeparator() +
-                                "route id value,agency id value,short name,long name,2");
-        GtfsRouteTableLoader loader = new GtfsRouteTableLoader();
-        NoticeContainer noticeContainer = new NoticeContainer();
-        GtfsRouteTableContainer tableContainer =
-                (GtfsRouteTableContainer) loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+  @Test
+  public void validFileShouldNotGenerateNotice() throws IOException {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    Reader reader =
+        new StringReader(
+            "route_id,agency_id,route_short_name,route_long_name,route_type"
+                + System.lineSeparator()
+                + "route id value,agency id value,short name,long name,2");
+    GtfsRouteTableLoader loader = new GtfsRouteTableLoader();
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsRouteTableContainer tableContainer =
+        (GtfsRouteTableContainer) loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    reader.close();
 
-        assertThat(noticeContainer.getNotices()).isEmpty();
-        assertThat(tableContainer.entityCount()).isEqualTo(1);
-        GtfsRoute route = tableContainer.byRouteId("route id value");
-        assertThat(route).isNotNull();
-        assertThat(route.routeId()).matches("route id value");
-        assertThat(route.agencyId()).matches("agency id value");
-        assertThat(route.routeShortName()).matches("short name");
-        assertThat(route.routeLongName()).matches("long name");
-        assertThat(route.routeType()).isEqualTo(GtfsRouteType.RAIL);
+    assertThat(noticeContainer.getValidationNotices()).isEmpty();
+    assertThat(tableContainer.entityCount()).isEqualTo(1);
+    GtfsRoute route = tableContainer.byRouteId("route id value");
+    assertThat(route).isNotNull();
+    assertThat(route.routeId()).matches("route id value");
+    assertThat(route.agencyId()).matches("agency id value");
+    assertThat(route.routeShortName()).matches("short name");
+    assertThat(route.routeLongName()).matches("long name");
+    assertThat(route.routeType()).isEqualTo(GtfsRouteType.RAIL);
+  }
 
-        reader.close();
-    }
+  @Test
+  public void missingRequiredFieldShouldGenerateNotice() throws IOException {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    Reader reader =
+        new StringReader(
+            "route_id,agency_id,route_short_name,route_long_name,route_type"
+                + System.lineSeparator()
+                + "route id value,agency id value,short name,long name,");
+    GtfsRouteTableLoader loader = new GtfsRouteTableLoader();
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsRouteTableContainer tableContainer =
+        (GtfsRouteTableContainer) loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    reader.close();
 
-    @Test
-    public void missingRequiredFieldShouldGenerateNotice() throws IOException {
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        Reader reader =
-                new StringReader(
-                        "route_id,agency_id,route_short_name,route_long_name,route_type"
-                                + System.lineSeparator() +
-                                "route id value,agency id value,short name,long name,");
-        GtfsRouteTableLoader loader = new GtfsRouteTableLoader();
-        NoticeContainer noticeContainer = new NoticeContainer();
-        GtfsRouteTableContainer tableContainer =
-                (GtfsRouteTableContainer) loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    // FIXME: this is linked to issue #625
+    // (https://github.com/MobilityData/gtfs-validator/issues/625): routes.route_type should be
+    // required:
+    // http://gtfs.org/reference/static/#routestxt.
+    assertThat(noticeContainer.getValidationNotices())
+        .containsExactly(new MissingRequiredFieldError("routes.txt", 2, "route_type"));
+    assertThat(tableContainer.entityCount()).isEqualTo(0);
+  }
 
-        assertThat(noticeContainer.getNotices()).isNotEmpty();
-        assertThat(noticeContainer.getNotices().get(0).getCode()).matches("missing_required_field");
-        assertThat(noticeContainer.getNotices().get(0).getContext()).containsEntry("filename", "routes.txt");
-        assertThat(noticeContainer.getNotices().get(0).getContext()).containsEntry("csvRowNumber", 2L);
-        assertThat(noticeContainer.getNotices().get(0).getContext()).containsEntry("fieldName", "route_type");
-        assertThat(tableContainer.entityCount()).isEqualTo(0);
-        reader.close();
-    }
+  @Test
+  public void emptyFileShouldGenerateNotice() throws IOException {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    Reader reader = new StringReader("");
+    GtfsRouteTableLoader loader = new GtfsRouteTableLoader();
+    NoticeContainer noticeContainer = new NoticeContainer();
 
-    @Test
-    public void emptyFileShouldGenerateNotice() throws IOException {
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        Reader reader = new StringReader("");
-        GtfsRouteTableLoader loader = new GtfsRouteTableLoader();
-        NoticeContainer noticeContainer = new NoticeContainer();
+    loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    reader.close();
 
-        loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
-
-        assertThat(noticeContainer.getNotices()).isNotEmpty();
-        assertThat(noticeContainer.getNotices().get(0).getClass().getSimpleName())
-                .isEqualTo("EmptyFileNotice");
-        reader.close();
-    }
+    assertThat(noticeContainer.getValidationNotices())
+        .containsExactly(new EmptyFileNotice("routes.txt"));
+  }
 }
