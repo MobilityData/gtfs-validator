6a7ecece4815323ae05304dac40c88b74c192a95
lionel-nj <lionel@mobilitydata.org>
2021-01-25 12:39:15 -0400

    test: GtfsStopTimeTableLoader class

--- main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsStopTimeTableLoaderTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsStopTimeTableLoaderTest.java
@@ -16,83 +16,77 @@
 
 package org.mobilitydata.gtfsvalidator.table;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
-import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
-import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
+import static com.google.common.truth.Truth.assertThat;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mobilitydata.gtfsvalidator.input.GtfsFeedName;
+import org.mobilitydata.gtfsvalidator.notice.EmptyFileNotice;
+import org.mobilitydata.gtfsvalidator.notice.MissingRequiredFieldError;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.validator.ValidatorLoader;
 
-import static com.google.common.truth.Truth.assertThat;
-
-/**
- * Runs GtfsStopTimeTableLoader on test CSV data.
- */
+/** Runs GtfsStopTimeTableLoader on test CSV data. */
 @RunWith(JUnit4.class)
 public class GtfsStopTimeTableLoaderTest {
-    private static final GtfsFeedName FEED_NAME = GtfsFeedName.parseString("au-sydney-buses");
+  private static final GtfsFeedName FEED_NAME = GtfsFeedName.parseString("au-sydney-buses");
 
-    @Test
-    public void validFileShouldNotGenerateNotice() throws IOException {
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        Reader reader =
-                new StringReader(
-                        "trip_id,stop_id,stop_sequence"
-                                + System.lineSeparator() +
-                                "trip id value,stop id value,20");
-        GtfsStopTimeTableLoader loader = new GtfsStopTimeTableLoader();
-        NoticeContainer noticeContainer = new NoticeContainer();
-        GtfsStopTimeTableContainer tableContainer =
-                (GtfsStopTimeTableContainer) loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+  @Test
+  public void validFileShouldNotGenerateNotice() throws IOException {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    Reader reader =
+        new StringReader(
+            "trip_id,stop_id,stop_sequence"
+                + System.lineSeparator()
+                + "trip id value,stop id value,20");
+    GtfsStopTimeTableLoader loader = new GtfsStopTimeTableLoader();
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsStopTimeTableContainer tableContainer =
+        (GtfsStopTimeTableContainer)
+            loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    reader.close();
 
-        assertThat(noticeContainer.getNotices()).isEmpty();
-        assertThat(tableContainer.entityCount()).isEqualTo(1);
-        GtfsStopTime stopTime = tableContainer.byTripId("trip id value").get(0);
-        assertThat(stopTime).isNotNull();
-        assertThat(stopTime.tripId()).matches("trip id value");
-        assertThat(stopTime.stopId()).matches("stop id value");
-        assertThat(stopTime.stopSequence()).isEqualTo(20);
+    assertThat(noticeContainer.getValidationNotices()).isEmpty();
+    assertThat(tableContainer.entityCount()).isEqualTo(1);
+    GtfsStopTime stopTime = tableContainer.byTripId("trip id value").get(0);
+    assertThat(stopTime).isNotNull();
+    assertThat(stopTime.tripId()).matches("trip id value");
+    assertThat(stopTime.stopId()).matches("stop id value");
+    assertThat(stopTime.stopSequence()).isEqualTo(20);
+  }
 
-        reader.close();
-    }
+  @Test
+  public void missingRequiredFieldShouldGenerateNotice() throws IOException {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    Reader reader =
+        new StringReader(
+            "trip_id,stop_id,stop_sequence" + System.lineSeparator() + ",stop id value,20");
+    GtfsStopTimeTableLoader loader = new GtfsStopTimeTableLoader();
+    NoticeContainer noticeContainer = new NoticeContainer();
+    GtfsStopTimeTableContainer tableContainer =
+        (GtfsStopTimeTableContainer)
+            loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    reader.close();
 
-    @Test
-    public void missingRequiredFieldShouldGenerateNotice() throws IOException {
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        Reader reader =
-                new StringReader(
-                        "trip_id,stop_id,stop_sequence"
-                                + System.lineSeparator() +
-                                ",stop id value,20");
-        GtfsStopTimeTableLoader loader = new GtfsStopTimeTableLoader();
-        NoticeContainer noticeContainer = new NoticeContainer();
-        GtfsStopTimeTableContainer tableContainer =
-                (GtfsStopTimeTableContainer) loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    assertThat(noticeContainer.getValidationNotices())
+        .containsExactly(new MissingRequiredFieldError("stop_times.txt", 2, "trip_id"));
+    assertThat(tableContainer.entityCount()).isEqualTo(0);
+  }
 
-        assertThat(noticeContainer.getNotices()).isNotEmpty();
-        assertThat(noticeContainer.getNotices().get(0).getCode()).matches("missing_required_field");
-        assertThat(noticeContainer.getNotices().get(0).getContext()).containsEntry("filename", "stop_times.txt");
-        assertThat(noticeContainer.getNotices().get(0).getContext()).containsEntry("csvRowNumber", 2L);
-        assertThat(noticeContainer.getNotices().get(0).getContext()).containsEntry("fieldName", "trip_id");
-        assertThat(tableContainer.entityCount()).isEqualTo(0);
-        reader.close();
-    }
+  @Test
+  public void emptyFileShouldGenerateNotice() throws IOException {
+    ValidatorLoader validatorLoader = new ValidatorLoader();
+    Reader reader = new StringReader("");
+    GtfsStopTimeTableLoader loader = new GtfsStopTimeTableLoader();
+    NoticeContainer noticeContainer = new NoticeContainer();
+    loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
+    reader.close();
 
-    @Test
-    public void emptyFileShouldGenerateNotice() throws IOException {
-        ValidatorLoader validatorLoader = new ValidatorLoader();
-        Reader reader = new StringReader("");
-        GtfsStopTimeTableLoader loader = new GtfsStopTimeTableLoader();
-        NoticeContainer noticeContainer = new NoticeContainer();
-        loader.load(reader, FEED_NAME, validatorLoader, noticeContainer);
-
-        assertThat(noticeContainer.getNotices()).isNotEmpty();
-        assertThat(noticeContainer.getNotices().get(0).getClass().getSimpleName())
-                .isEqualTo("EmptyFileNotice");
-        reader.close();
-    }
+    assertThat(noticeContainer.getValidationNotices())
+        .containsExactly(new EmptyFileNotice("stop_times.txt"));
+  }
 }
