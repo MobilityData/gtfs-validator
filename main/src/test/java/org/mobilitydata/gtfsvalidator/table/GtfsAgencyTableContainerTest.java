de520abc7511794e48606476b611c2635c37f10e
lionel-nj <lionel@mobilitydata.org>
2021-01-25 16:30:07 -0400

    test: GtfsAgencyTableContainer class

--- main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsAgencyTableContainerTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/table/GtfsAgencyTableContainerTest.java
@@ -16,61 +16,59 @@
 
 package org.mobilitydata.gtfsvalidator.table;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
-
 @RunWith(JUnit4.class)
 public class GtfsAgencyTableContainerTest {
-    @Test
-    public void forEmptyFile() {
-        GtfsAgencyTableContainer tableContainer = GtfsAgencyTableContainer.forEmptyFile();
-        assertThat(tableContainer.getEntities()).isEmpty();
-        assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
-        assertThat(tableContainer.isEmptyFile()).isTrue();
-        assertThat(tableContainer.hasInvalidHeaders()).isFalse();
-        assertThat(tableContainer.isMissingFile()).isFalse();
-    }
+  @Test
+  public void forEmptyFile() {
+    GtfsAgencyTableContainer tableContainer = GtfsAgencyTableContainer.forEmptyFile();
+    assertThat(tableContainer.getEntities()).isEmpty();
+    assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
+    assertThat(tableContainer.isEmptyFile()).isTrue();
+    assertThat(tableContainer.hasInvalidHeaders()).isFalse();
+    assertThat(tableContainer.isMissingFile()).isFalse();
+  }
 
-    @Test
-    public void forMissingFile() {
-        GtfsAgencyTableContainer tableContainer = GtfsAgencyTableContainer.forMissingFile();
-        assertThat(tableContainer.getEntities()).isEmpty();
-        assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
-        assertThat(tableContainer.isEmptyFile()).isFalse();
-        assertThat(tableContainer.hasInvalidHeaders()).isFalse();
-        assertThat(tableContainer.isMissingFile()).isTrue();
-    }
+  @Test
+  public void forMissingFile() {
+    GtfsAgencyTableContainer tableContainer = GtfsAgencyTableContainer.forMissingFile();
+    assertThat(tableContainer.getEntities()).isEmpty();
+    assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
+    assertThat(tableContainer.isEmptyFile()).isFalse();
+    assertThat(tableContainer.hasInvalidHeaders()).isFalse();
+    assertThat(tableContainer.isMissingFile()).isTrue();
+  }
 
-    @Test
-    public void forInvalidHeaders() {
-        GtfsAgencyTableContainer tableContainer = GtfsAgencyTableContainer.forInvalidHeaders();
-        assertThat(tableContainer.getEntities()).isEmpty();
-        assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
-        assertThat(tableContainer.isEmptyFile()).isFalse();
-        assertThat(tableContainer.hasInvalidHeaders()).isTrue();
-        assertThat(tableContainer.isMissingFile()).isFalse();
-    }
+  @Test
+  public void forInvalidHeaders() {
+    GtfsAgencyTableContainer tableContainer = GtfsAgencyTableContainer.forInvalidHeaders();
+    assertThat(tableContainer.getEntities()).isEmpty();
+    assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
+    assertThat(tableContainer.isEmptyFile()).isFalse();
+    assertThat(tableContainer.hasInvalidHeaders()).isTrue();
+    assertThat(tableContainer.isMissingFile()).isFalse();
+  }
 
-    @Test
-    public void forEntities() {
-        NoticeContainer noticeContainer = new NoticeContainer();
-        List<GtfsAgency> agencyCollection = new ArrayList<>();
-        GtfsAgency mockAgency = mock(GtfsAgency.class);
-        agencyCollection.add(mockAgency);
-        GtfsAgencyTableContainer tableContainer =
-                GtfsAgencyTableContainer.forEntities(agencyCollection, noticeContainer);
-        assertThat(tableContainer.getEntities().isEmpty()).isFalse();
-        assertThat(tableContainer.isEmptyFile()).isFalse();
-        assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
-        assertThat(tableContainer.hasInvalidHeaders()).isFalse();
-        assertThat(tableContainer.isMissingFile()).isFalse();
-    }
+  @Test
+  public void forEntities() {
+    NoticeContainer noticeContainer = new NoticeContainer();
+    List<GtfsAgency> agencyCollection = new ArrayList<>();
+    GtfsAgency agency = new GtfsAgency.Builder().build();
+    agencyCollection.add(agency);
+    GtfsAgencyTableContainer tableContainer =
+        GtfsAgencyTableContainer.forEntities(agencyCollection, noticeContainer);
+    assertThat(tableContainer.getEntities().isEmpty()).isFalse();
+    assertThat(tableContainer.isEmptyFile()).isFalse();
+    assertThat(tableContainer.gtfsFilename()).matches("agency.txt");
+    assertThat(tableContainer.hasInvalidHeaders()).isFalse();
+    assertThat(tableContainer.isMissingFile()).isFalse();
+  }
 }
