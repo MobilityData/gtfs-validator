a7c3fca709f08a62bafb27b79a52250b37b44c5d
Alexej Ababilov <aababilov@google.com>
2021-01-28 04:45:19 +1100

    feat: Support CSV files that start with UTF-8 BOM (#692)
    GTFS spec says:
    
    Files should be encoded in UTF-8 to support all Unicode characters.
    Files that include the Unicode byte-order mark (BOM) character are
    acceptable. See http://unicode.org/faq/utf_bom.html#BOM for more
    information on the BOM character and UTF-8.
    
    The Unicode Standard permits the BOM in UTF-8, but does not require
    or recommend its use. Byte order has no meaning in UTF-8, so its only
    use in UTF-8 is to signal at the start that the text stream is
    encoded in UTF-8, or that it was converted to UTF-8 from a stream that
    contained an optional BOM.

--- core/src/test/java/org/mobilitydata/gtfsvalidator/parsing/CsvRowTest.java
+++ core/src/test/java/org/mobilitydata/gtfsvalidator/parsing/CsvRowTest.java
@@ -18,9 +18,10 @@ package org.mobilitydata.gtfsvalidator.parsing;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -28,13 +29,17 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CsvRowTest {
 
+  private static InputStream toInputStream(String s) {
+    return new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+  }
+
   @Test
   public void testCsvRowAllMethods() throws IOException {
-    Reader reader =
-        new StringReader(
+    InputStream inputStream =
+        toInputStream(
             "stop_id,stop_name,stop_lat\n" + "s1,First stop,3.21\n" + "s2,Second stop,1.31\n");
-    CsvFile csvFile = new CsvFile(reader, "stops.txt");
-    reader.close();
+    CsvFile csvFile = new CsvFile(inputStream, "stops.txt");
+    inputStream.close();
 
     String[] columnValues = {
       "stop_id",
