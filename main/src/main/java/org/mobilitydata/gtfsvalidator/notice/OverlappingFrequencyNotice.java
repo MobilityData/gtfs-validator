789a22ec7754aec0b9cafdece17333ad1317f8b9
lionel-nj <lionel@mobilitydata.org>
2021-02-05 10:30:38 -0400

    fix: javadoc aggregation

--- main/src/main/java/org/mobilitydata/gtfsvalidator/notice/OverlappingFrequencyNotice.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/notice/OverlappingFrequencyNotice.java
@@ -7,7 +7,7 @@ import org.mobilitydata.gtfsvalidator.type.GtfsTime;
  * Two frequency entries referring to the same trip may not have an overlapping time range.
  *
  * <p>Two entries X and Y are considered to directly overlap if <i>X.start_time &lt;= Y.start_time</i>
- * and <i>Y.start_time &lt; X.end_time</i>.
+ * and <i>Y.start_time &lt;X.end_time</i>.
  */
 public class OverlappingFrequencyNotice extends ValidationNotice {
   public OverlappingFrequencyNotice(
