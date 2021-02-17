d355fbd15f2f971f990edaba189944f3fb7c7fb1
lionel-nj <lionel@mobilitydata.org>
2021-02-17 09:22:18 +0100

    fix typo in javadocs

--- main/src/main/java/org/mobilitydata/gtfsvalidator/notice/SameNameAndDescriptionForRouteNotice.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/notice/SameNameAndDescriptionForRouteNotice.java
@@ -20,7 +20,7 @@ import static org.mobilitydata.gtfsvalidator.table.GtfsRouteTableLoader.FILENAME
 
 import com.google.common.collect.ImmutableMap;
 
-/** A {@code GtfsRoute] has identical value for `routes.route_desc` and
+/** A {@code GtfsRoute} has identical value for `routes.route_desc` and
  * `routes.route_long_name`/`routes/route_short_name.
  * <p>"Do not simply duplicate the name of the location." (http://gtfs.org/reference/static#routestxt)
  *
