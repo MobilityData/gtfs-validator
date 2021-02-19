e47ee19d3730fa1281782e89a75a156524f65641
Sean Barbeau <sjbarbeau@gmail.com>
2021-02-19 14:58:13 -0500

    Add time complexity and more documentation

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/StopTooFarFromTripShapeValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/StopTooFarFromTripShapeValidator.java
@@ -48,6 +48,39 @@ import org.mobilitydata.gtfsvalidator.util.GeospatialUtil;
  * Validates: a {@link GtfsStop} is within a distance threshold for a trip shape.
  *
  * <p>Generated notice: {@link StopTooFarFromTripShapeNotice}.
+ *
+ * <p>Time complexity: <i>O(n * p)</i>, where <i>n</i> is the number of records in <i>stop_times.txt</i>
+ * and <i>p</i> is the number of points in a trip shape. This assumes that the time complexity to
+ * check if the stop location lies within the buffered trip shape is <i>O(p)</i> (see below).
+ *
+ * <p>In an extreme, not-very-likely case where a GTFS file consists of entirely of
+ * trips that reuse the same shape but all have disjoint sets of stops along that shape, <i>p</i>
+ * may be the number of records in <i>shapes.txt</i>. This is because every stop time must
+ * be compared against every trip shape point.
+ *
+ * <p>However, in a typical case where most trips have independent shapes and the trips that do re-use
+ * shapes share most of the same stops, and therefore the time complexity has a tighter bound of
+ * <i>O(n)</i>, where <i>n</i> is the number of records in <i>stop_times.txt<i>. This is because
+ * <i>p</i> is a fixed constant less than the number of records in <i>shapes.txt</i> and each point
+ * is not processed more than once. Note that this point-in-polygon operation can still take a
+ * reasonable amount of time to execute (the constant/overhead can be large) and should be considered
+ * expensive. A cache is used to avoid calculating the distance between the same stop and shape more
+ * than once when multiple trips share the same shape.
+ *
+ * <p>This validator uses spatial4j Euclidean operations to check if the stop location lies within the
+ * buffered trip shape, which as mentioned above has a time complexity of <i>O(p)</i>, where p is the
+ * number of points in a trip shape.
+ * See:
+ * https://github.com/locationtech/spatial4j/blob/1f6e2047f0574a430fc711cf2cd5adf141a8bda9/src/main/java/org/locationtech/spatial4j/shape/impl/BufferedLineString.java#L107
+ * https://github.com/locationtech/spatial4j/blob/1f6e2047f0574a430fc711cf2cd5adf141a8bda9/src/main/java/org/locationtech/spatial4j/shape/impl/InfBufLine.java#L81
+ *
+ * <p>A future running time optimization could be caching the buffered line for each shape when it
+ * is first constructed. However, this is not currently implemented because shapes.txt is typically
+ * a large file and this would have negative memory implications. This would help reduce the
+ * initialization overhead for each shape IFF multiple trips share the same shape.
+ *
+ * <p>If additional accuracy is desired geodesic calculations may be use instead of Euclidean
+ * calculations, although this comes at a performance cost - see https://github.com/MobilityData/gtfs-validator/pull/750#discussion_r578667817.
  */
 @GtfsValidator
 public class StopTooFarFromTripShapeValidator extends FileValidator {
@@ -116,7 +149,7 @@ public class StopTooFarFromTripShapeValidator extends FileValidator {
       return notices;
     }
 
-    // Create a buffered polyline from the GTFS shapes data
+    // Create a buffered polyline from the GTFS shapes data - uses Euclidean operations (not geodesic)
     ShapeFactory.LineStringBuilder lineBuilder = GeospatialUtil.getShapeFactory().lineString();
     for (GtfsShape shapePoint : shapePoints) {
       lineBuilder.pointXY(shapePoint.shapePtLon(), shapePoint.shapePtLat());
@@ -133,7 +166,8 @@ public class StopTooFarFromTripShapeValidator extends FileValidator {
         continue;
       }
       if (testedCache.contains(shapeId + stop.stopId())) {
-        // We've already tested this combination of shape ID and stop ID - skip to next stop
+        // We've already tested this combination of shape ID and stop ID - skip to next stop to
+        // avoid spamming multiple duplicate notices and as a performance optimization
         continue;
       }
       testedCache.add(shapeId + stop.stopId());
