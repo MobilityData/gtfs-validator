2323bc5e6bcaf24878f72499a9fd8e57dd695f27
Alexei Ababilov <aababilov@google.com>
2021-02-26 13:43:30 +1100

    feat: Emit a warning for empty column name
    Consider a header row:
    stop_id,stop_name,
    
    Its third column does not have a name. We ignore it in the same way as
    we ignore unknown columns but we need to emit a clear warning for that.

---
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/notice/EmptyColumnNameNotice.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilitydata.gtfsvalidator.notice;
+
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * A column name is empty. Such columns are skipped by the validator.
+ *
+ * <p>Severity: {@code SeverityLevel.WARNING}
+ */
+public class EmptyColumnNameNotice extends ValidationNotice {
+  public EmptyColumnNameNotice(String filename, int index) {
+    super(
+        ImmutableMap.of(
+            "filename", filename,
+            "index", index),
+        SeverityLevel.WARNING);
+  }
+
+  @Override
+  public String getCode() {
+    return "empty_column_name";
+  }
+}
