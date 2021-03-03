ab5b78efb0b2281d2b1c2724ad315d187246ae33
Sean Barbeau <sjbarbeau@gmail.com>
2021-03-03 11:50:18 -0500

    docs: Add auto-gen Javadocs for Lists of @SequenceKey fields (#782)
    The goal is to make the sorting of GTFS entities that have been annotated with @SequenceKey more transparent in List data structure. Currently for a developer writing a new rule it's not clear whether they are responsible for sorting entities like GtfsShape by shapePtSequence in the rule implementation or if those entities have already be sorted in preprocessing.
    
    This change auto-generates Javadocs for methods that return Lists of entities to make it clear which have been sorted, and by what field. This should hopefully result in less uncertainty for developers implementing new rules and should avoid potential performance pitfalls of sorting being unnecessarily implemented within validation rules.

--- processor/src/main/java/org/mobilitydata/gtfsvalidator/processor/TableContainerGenerator.java
+++ processor/src/main/java/org/mobilitydata/gtfsvalidator/processor/TableContainerGenerator.java
@@ -36,6 +36,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nullable;
 import javax.lang.model.element.Modifier;
 import org.mobilitydata.gtfsvalidator.annotation.Generated;
 import org.mobilitydata.gtfsvalidator.notice.DuplicateKeyError;
@@ -60,6 +61,12 @@ public class TableContainerGenerator {
 
   private static void addListMultimapWithGetters(
       TypeSpec.Builder typeSpec, GtfsFieldDescriptor indexField, TypeName entityTypeName) {
+    addListMultimapWithGetters(typeSpec, indexField, null, entityTypeName);
+  }
+
+  private static void addListMultimapWithGetters(
+      TypeSpec.Builder typeSpec, GtfsFieldDescriptor indexField, @Nullable GtfsFieldDescriptor sequenceField,
+      TypeName entityTypeName) {
     TypeName keyMapType =
         ParameterizedTypeName.get(
             ClassName.get(ListMultimap.class), TypeName.get(indexField.javaType()), entityTypeName);
@@ -69,18 +76,21 @@ public class TableContainerGenerator {
         FieldSpec.builder(keyMapType, fieldName, Modifier.PRIVATE)
             .initializer("$T.create()", ParameterizedTypeName.get(ArrayListMultimap.class))
             .build());
+    String sortedBy = sequenceField != null ? " sorted by " + FieldNameConverter.gtfsColumnName(sequenceField.name()) : "";
     typeSpec.addMethod(
         MethodSpec.methodBuilder(methodName)
             .addModifiers(Modifier.PUBLIC)
             .addParameter(TypeName.get(indexField.javaType()), "key")
             .returns(ParameterizedTypeName.get(ClassName.get(List.class), entityTypeName))
             .addStatement("return $L.get(key)", fieldName)
+            .addJavadoc("@return List of " + entityTypeName + sortedBy)
             .build());
     typeSpec.addMethod(
         MethodSpec.methodBuilder(methodName + "Map")
             .addModifiers(Modifier.PUBLIC)
             .returns(keyMapType)
             .addStatement("return $L", fieldName)
+            .addJavadoc("@return ListMultimap keyed on " + FieldNameConverter.gtfsColumnName(indexField.name()) + " with values that are Lists of " + entityTypeName + sortedBy)
             .build());
   }
 
@@ -163,7 +173,7 @@ public class TableContainerGenerator {
               .build());
     } else if (fileDescriptor.sequenceKey().isPresent()) {
       addListMultimapWithGetters(
-          typeSpec, fileDescriptor.firstKey().get(), classNames.entityImplementationTypeName());
+          typeSpec, fileDescriptor.firstKey().get(), fileDescriptor.sequenceKey().get(), classNames.entityImplementationTypeName());
     } else if (fileDescriptor.primaryKey().isPresent()) {
       addMapWithGetter(
           typeSpec, fileDescriptor.primaryKey().get(), classNames.entityImplementationTypeName());
