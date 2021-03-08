c0732911e4b070236fb67dd0ded790698de764e8
lionel-nj <lionel@mobilitydata.org>
2021-03-08 11:40:55 -0500

    use getClass() instead of leveraging getSimpleName() method

--- core/src/main/java/org/mobilitydata/gtfsvalidator/notice/Notice.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/notice/Notice.java
@@ -54,7 +54,7 @@ public abstract class Notice {
     if (other instanceof Notice) {
       Notice otherNotice = (Notice) other;
       return context.equals(otherNotice.context)
-          && getClass().getSimpleName().equals(otherNotice.getClass().getSimpleName())
+          && getClass().equals(otherNotice.getClass())
           && severityLevel.equals(otherNotice.severityLevel);
     }
     return false;
@@ -71,6 +71,6 @@ public abstract class Notice {
 
   @Override
   public int hashCode() {
-    return Objects.hash(getClass().getSimpleName(), getContext(), getSeverityLevel());
+    return Objects.hash(getClass(), getContext(), getSeverityLevel());
   }
 }
