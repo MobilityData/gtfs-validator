3513919315cd8a2baa3fd631bf3dd62dbf65ea9f
lionel-nj <lionel@mobilitydata.org>
2021-03-15 16:22:24 -0400

    refactor: make FeedServiceDateValidator extend SingleEntityValidator
    - revisit unit tests

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/FeedServiceDateValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/FeedServiceDateValidator.java
@@ -16,43 +16,43 @@
 
 package org.mobilitydata.gtfsvalidator.validator;
 
-import javax.inject.Inject;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
 import org.mobilitydata.gtfsvalidator.notice.MissingFeedInfoDateNotice;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.StartAndEndDateOutOfOrderNotice;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfo;
-import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfoTableContainer;
+import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfoTableLoader;
 
 /**
- * Validates 3 rules: 1) start_date &lt;= end_date for all rows in "feed_info.txt" 2)
- * feed_info.start_date is provided if feed_info.end_date is provided 3) feed_info.end_date is
- * provided if feed_info.start_date is provided.
+ * Validates 3 rules: 1) start_date &lt;= end_date 2) feed_info.start_date is provided if
+ * feed_info.end_date is provided 3) feed_info.end_date is provided if feed_info.start_date is
+ * provided.
  *
  * <p>Generated notice: {@link StartAndEndDateOutOfOrderNotice}.
  */
 @GtfsValidator
-public class FeedServiceDateValidator extends FileValidator {
-  @Inject GtfsFeedInfoTableContainer feedInfoTable;
+public class FeedServiceDateValidator extends SingleEntityValidator<GtfsFeedInfo> {
 
   @Override
-  public void validate(NoticeContainer noticeContainer) {
-    for (GtfsFeedInfo feedInfo : feedInfoTable.getEntities()) {
-      if (feedInfo.hasFeedStartDate() && !feedInfo.hasFeedEndDate()) {
-        noticeContainer.addValidationNotice(
-            new MissingFeedInfoDateNotice(feedInfo.csvRowNumber(), "feed_end_date"));
-      } else if (!feedInfo.hasFeedStartDate() && feedInfo.hasFeedEndDate()) {
-        noticeContainer.addValidationNotice(
-            new MissingFeedInfoDateNotice(feedInfo.csvRowNumber(), "feed_start_date"));
-      }
-      if (feedInfo.hasFeedStartDate()
-          && feedInfo.hasFeedEndDate()
-          && feedInfo.feedStartDate().isAfter(feedInfo.feedEndDate())) {
-        noticeContainer.addValidationNotice(
-            new StartAndEndDateOutOfOrderNotice(
-                feedInfoTable.gtfsFilename(), feedInfo.csvRowNumber(),
-                feedInfo.feedStartDate(), feedInfo.feedEndDate()));
-      }
+  public void validate(GtfsFeedInfo feedInfo, NoticeContainer noticeContainer) {
+    if (feedInfo.hasFeedStartDate() && !feedInfo.hasFeedEndDate()) {
+      noticeContainer.addValidationNotice(
+          new MissingFeedInfoDateNotice(
+              feedInfo.csvRowNumber(), GtfsFeedInfoTableLoader.FEED_END_DATE_FIELD_NAME));
+    } else if (!feedInfo.hasFeedStartDate() && feedInfo.hasFeedEndDate()) {
+      noticeContainer.addValidationNotice(
+          new MissingFeedInfoDateNotice(
+              feedInfo.csvRowNumber(), GtfsFeedInfoTableLoader.FEED_START_DATE_FIELD_NAME));
+    }
+    if (feedInfo.hasFeedStartDate()
+        && feedInfo.hasFeedEndDate()
+        && feedInfo.feedStartDate().isAfter(feedInfo.feedEndDate())) {
+      noticeContainer.addValidationNotice(
+          new StartAndEndDateOutOfOrderNotice(
+              GtfsFeedInfoTableLoader.FILENAME,
+              feedInfo.csvRowNumber(),
+              feedInfo.feedStartDate(),
+              feedInfo.feedEndDate()));
     }
   }
 }
--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/FeedServiceDateValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/FeedServiceDateValidatorTest.java
@@ -2,25 +2,17 @@ package org.mobilitydata.gtfsvalidator.validator;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.google.common.collect.ImmutableList;
-import java.util.List;
 import java.util.Locale;
 import org.junit.Test;
 import org.mobilitydata.gtfsvalidator.notice.MissingFeedInfoDateNotice;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.StartAndEndDateOutOfOrderNotice;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfo;
-import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfoTableContainer;
 import org.mobilitydata.gtfsvalidator.type.GtfsDate;
 
 public class FeedServiceDateValidatorTest {
 
-  private static GtfsFeedInfoTableContainer createFeedInfoTable(
-      NoticeContainer noticeContainer, List<GtfsFeedInfo> entities) {
-    return GtfsFeedInfoTableContainer.forEntities(entities, noticeContainer);
-  }
-
-  public static GtfsFeedInfo createFeedInfo(
+  private static GtfsFeedInfo createFeedInfo(
       long csvRowNumber,
       String feedPublisherName,
       String feedPublisherUrl,
@@ -44,44 +36,34 @@ public class FeedServiceDateValidatorTest {
   @Test
   public void startDateBeforeEndDateShouldNotGenerateNotice() {
     NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
-        createFeedInfoTable(
-            noticeContainer,
-            ImmutableList.of(
-                createFeedInfo(
-                    1,
-                    "name value",
-                    "url value",
-                    Locale.CANADA,
-                    GtfsDate.fromEpochDay(340),
-                    GtfsDate.fromEpochDay(450))));
-
     FeedServiceDateValidator underTest = new FeedServiceDateValidator();
-    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    underTest.validate(noticeContainer);
+    underTest.validate(
+        createFeedInfo(
+            1,
+            "name value",
+            "url value",
+            Locale.CANADA,
+            GtfsDate.fromEpochDay(340),
+            GtfsDate.fromEpochDay(450)),
+        noticeContainer);
     assertThat(noticeContainer.getValidationNotices().isEmpty());
   }
 
   @Test
   public void startDateAfterEndDateShouldGenerateNotice() {
     NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
-        createFeedInfoTable(
-            noticeContainer,
-            ImmutableList.of(
-                createFeedInfo(
-                    1,
-                    "name value",
-                    "url value",
-                    Locale.CANADA,
-                    GtfsDate.fromEpochDay(450),
-                    GtfsDate.fromEpochDay(340))));
-
     FeedServiceDateValidator underTest = new FeedServiceDateValidator();
-    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    underTest.validate(noticeContainer);
+    underTest.validate(
+        createFeedInfo(
+            1,
+            "name value",
+            "url value",
+            Locale.CANADA,
+            GtfsDate.fromEpochDay(450),
+            GtfsDate.fromEpochDay(340)),
+        noticeContainer);
     assertThat(noticeContainer.getValidationNotices())
         .containsExactly(
             new StartAndEndDateOutOfOrderNotice(
@@ -91,22 +73,12 @@ public class FeedServiceDateValidatorTest {
   @Test
   public void noStartDateShouldGenerateNotice() {
     NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
-        createFeedInfoTable(
-            noticeContainer,
-            ImmutableList.of(
-                createFeedInfo(
-                    1,
-                    "name value",
-                    "url value",
-                    Locale.CANADA,
-                    null,
-                    GtfsDate.fromEpochDay(450))));
-
     FeedServiceDateValidator underTest = new FeedServiceDateValidator();
-    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    underTest.validate(noticeContainer);
+    underTest.validate(
+        createFeedInfo(
+            1, "name value", "url value", Locale.CANADA, null, GtfsDate.fromEpochDay(450)),
+        noticeContainer);
     assertThat(noticeContainer.getValidationNotices())
         .containsExactly(new MissingFeedInfoDateNotice(1, "feed_start_date"));
   }
@@ -114,22 +86,12 @@ public class FeedServiceDateValidatorTest {
   @Test
   public void noEndDateShouldGenerateNotice() {
     NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
-        createFeedInfoTable(
-            noticeContainer,
-            ImmutableList.of(
-                createFeedInfo(
-                    1,
-                    "name value",
-                    "url value",
-                    Locale.CANADA,
-                    GtfsDate.fromEpochDay(450),
-                    null)));
-
     FeedServiceDateValidator underTest = new FeedServiceDateValidator();
-    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    underTest.validate(noticeContainer);
+    underTest.validate(
+        createFeedInfo(
+            1, "name value", "url value", Locale.CANADA, GtfsDate.fromEpochDay(450), null),
+        noticeContainer);
     assertThat(noticeContainer.getValidationNotices())
         .containsExactly(new MissingFeedInfoDateNotice(1, "feed_end_date"));
   }
@@ -137,39 +99,28 @@ public class FeedServiceDateValidatorTest {
   @Test
   public void bothDatesCanBeBlank() {
     NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
-        createFeedInfoTable(
-            noticeContainer,
-            ImmutableList.of(
-                createFeedInfo(
-                    1, "name value", "https://www.mobilitydata.org", Locale.CANADA, null, null)));
-
     FeedServiceDateValidator underTest = new FeedServiceDateValidator();
-    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    underTest.validate(noticeContainer);
+    underTest.validate(
+        createFeedInfo(1, "name value", "https://www.mobilitydata.org", Locale.CANADA, null, null),
+        noticeContainer);
     assertThat(noticeContainer.getValidationNotices().isEmpty());
   }
 
   @Test
   public void bothDatesCanBeProvided() {
     NoticeContainer noticeContainer = new NoticeContainer();
-    GtfsFeedInfoTableContainer gtfsFeedInfoTable =
-        createFeedInfoTable(
-            noticeContainer,
-            ImmutableList.of(
-                createFeedInfo(
-                    1,
-                    "name value",
-                    "https://www.mobilitydata.org",
-                    Locale.CANADA,
-                    GtfsDate.fromEpochDay(450),
-                    GtfsDate.fromEpochDay(555))));
-
     FeedServiceDateValidator underTest = new FeedServiceDateValidator();
-    underTest.feedInfoTable = gtfsFeedInfoTable;
 
-    underTest.validate(noticeContainer);
+    underTest.validate(
+        createFeedInfo(
+            1,
+            "name value",
+            "https://www.mobilitydata.org",
+            Locale.CANADA,
+            GtfsDate.fromEpochDay(450),
+            GtfsDate.fromEpochDay(555)),
+        noticeContainer);
     assertThat(noticeContainer.getValidationNotices().isEmpty());
   }
 }
