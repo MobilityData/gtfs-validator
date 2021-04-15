3657758daf84255c7f0a6677e27d4099806b8292
lionel-nj <lionel@mobilitydata.org>
2021-04-15 13:03:25 -0400

    refactor: remove mocks from tests

--- main/src/test/java/org/mobilitydata/gtfsvalidator/cli/CliParametersAnalyzerTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/cli/CliParametersAnalyzerTest.java
@@ -17,130 +17,123 @@
 package org.mobilitydata.gtfsvalidator.cli;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import org.junit.Before;
+import com.beust.jcommander.JCommander;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 
 @RunWith(JUnit4.class)
 public class CliParametersAnalyzerTest {
-  private Handler mockHandler = null;
-  private ArgumentCaptor<LogRecord> logRecordCaptor = null;
+  @Test
+  public void isValid_false() {
+    Arguments args = new Arguments();
+    // --url and --input not provided
+    CliParametersAnalyzer cliParametersAnalyzer = new CliParametersAnalyzer();
+    String[] argv = {
+      "--output_base", "output value",
+      "--threads", "4"
+    };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isFalse();
 
-  @Before
-  public void installLogRecordCaptor() {
-    mockHandler = Mockito.mock(Handler.class);
-    logRecordCaptor = ArgumentCaptor.forClass(LogRecord.class);
-    Logger.getLogger(CliParametersAnalyzer.class.getName()).addHandler(mockHandler);
+    // --u and -i not provided
+    argv =
+        new String[] {
+          "-o", "output value",
+          "-t", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isFalse();
+
+    // --u and -i are provided
+    argv =
+        new String[] {
+          "-u", "url value",
+          "-i", "input value",
+          "-o", "output value",
+          "-t", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isFalse();
+
+    // --url and --input are provided
+    argv =
+        new String[] {
+          "--url", "url value",
+          "--input", "input value",
+          "--output_base", "output value",
+          "--threads", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isFalse();
+
+    // --storage_directory is provided and --url is not provided
+    argv =
+        new String[] {
+          "--storage_directory", "storage directory value",
+          "--input", "input value",
+          "--output_base", "output value",
+          "--threads", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isFalse();
+
+    // -s is provided and -u is not provided
+    argv =
+        new String[] {
+          "-s", "storage directory value",
+          "-i", "input value",
+          "-o", "output value",
+          "-t", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isFalse();
   }
 
-  // FIXME(lionel-nj): Please fix this test (see issue #591)
-  //  @Test
-  //  public void provideUrlAndInputCliParametersShouldReturnFalse() {
-  //    Arguments mockArguments = mock(Arguments.class);
-  //    when(mockArguments.getUrl()).thenReturn("url to dataset");
-  //    when(mockArguments.getInput()).thenReturn("path to dataset");
-  //
-  //    CliParametersAnalyzer underTest = new CliParametersAnalyzer();
-  //    assertThat(underTest.isValid(mockArguments)).isFalse();
-  //    verify(mockHandler).publish(logRecordCaptor.capture());
-  //    assertThat(logRecordCaptor.getValue().getMessage())
-  //        .contains(
-  //            "The two following CLI parameters cannot be "
-  //                + "provided at the same time: '--input' and '--url'");
-  //
-  //    //noinspection ResultOfMethodCallIgnored because object is mocked
-  //    verify(mockArguments, times(1)).getUrl();
-  //    //noinspection ResultOfMethodCallIgnored because object is mocked
-  //    verify(mockArguments, times(2)).getInput();
-  //    verifyNoMoreInteractions(mockArguments, mockHandler);
-  //  }
-
   @Test
-  public void bothUrlAndInputCliParametersNotProvidedShouldReturnFalse() {
-    Arguments mockArguments = mock(Arguments.class);
-    when(mockArguments.getUrl()).thenReturn(null);
-    when(mockArguments.getInput()).thenReturn(null);
+  public void isValid_true() {
+    Arguments args = new Arguments();
+    // --url is provided and --storage_directory is not provided
+    CliParametersAnalyzer cliParametersAnalyzer = new CliParametersAnalyzer();
+    String[] argv = {
+      "--url", "url value",
+      "--output_base", "output value",
+      "--threads", "4"
+    };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isTrue();
 
-    CliParametersAnalyzer underTest = new CliParametersAnalyzer();
-    assertThat(underTest.isValid(mockArguments)).isFalse();
-    verify(mockHandler).publish(logRecordCaptor.capture());
-    assertThat(logRecordCaptor.getValue().getMessage())
-        .contains(
-            "One of the two following CLI parameter must be" + " provided: '--input' and '--url'");
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(1)).getUrl();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(1)).getInput();
-    verifyNoMoreInteractions(mockArguments, mockHandler);
-  }
+    // --u is provided and --s is not provided
+    argv =
+        new String[] {
+          "-u", "url value",
+          "-o", "output value",
+          "-t", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isTrue();
 
-  @Test
-  public void provideUrlWithoutSpecifyingStorageDirectoryCliParameterShouldReturnTrue() {
-    Arguments mockArguments = mock(Arguments.class);
-    when(mockArguments.getUrl()).thenReturn("url to dataset");
-    when(mockArguments.getInput()).thenReturn(null);
-    when(mockArguments.getStorageDirectory()).thenReturn(null);
+    // -s is provided and -i is not provided
+    argv =
+        new String[] {
+          "-u", "url value",
+          "-s", "storage directory value",
+          "-o", "output value",
+          "-t", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isTrue();
 
-    CliParametersAnalyzer underTest = new CliParametersAnalyzer();
-    assertThat(underTest.isValid(mockArguments)).isTrue();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(1)).getUrl();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(2)).getInput();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(1)).getStorageDirectory();
-    verifyNoMoreInteractions(mockArguments, mockHandler);
-  }
-
-  // FIXME(lionel-nj): Please fix this test (see issue #591)
-  //  @Test
-  //  public void provideStorageDirectoryCliParameterWithoutSpecifyingUrlShouldReturnFalse() {
-  //    Arguments mockArguments = mock(Arguments.class);
-  //    when(mockArguments.getUrl()).thenReturn(null);
-  //    when(mockArguments.getInput()).thenReturn(null);
-  //    when(mockArguments.getStorageDirectory()).thenReturn("storage.zip");
-  //
-  //    CliParametersAnalyzer underTest = new CliParametersAnalyzer();
-  //    assertThat(underTest.isValid(mockArguments)).isFalse();
-  //    verify(mockHandler).publish(logRecordCaptor.capture());
-  //    assertThat(logRecordCaptor.getValue().getMessage())
-  //        .contains(
-  //            "One of the two following CLI parameter must be" + " provided: '--input' and
-  // '--url'");
-  //    //noinspection ResultOfMethodCallIgnored because object is mocked
-  //    verify(mockArguments, times(1)).getUrl();
-  //    //noinspection ResultOfMethodCallIgnored because object is mocked
-  //    verify(mockArguments, times(1)).getInput();
-  //    verifyNoMoreInteractions(mockArguments, mockHandler);
-  //  }
-
-  @Test
-  public void provideUrlStorageDirectoryAndNoInputCliParameterShouldReturnTrue() {
-    Arguments mockArguments = mock(Arguments.class);
-    when(mockArguments.getUrl()).thenReturn("url to dataset");
-    when(mockArguments.getInput()).thenReturn(null);
-    when(mockArguments.getStorageDirectory()).thenReturn("storage.zip");
-
-    CliParametersAnalyzer underTest = new CliParametersAnalyzer();
-    assertThat(underTest.isValid(mockArguments)).isTrue();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(2)).getUrl();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(2)).getInput();
-    //noinspection ResultOfMethodCallIgnored because object is mocked
-    verify(mockArguments, times(1)).getStorageDirectory();
-    verifyNoMoreInteractions(mockArguments, mockHandler);
+    // --storage_directory is provided and --input is not provided
+    argv =
+        new String[] {
+          "--url", "url value",
+          "--storage_directory", "storage directory value",
+          "--output_base", "output value",
+          "--threads", "4"
+        };
+    new JCommander(args).parse(argv);
+    assertThat(cliParametersAnalyzer.isValid(args)).isTrue();
   }
 }
