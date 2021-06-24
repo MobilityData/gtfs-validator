a1e00b89c426a5316a63d6df5fbaefc82b10f4a7
Alexei Ababilov <aababilov@google.com>
2021-06-24 19:46:58 +1000

    feat: Reduce severity of SameNameAndDescriptionForRouteNotice to WARNING
    A route that has the same route_desc and route_short_name or
    route_long_name may be still served to the end user, so this is not an
    error. In fact, many consumers may just ignore route_desc completely.
    
    Numerous feeds that are consumed by Google do have this notice and they
    are considered valid.
    
    We also update the comment for the notice.

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/RouteNameValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/RouteNameValidator.java
@@ -138,13 +138,10 @@ public class RouteNameValidator extends SingleEntityValidator<GtfsRoute> {
   }
 
   /**
-   * A {@code GtfsRoute} has identical value for `routes.route_desc` and
-   * `routes.route_long_name`{@code /}`routes`{@code /}route_short_name.
+   * A route has identical values for {@code routes.route_desc} and {@code route_long_name} or
+   * {@code route_short_name}.
    *
-   * <p>"Do not simply duplicate the name of the location."
-   * (http://gtfs.org/reference/static#routestxt)
-   *
-   * <p>Severity: {@code SeverityLevel.ERROR}
+   * <p>Severity: {@code SeverityLevel.WARNING}
    */
   static class SameNameAndDescriptionForRouteNotice extends ValidationNotice {
     SameNameAndDescriptionForRouteNotice(
@@ -157,7 +154,7 @@ public class RouteNameValidator extends SingleEntityValidator<GtfsRoute> {
               .put("routeDesc", routeDesc)
               .put("specifiedField", routeShortOrLongName)
               .build(),
-          SeverityLevel.ERROR);
+          SeverityLevel.WARNING);
     }
   }
 }
