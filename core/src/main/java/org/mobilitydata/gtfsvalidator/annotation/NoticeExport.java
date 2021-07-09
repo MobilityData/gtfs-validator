1036c0bdde5d0e773664faa4232168ce8169892c
lionel-nj <lionel@mobilitydata.org>
2021-07-09 13:46:14 -0400

    make NoticeExport annotation visible at runtime

--- core/src/main/java/org/mobilitydata/gtfsvalidator/annotation/NoticeExport.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/annotation/NoticeExport.java
@@ -1,7 +1,11 @@
 package org.mobilitydata.gtfsvalidator.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Annotation to be used on notice constructor. This specifies the constructor to be considered
  * while exporting notice information.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface NoticeExport {}
