429442d9ae6d091ed8d8f56e71fd75e6d5991de3
lionel-nj <lionel@mobilitydata.org>
2021-07-14 15:16:55 -0400

    fix license header

--- scripts/mobility-database-harvester/harvest_latest_versions.py
+++ scripts/mobility-database-harvester/harvest_latest_versions.py
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import dateutil.parser as dp
 import json
@@ -10,19 +22,6 @@ from google.cloud import storage
 ###############################################################################
 # This script harvests the latest versions of the sources listed in a catalog.
 # Made for Python 3.9. Requires the modules listed in requirements.txt.
-# Copyright 2021 MobilityData IO
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 ###############################################################################
 
 # API constants
@@ -127,7 +126,7 @@ def harvest_latest_versions(archives_ids, credentials):
                     blob_name=blob.name
                 )
                 latest_versions[archives_id] = archives_url
-    
+
     return latest_versions
 
 
