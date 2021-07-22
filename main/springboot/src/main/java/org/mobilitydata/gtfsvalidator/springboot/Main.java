619fe9328e8f59722749c391b1c1529ef136f343
lionel-nj <lionel@mobilitydata.org>
2021-07-22 12:35:50 -0400

    setup springboot entry point en REST controller

---
+++ main/springboot/src/main/java/org/mobilitydata/gtfsvalidator/springboot/Main.java
@@ -0,0 +1,15 @@
+package org.mobilitydata.gtfsvalidator.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot application entry point.
+ */
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+}
