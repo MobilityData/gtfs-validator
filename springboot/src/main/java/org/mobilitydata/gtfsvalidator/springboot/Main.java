23e6a1ad1c6df8deb3479d73bfbc20b1e75875e5
lionel-nj <lionel@mobilitydata.org>
2021-08-24 18:18:27 -0400

    apply goJF

--- springboot/src/main/java/org/mobilitydata/gtfsvalidator/springboot/Main.java
+++ springboot/src/main/java/org/mobilitydata/gtfsvalidator/springboot/Main.java
@@ -4,9 +4,7 @@ import com.google.common.flogger.FluentLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Spring boot application entry point.
- */
+/** Spring boot application entry point. */
 @SpringBootApplication
 public class Main {
 
@@ -15,10 +13,9 @@ public class Main {
    * incoming HTTP request is processed) and this web application relies entirely on {@code
    * JCommander} to parse query parameters. To be fully secure, this web application should run in a
    * highly secure or sandboxed environment. Also, HTTP communication is used and is inherently
-   * insecure.
-   * <a href=https://www.hostasean.com/http-vs-https-risks-of-not-using-an-ssl-certificate-on-your-website/>Risks
-   * of HTTP communications
-   * </a>
+   * insecure. <a
+   * href=https://www.hostasean.com/http-vs-https-risks-of-not-using-an-ssl-certificate-on-your-website/>Risks
+   * of HTTP communications </a>
    *
    * @param args CLI arguments
    */
