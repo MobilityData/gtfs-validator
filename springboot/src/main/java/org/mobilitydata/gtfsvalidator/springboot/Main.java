13fddb886b782a61db90bac234958e0b69d8854c
lionel-nj <lionel@mobilitydata.org>
2021-08-26 16:14:57 -0400

    update docs

--- springboot/src/main/java/org/mobilitydata/gtfsvalidator/springboot/Main.java
+++ springboot/src/main/java/org/mobilitydata/gtfsvalidator/springboot/Main.java
@@ -8,28 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
-  /**
-   * Before running this, the user should understand that: There isn't any authentication (i.e. any
-   * incoming HTTP request is processed) and this web application relies entirely on {@code
-   * JCommander} to parse query parameters. To be fully secure, this web application should run in a
-   * highly secure or sandboxed environment. Also, HTTP communication is used and is inherently
-   * insecure. <a
-   * href=https://www.hostasean.com/http-vs-https-risks-of-not-using-an-ssl-certificate-on-your-website/>Risks
-   * of HTTP communications </a>
-   *
-   * @param args CLI arguments
-   */
   public static void main(String[] args) {
     final FluentLogger logger = FluentLogger.forEnclosingClass();
 
-    logger.atSevere().log(
+    logger.atWarning().log(
         "Before running this, the user should understand that: There isn't any authentication "
             + "(i.e. any incoming HTTP request is processed) and this web application relies"
             + " entirely on JCommander to parse query parameters. To be fully secure, this web"
             + " application should run in a highly secure or sandboxed environment. Also, HTTP "
             + "communication is used and is inherently insecure. "
             + "Risks of HTTP communications are detailed at: "
-            + "https://www.hostasean.com/http-vs-https-risks-of-not-using-an-ssl-certificate-on-your-websiteRisks of HTTP communications");
+            + "https://www.hostasean.com/http-vs-https-risks-of-not-using-an-ssl-certificate-on-your-website.");
     SpringApplication.run(Main.class, args);
   }
 }
