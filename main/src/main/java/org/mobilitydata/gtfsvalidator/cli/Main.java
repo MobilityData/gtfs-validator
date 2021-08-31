187695ae64bbf89ffec9423595c47435011f00a3
lionel-nj <lionel@mobilitydata.org>
2021-08-31 13:30:13 -0400

    apply goJF

--- main/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/cli/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC, MobilityData IO
+ * Copyright 2020-2021 Google LLC, MobilityData IO
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,6 +118,7 @@ public class Main {
 
   /**
    * Prints validation metadata.
+   *
    * @param startNanos start time as nanoseconds
    * @param feedContainer the {@code GtfsFeedContainer}
    */
@@ -136,9 +137,12 @@ public class Main {
   }
 
   /**
-   * Closes a {@code GtfsInput}. Yields {@code IOError} if the {@code GtfsInput} could not be closed.
+   * Closes a {@code GtfsInput}. Yields {@code IOError} if the {@code GtfsInput} could not be
+   * closed.
+   *
    * @param gtfsInput the {@code GtfsInput} to close
-   * @param noticeContainer the {@code NoticeContainer} that will contain the {@code IOError} if the {@code GtfsInput} could not be closed.
+   * @param noticeContainer the {@code NoticeContainer} that will contain the {@code IOError} if the
+   *     {@code GtfsInput} could not be closed.
    */
   public static void closeGtfsInput(GtfsInput gtfsInput, NoticeContainer noticeContainer) {
     try {
@@ -151,9 +155,11 @@ public class Main {
 
   /**
    * Loads and validates GTFS feeds
+   *
    * @param validatorLoader the {@code ValidatorLoader} used in the process
    * @param feedLoader the {@code GtfsFeedLoader} used in the process
-   * @param noticeContainer the {@code NoticeContainer} that will contain {@code Notice}s related to the GTFS feed
+   * @param noticeContainer the {@code NoticeContainer} that will contain {@code Notice}s related to
+   *     the GTFS feed
    * @param gtfsInput the source of data
    * @param validationContext the {@code ValidationContext} do be used during validation
    * @return the {@code GtfsFeedContainer} used in the validation process
@@ -177,6 +183,7 @@ public class Main {
 
   /**
    * Performs parsing and sanity checks on CLI arguments.
+   *
    * @param argv the CLI arguments
    * @return the {@code Argument} generated after parsing the command line
    */
@@ -233,8 +240,8 @@ public class Main {
       Files.write(
           Paths.get(args.getOutputBase(), NOTICE_SCHEMA_JSON),
           gson.toJson(
-              NoticeSchemaGenerator.jsonSchemaForPackages(
-                  NoticeSchemaGenerator.DEFAULT_NOTICE_PACKAGES))
+                  NoticeSchemaGenerator.jsonSchemaForPackages(
+                      NoticeSchemaGenerator.DEFAULT_NOTICE_PACKAGES))
               .getBytes(StandardCharsets.UTF_8));
     } catch (IOException e) {
       logger.atSevere().withCause(e).log("Cannot store notice schema file");
@@ -243,7 +250,9 @@ public class Main {
 
   /**
    * Creates a {@code GtfsInput}
-   * @param args the {@code Argument} to be used to retrieve information needed to the creation of the {@code GtfsInput}
+   *
+   * @param args the {@code Argument} to be used to retrieve information needed to the creation of
+   *     the {@code GtfsInput}
    * @return the {@code GtfsInput} generated after
    * @throws IOException in case of error while loading a file
    * @throws URISyntaxException in case of error in the {@code URL} syntax
