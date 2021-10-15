41aa813e10e1b20ce7980bd49dc8d6bef8103c74
lionel-nj <lionel@mobilitydata.org>
2021-10-15 13:45:35 -0400

    try not to mangle backslashes

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 array_string=$*
 echo $array_string
-IFS=" " read -a my_array <<< $array_string
+IFS=" " read -a -r my_array <<< $array_string
 echo ${#my_array[@]}
 
 for el in "${my_array[@]}"
 do
    echo "$el"
+#   ID=jq '.id' <<< "$el"
+#   URL=jq '.url' <<< "$el"
    # or do whatever with individual element of the array
 done
