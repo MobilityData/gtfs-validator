29c1effe3d7297216c9e7b2c89904f3e694f95be
lionel-nj <lionel@mobilitydata.org>
2021-10-15 13:21:52 -0400

    use double quotes

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 echo 1
 array_string=$*
 echo 2
-echo $array_string
+echo "$array_string"
 echo 3
-IFS=" " read -a my_array <<< $array_string
+IFS=" " read -a my_array <<< "$array_string"
 echo 4
 echo ${#my_array[@]}
 echo "end"
