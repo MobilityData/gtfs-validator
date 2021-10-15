6fe231e1db0bb661de5d26d9ed4f07ef0add40e4
lionel-nj <lionel@mobilitydata.org>
2021-10-15 15:37:41 -0400

    use continue

--- run.sh
+++ run.sh
@@ -14,8 +14,8 @@ do
    URL=$(jq '.url' <<< "$el")
    path_name=${ID//\"/}
    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $URL)
-   if [ "$response" != "404" ];
-   then
-    java -Xmx8G -Xms8G -jar gtfs-validator*.jar --url $URL --output_base output/$path_name
+   if [ $response == 404 ]; then
+     continue
    fi
+   java -Xmx8G -Xms8G -jar gtfs-validator*.jar --url $URL --output_base output/$path_name
 done
