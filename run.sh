100c937e7fe1edc0d931516713245b517c711fa6
lionel-nj <lionel@mobilitydata.org>
2021-10-15 14:44:50 -0400

    enable continuation on error

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 closing_curly_bracket="}"
 array_string=$*
-echo $array_string
 IFS=" " read -a my_array <<< $array_string
-echo ${#my_array[@]}
 for el in "${my_array[@]}"
 do
    el=${el//id/\"id\"}
@@ -14,9 +12,6 @@ do
 
    ID=$(jq '.id' <<< "$el")
    URL=$(jq '.url' <<< "$el")
-   echo $ID
-   echo $URL
    path_name=${ID//\"/}
    java -jar gtfs-validator*.jar --url $URL --output_base output/$path_name -t 4
-   # or do whatever with individual element of the array
 done
