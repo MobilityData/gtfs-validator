07381553b550519cde1f111a228892c8a5bf0b47
lionel-nj <lionel@mobilitydata.org>
2021-10-15 12:55:14 -0400

    treat array in shell script

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 my_array=$1
-#echo "$my_array"[@]
-echo ${#my_array[@]}
+echo "$my_array"
+echo "/n"
+echo "/n"
+IFS=" " read -a -r data_array <<< "$my_array"
+echo "/n"
+echo "/n"
+echo ${#data_array[@]}
