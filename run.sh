1444389314cd67ab283c559acd14622691786aed
lionel-nj <lionel@mobilitydata.org>
2021-10-15 14:10:09 -0400

    make input a valid json

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+closing_curly_bracket="}"
 array_string=$*
 echo $array_string
 IFS=" " read -a my_array <<< $array_string
@@ -7,7 +8,14 @@ echo ${#my_array[@]}
 for el in "${my_array[@]}"
 do
    echo "$el"
-#   ID=jq '.id' <<< "$el"
-#   URL=jq '.url' <<< "$el"
+   el=${el//id/\"id\"}
+   el=${el//url/\"url\"}
+   el=${el//,/\",}
+   el=${el//\":/\":\"}
+   el=${el//$closing_curly_bracket/\"$closing_curly_bracket}
+   ID=jq '.id' <<< "$el"
+   URL=jq '.url' <<< "$el"
+   echo $ID
+   echo $URL
    # or do whatever with individual element of the array
 done
