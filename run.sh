5fea3deecc03540f92e0e3ed1683e080d5ff414b
lionel-nj <lionel@mobilitydata.org>
2021-10-15 14:16:03 -0400

    remove extra prints

--- run.sh
+++ run.sh
@@ -6,14 +6,12 @@ IFS=" " read -a my_array <<< $array_string
 echo ${#my_array[@]}
 for el in "${my_array[@]}"
 do
-   echo "$el"
    el=${el//id/\"id\"}
    el=${el//url/\"url\"}
    el=${el//,/\",}
    el=${el//\":/\":\"}
    el=${el//$closing_curly_bracket/\"$closing_curly_bracket}
 
-   echo $el
    ID=$(jq '.id' <<< "$el")
    URL=$(jq '.url' <<< "$el")
    echo $ID
