4771c881c7b70100dd3270c3a159a66599ddd8f3
lionel-nj <lionel@mobilitydata.org>
2021-10-15 13:29:51 -0400

    iterate over array items

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo 1
 array_string=$*
-echo 2
 echo $array_string
-echo 3
 IFS=" " read -a my_array <<< $array_string
-echo 4
 echo ${#my_array[@]}
-echo "end"
+
+for el in "${my_array[@]}"
+do
+   echo "$el"
+   # or do whatever with individual element of the array
+done
