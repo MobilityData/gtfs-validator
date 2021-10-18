a5964865d6311bee9751dbaddfd04658ed3d000a
lionel-nj <lionel@mobilitydata.org>
2021-10-18 13:29:54 -0400

    remove debug prints

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 closing_curly_bracket="}"
 array_string=$*
 IFS=" " read -a my_array <<< $array_string
-echo "${my_array[@]}"
 for el in "${my_array[@]}"
 do
    el=${el//\{id/\{\"id\"}
@@ -13,14 +12,6 @@ do
 
    ID=$(jq '.id' <<< "$el")
    URL=$(jq '.url' <<< "$el")
-   echo $ID
-   echo $URL
    path_name=${ID//\"/}
-   response=$(curl --write-out '%{http_code}' --silent --output /dev/null $URL)
-   if [ $response == 404 ]; then
-     continue
-   fi
-   echo $path_name
-   echo $URL
    java -Xmx8G -Xms8G -jar gtfs-validator*.jar --url $URL --output_base output/$path_name
 done
