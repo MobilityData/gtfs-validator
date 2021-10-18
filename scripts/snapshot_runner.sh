8fb02b774979e9d1f1890308e8569bf336ffbb05
lionel-nj <lionel@mobilitydata.org>
2021-10-18 13:41:54 -0400

    rename variable

--- scripts/snapshot_runner.sh
+++ scripts/snapshot_runner.sh
@@ -2,16 +2,16 @@
 closing_curly_bracket="}"
 raw_queue_string=$*
 IFS=" " read -a queue <<< $raw_queue_string
-for el in "${queue[@]}"
+for item in "${queue[@]}"
 do
-   el=${el//\{id/\{\"id\"}
-   el=${el//,/\",}
-   el=${el//\,url/\,\"url\"}
-   el=${el//\":/\":\"}
-   el=${el//$closing_curly_bracket/\"$closing_curly_bracket}
+   item=${item//\{id/\{\"id\"}
+   item=${item//,/\",}
+   item=${item//\,url/\,\"url\"}
+   item=${item//\":/\":\"}
+   item=${item//$closing_curly_bracket/\"$closing_curly_bracket}
 
-   ID=$(jq '.id' <<< "$el")
-   URL=$(jq '.url' <<< "$el")
+   ID=$(jq '.id' <<< "$item")
+   URL=$(jq '.url' <<< "$item")
    path_name=${ID//\"/}
    java -Xmx8G -Xms8G -jar gtfs-validator*.jar --url $URL --output_base output/$path_name
 done
