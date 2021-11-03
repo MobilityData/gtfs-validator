5c3d10a64c3a1deb5994b9eade5b2a9411532e22
lionel-nj <lionel@mobilitydata.org>
2021-11-03 17:09:41 -0400

    all reports in the same directory

--- scripts/queue_runner.sh
+++ scripts/queue_runner.sh
@@ -13,7 +13,7 @@ do
    ID=$(jq '.id' <<< "$item")
    URL=$(jq '.url' <<< "$item")
    path_name=${ID//\"/}
-   java -Xmx8G -Xms8G -jar gtfs-validator*SNAPSHOT_cli.jar --url $URL --output_base $OUTPUT_BASE/output/v3/$path_name --validation_report_name latest.json --system_errors_report_name latest_errors.json
-   java -Xmx8G -Xms8G -jar gtfs-validator*v2_cli.jar --url $URL --output_base $OUTPUT_BASE/output/v2/$path_name -f us-test
+   java -Xmx8G -Xms8G -jar gtfs-validator*SNAPSHOT_cli.jar --url $URL --output_base $OUTPUT_BASE/output/$path_name --validation_report_name v3_latest.json --system_errors_report_name v3_latest_errors.json
+   java -Xmx8G -Xms8G -jar gtfs-validator*v2_cli.jar --url $URL --output_base $OUTPUT_BASE/output/$path_name -f us-test
    wait
 done
