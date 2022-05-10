eb431520389c44696f010d83ba4fd30b9d07b666
Maxime Armstrong <info@maximearmstrong.com>
2022-05-10 11:13:21 -0400

    Revert "Fixes tests in NoticeViewTest.java post revire [acceptance test skip]"
    This reverts commit cfe4b707ced0ffeb82125dca25d1d1de5f8a4140.

--- main/src/test/java/org/mobilitydata/gtfsvalidator/report/model/NoticeViewTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/report/model/NoticeViewTest.java
@@ -3,54 +3,40 @@ package org.mobilitydata.gtfsvalidator.report.model;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import com.google.gson.JsonPrimitive;
 import org.junit.Test;
 import org.mobilitydata.gtfsvalidator.notice.MissingRequiredFieldNotice;
 import org.mobilitydata.gtfsvalidator.notice.Notice;
-import org.mobilitydata.gtfsvalidator.notice.SeverityLevel;
 
 public class NoticeViewTest {
-  private static final String FILENAME = "filename";
-  private static final String CSV_ROW_NUMBER = "csvRowNumber";
-  private static final String FIELD_NAME = "fieldName";
-
-  private static final String FILENAME_VALUE = "test.txt";
-  private static final int CSV_ROW_NUMBER_VALUE = 1;
-  private static final String FIELD_NAME_VALUE = "field";
-
-  private static final Notice notice = new MissingRequiredFieldNotice(FILENAME_VALUE, CSV_ROW_NUMBER_VALUE, FIELD_NAME_VALUE);
+  private static final Notice notice = new MissingRequiredFieldNotice("test.txt", 1, "field");
   private static final NoticeView noticeView = new NoticeView(notice);
-
-  private static final String MISSING_REQUIRED_FIELD_NOTICE_NAME = "MissingRequiredFieldNotice";
-  private static final String MISSING_REQUIRED_FIELD_NOTICE_CODE = "missing_required_field";
-  private static final List<String> MISSING_REQUIRED_FIELD_NOTICE_FIELDS = new ArrayList<>(List.of(FILENAME, CSV_ROW_NUMBER, FIELD_NAME));
 
   @Test
   public void getNameTest() {
-    assertEquals(noticeView.getName(), MISSING_REQUIRED_FIELD_NOTICE_NAME);
+    assertEquals(noticeView.getName(), notice.getClass().getSimpleName());
   }
 
   @Test
   public void getFieldsTest() {
-    assertEquals(noticeView.getFields(), MISSING_REQUIRED_FIELD_NOTICE_FIELDS);
+    assertEquals(
+        noticeView.getFields(), new ArrayList<>(notice.getContext().getAsJsonObject().keySet()));
   }
 
   @Test
   public void getValueForFieldTest() {
-    assertEquals(noticeView.getValueForField(FILENAME), new JsonPrimitive(FILENAME_VALUE));
-    assertEquals(noticeView.getValueForField(CSV_ROW_NUMBER), new JsonPrimitive(CSV_ROW_NUMBER_VALUE));
-    assertEquals(noticeView.getValueForField(FIELD_NAME), new JsonPrimitive(FIELD_NAME_VALUE));
+    for (String field : noticeView.getFields()) {
+      assertEquals(
+          noticeView.getValueForField(field), notice.getContext().getAsJsonObject().get(field));
+    }
   }
 
   @Test
   public void getSeverityLevelTest() {
-    assertEquals(noticeView.getSeverityLevel(), SeverityLevel.ERROR);
+    assertEquals(noticeView.getSeverityLevel(), notice.getSeverityLevel());
   }
 
   @Test
   public void getCodeTest() {
-    assertEquals(noticeView.getCode(), MISSING_REQUIRED_FIELD_NOTICE_CODE);
+    assertEquals(noticeView.getCode(), notice.getCode());
   }
 }
