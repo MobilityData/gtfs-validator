1e30846a1bb5e9b3c2d261c5ec42da64a889a912
Maxime Armstrong <info@maximearmstrong.com>
2022-05-10 15:35:29 -0400

    Fixes tests in NoticeViewTest.java post review [acceptance test skip]

--- main/src/test/java/org/mobilitydata/gtfsvalidator/report/model/NoticeViewTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/report/model/NoticeViewTest.java
@@ -23,19 +23,15 @@ public class NoticeViewTest {
       new MissingRequiredFieldNotice(FILENAME_VALUE, CSV_ROW_NUMBER_VALUE, FIELD_NAME_VALUE);
   private static final NoticeView noticeView = new NoticeView(notice);
 
-  private static final List<String> MISSING_REQUIRED_FIELD_NOTICE_FIELDS =
-      new ArrayList<>(List.of(FILENAME, CSV_ROW_NUMBER, FIELD_NAME));
-  private static final String MISSING_REQUIRED_FIELD_NOTICE_NAME = "MissingRequiredFieldNotice";
-  private static final String MISSING_REQUIRED_FIELD_NOTICE_CODE = "missing_required_field";
-
   @Test
   public void getNameTest() {
-    assertEquals(noticeView.getName(), MISSING_REQUIRED_FIELD_NOTICE_NAME);
+    assertEquals(noticeView.getName(), "MissingRequiredFieldNotice");
   }
 
   @Test
   public void getFieldsTest() {
-    assertEquals(noticeView.getFields(), MISSING_REQUIRED_FIELD_NOTICE_FIELDS);
+    assertEquals(
+        noticeView.getFields(), new ArrayList<>(List.of(FILENAME, CSV_ROW_NUMBER, FIELD_NAME)));
   }
 
   @Test
@@ -53,6 +49,6 @@ public class NoticeViewTest {
 
   @Test
   public void getCodeTest() {
-    assertEquals(noticeView.getCode(), MISSING_REQUIRED_FIELD_NOTICE_CODE);
+    assertEquals(noticeView.getCode(), "missing_required_field");
   }
 }
