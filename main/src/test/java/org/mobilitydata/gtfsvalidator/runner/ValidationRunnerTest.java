e64e23435080c5e171b2d6d126b223b6355e27d2
Sean Barbeau <sjbarbeau@gmail.com>
2022-05-25 11:54:04 -0400

    test: Add path parsing tests

---
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/runner/ValidationRunnerTest.java
@@ -0,0 +1,42 @@
+package org.mobilitydata.gtfsvalidator.runner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mobilitydata.gtfsvalidator.input.CountryCode;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertThrows;
+
+@RunWith(JUnit4.class)
+public class ValidationRunnerTest {
+
+    private static ValidationRunnerConfig buildConfig(String gtfsDirectory) {
+        ValidationRunnerConfig.Builder config = ValidationRunnerConfig.builder();
+        config.setGtfsSource(Path.of(gtfsDirectory).toUri());
+        config.setOutputDirectory(Path.of(""));
+        config.setNumThreads(1);
+        config.setCountryCode(CountryCode.forStringOrUnknown(""));
+        return config.build();
+    }
+
+    @Test
+    public void createGtfsInput_WindowsPath_valid() throws IOException, URISyntaxException {
+        ValidationRunnerConfig config = buildConfig("C:\\projects\\gtfs-validator\\non-existent-file.zip");
+
+        // We are testing path parsing here only. We expect a FileNotFoundException but NOT a InvalidPathException.
+        assertThrows(FileNotFoundException.class, () -> ValidationRunner.createGtfsInput(config));
+    }
+
+    @Test
+    public void createGtfsInput_LinuxPath_valid() throws IOException, URISyntaxException {
+        ValidationRunnerConfig config = buildConfig("/Users/me/gtfs-validator/non-existent-file.zip");
+        // We are testing path parsing here only. We expect a FileNotFoundException but NOT a InvalidPathException.
+        assertThrows(FileNotFoundException.class, () -> ValidationRunner.createGtfsInput(config));
+    }
+}
