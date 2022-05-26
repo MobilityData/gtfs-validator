42c6dd34bfbe346fdfcf2539a5a4634c5d543283
Sean Barbeau <sjbarbeau@gmail.com>
2022-05-26 11:52:23 -0400

    test: Add path parsing tests (#1155)
    * Per issue #1139, refactor how versions are handled in project.
    
    Currently, project version info is passed in via a `versionTag` env variable,
    which is only specified in the Github workflow environment.  Consequently,
    `project.version` ends up being `null` when run locally.  This causes
    weirdness for tools that expect a valid version number.
    
    This change switches the project to use the axion-release plugin, which
    determines the project version by looking at the git repo's tags.
    
    Along with that switch, I've moved the project group and version
    specification into the root build.gradle allprojects {} such that
    these values do not need to be specified in each sub-poject.
    
    I also did some related clean-up for our jitpack configuration (and
    Maven repository modules, should we every decide to publish them).
    I believe our current jitpack config doesn't actually work correctly
    because we only publish artifacts from the main package, but not core.
    
    You can see this in practice when looking at:
    https://jitpack.io/com/github/MobilityData/gtfs-validator/3.0.1/gtfs-validator-3.0.1.pom
    
    Notice how it has a dependency on the `core` module but we don't
    actually publish it anywhere.  If you actually attempt to use our
    project as a jitpack dependency, it will fail.
    
    The change was to also publish the core module and to specify more
    natural artifact names.
    
    * Remove references to versionTag env variable in workflows.  It is no
    longer needed now that project version is determined within Gradle
    directly.
    
    Also fixup references to shadow jars, since they no longer include
    the versionTag in the filename (also _cli => -cli).
    
    * Restore versionTag behavior for master branch.
    
    We can't update this code until the PR actually goes in on the master
    branch.
    
    * Fix typo in yml.
    
    * Download artifacts to separate directories to avoid eventual name conflicts.
    
    * ci: Add end-to-end Windows check
    
    * Remove all references to versionTag from docker.yml
    
    I also cleaned up some other variables from the docker config that don't seem to be used.
    
    * Remove all references to versionTag from test_pack_doc.yml
    
    * Add back `tags` output variable, which was improperly removed in the previous commit.
    
    * fix: Use bash on Windows (instead of PowerShell) as well as Linux
    
    * break: Intentionally break path handling on Windows to test CI
    
    * Revert "fix: Use bash on Windows (instead of PowerShell) as well as Linux"
    
    This reverts commit 31f95a098d0993450ef1e51933cd53be59021b23.
    
    * test: Add path parsing tests
    
    * ci: Remove Windows end-to-end CI job
    
    * chore: Fix whitespace
    
    * chore: Fix code formatting with ./gradlew goJF
    
    * docs: Add issue reference
    
    * fix: Remove the bug from #1158 re-introduced in this PR for testing
    
    Co-authored-by: Brian Ferris <bdferris+v2@google.com>

---
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/runner/ValidationRunnerTest.java
@@ -0,0 +1,44 @@
+package org.mobilitydata.gtfsvalidator.runner;
+
+import static org.junit.Assert.assertThrows;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mobilitydata.gtfsvalidator.input.CountryCode;
+
+@RunWith(JUnit4.class)
+public class ValidationRunnerTest {
+
+  private static ValidationRunnerConfig buildConfig(String gtfsDirectory) {
+    ValidationRunnerConfig.Builder config = ValidationRunnerConfig.builder();
+    config.setGtfsSource(Path.of(gtfsDirectory).toUri());
+    config.setOutputDirectory(Path.of(""));
+    config.setNumThreads(1);
+    config.setCountryCode(CountryCode.forStringOrUnknown(""));
+    return config.build();
+  }
+
+  @Test
+  public void createGtfsInput_WindowsPath_valid() throws IOException, URISyntaxException {
+    ValidationRunnerConfig config =
+        buildConfig("C:\\projects\\gtfs-validator\\non-existent-file.zip");
+
+    // We are testing path parsing here only. We expect a FileNotFoundException but NOT a
+    // InvalidPathException. This should catch issues such as #1158.
+    assertThrows(FileNotFoundException.class, () -> ValidationRunner.createGtfsInput(config));
+  }
+
+  @Test
+  public void createGtfsInput_LinuxPath_valid() throws IOException, URISyntaxException {
+    ValidationRunnerConfig config = buildConfig("/Users/me/gtfs-validator/non-existent-file.zip");
+
+    // We are testing path parsing here only. We expect a FileNotFoundException but NOT a
+    // InvalidPathException. This should catch issues such as #1158.
+    assertThrows(FileNotFoundException.class, () -> ValidationRunner.createGtfsInput(config));
+  }
+}
