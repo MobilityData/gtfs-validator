3cd3df263b9b9a481996e931394a33aaec9ce54f
Brian Ferris <bdferris+v2@google.com>
2022-06-03 16:31:26 +0000

    Support different resolution icons within the app JFrame.

---
+++ app/pkg/src/main/icons/svg2jframe_icons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Helper script for constructing a Windows icon bundle.  Requires
+# install of ImageMagik for `convert` command.
+
+set -e
+
+BASEDIR=$(dirname "$0")
+SVG=$BASEDIR/Icon.svg
+
+PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+RESOURCES_DIR=$PROJECT_ROOT_DIR/app/gui/src/main/resources/org/mobilitydata/gtfsvalidator/app/gui
+
+CONFIGS="
+icon_16x16.png,16x16
+icon_32x32.png,32x32
+icon_48x48.png,48x48
+"
+
+for CONFIG in $CONFIGS; do
+  FILENAME=$(echo $CONFIG | cut -d, -f1)
+  SIZE=$(echo $CONFIG | cut -d, -f2)
+  echo $FILENAME
+  convert -density 1200 -background none -resize $SIZE $SVG $RESOURCES_DIR/$FILENAME
+done
