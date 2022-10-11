ddc4937adc6a8293fa60734ed4510d24129d2872
Brian Ferris <bdferris+v2@google.com>
2022-10-11 15:12:43 -0700

    More transfers.txt validation.

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TransfersTripReferenceValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TransfersTripReferenceValidator.java
@@ -1,5 +1,6 @@
 package org.mobilitydata.gtfsvalidator.validator;
 
+import com.google.common.collect.ImmutableSet;
 import java.util.List;
 import java.util.Optional;
 import javax.inject.Inject;
@@ -7,6 +8,9 @@ import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.SeverityLevel;
 import org.mobilitydata.gtfsvalidator.notice.ValidationNotice;
+import org.mobilitydata.gtfsvalidator.table.GtfsLocationType;
+import org.mobilitydata.gtfsvalidator.table.GtfsStop;
+import org.mobilitydata.gtfsvalidator.table.GtfsStopTableContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsStopTime;
 import org.mobilitydata.gtfsvalidator.table.GtfsStopTimeTableContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsTransfer;
@@ -21,15 +25,18 @@ public class TransfersTripReferenceValidator extends FileValidator {
   private final GtfsTransferTableContainer transfersContainer;
   private final GtfsTripTableContainer tripsContainer;
   private final GtfsStopTimeTableContainer stopTimeContainer;
+  private final GtfsStopTableContainer stopsContainer;
 
   @Inject
   public TransfersTripReferenceValidator(
       GtfsTransferTableContainer transfersContainer,
       GtfsTripTableContainer tripsContainer,
-      GtfsStopTimeTableContainer stopTimeContainer) {
+      GtfsStopTimeTableContainer stopTimeContainer,
+      GtfsStopTableContainer stopsContainer) {
     this.transfersContainer = transfersContainer;
     this.tripsContainer = tripsContainer;
     this.stopTimeContainer = stopTimeContainer;
+    this.stopsContainer = stopsContainer;
   }
 
   @Override
@@ -74,6 +81,7 @@ public class TransfersTripReferenceValidator extends FileValidator {
     }
     Optional<GtfsTrip> optTrip = tripsContainer.byTripId(tripId.get());
     if (optTrip.isEmpty()) {
+      // The foreign key reference is validated elsewhere.
       return;
     }
     GtfsTrip trip = optTrip.get();
@@ -85,12 +93,32 @@ public class TransfersTripReferenceValidator extends FileValidator {
       }
     }
     if (stopId.isPresent()) {
-      List<GtfsStopTime> stopTimes = stopTimeContainer.byTripId(tripId.get());
-      if (!stopTimes.stream().anyMatch((st) -> st.stopId().equals(stopId.get()))) {
-        noticeContainer.addValidationNotice(
-            new TransferTripReferenceNotice(
-                entity.csvRowNumber(), tripFieldName, tripId.get(), stopFieldName, stopId.get()));
-      }
+      validateTripStopReference(
+          entity, tripFieldName, tripId, stopFieldName, stopId.get(), noticeContainer);
+    }
+  }
+
+  private void validateTripStopReference(
+      GtfsTransfer entity,
+      String tripFieldName,
+      Optional<String> tripId,
+      String stopFieldName,
+      String stopId,
+      NoticeContainer noticeContainer) {
+    Optional<GtfsStop> optStop = stopsContainer.byStopId(stopId);
+    if (optStop.isEmpty()) {
+      // The foreign key reference is validated elsewhere.
+      return;
+    }
+    ImmutableSet<GtfsStop> stops = expandStationIfNeeded(optStop.get());
+    ImmutableSet<String> ids =
+        stops.stream().map(GtfsStop::stopId).collect(ImmutableSet.toImmutableSet());
+
+    List<GtfsStopTime> stopTimes = stopTimeContainer.byTripId(tripId.get());
+    if (!stopTimes.stream().anyMatch((st) -> ids.contains(st.stopId()))) {
+      noticeContainer.addValidationNotice(
+          new TransferTripReferenceNotice(
+              entity.csvRowNumber(), tripFieldName, tripId.get(), stopFieldName, stopId));
     }
   }
 
@@ -98,10 +126,20 @@ public class TransfersTripReferenceValidator extends FileValidator {
     return hasValue ? Optional.of(value) : Optional.empty();
   }
 
+  private ImmutableSet<GtfsStop> expandStationIfNeeded(GtfsStop stop) {
+    if (stop.locationType() == GtfsLocationType.STOP) {
+      return ImmutableSet.of(stop);
+    } else if (stop.locationType() == GtfsLocationType.STATION) {
+      List<GtfsStop> stops = stopsContainer.byParentStation(stop.stopId());
+      return ImmutableSet.copyOf(stops);
+    } else {
+      // Invalid stop location types are validated elsewhere.
+      return ImmutableSet.of();
+    }
+  }
+
   public static class TransferTripReferenceNotice extends ValidationNotice {
-
     private final long csvRowNumber;
-
     private final String tripFieldName;
     private final String tripFieldValue;
     private final String referenceFieldName;
