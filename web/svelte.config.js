7f0f80265282cb8334c494f8482ab384559163db
Ryon Coleman <ryon55@gmail.com>
2022-12-22 21:52:42 +0000

    feat: switch to "static" svelte adapater

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: '200.html',
+    }),
   },
   preprocess: vitePreprocess(),
+  prerender: { entries: [] },
 };
 
 export default config;
