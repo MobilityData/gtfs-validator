06f4b329540d1fe1337ce76df59faae743a69547
Ryon Coleman <ryon55@gmail.com>
2023-02-01 22:27:00 +0000

    feat: add data retention info to rooter

--- web/client/src/routes/+layout.svelte
+++ web/client/src/routes/+layout.svelte
@@ -31,5 +31,16 @@
         by&nbsp;<a href="https://mobilitydata.org/">MobilityData</a>.
       </span>
     </p>
+
+    <p
+      class="text-gray-300 mt-16 mb-8 text-sm hover:text-gray-500 transition-colors"
+    >
+      For performance optimization reasons, any uploaded GTFS feeds can be
+      stored on MobilityData’s servers for 90 days. These feeds are not made
+      publicly available nor shared with any third party, unless required to
+      solve hosting issues. Our servers are located in the United States. If you
+      have any concerns with this, please contact us at
+      <a href="mailto:hello@mobilitydata.org">hello@mobilitydata.org</a>.
+    </p>
   </div>
 </footer>
