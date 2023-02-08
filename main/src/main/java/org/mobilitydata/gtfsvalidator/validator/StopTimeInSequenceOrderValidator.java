7caf69fc9895ea609d7bdd003e5333a3b76e2118
Alexei Ababilov <aababilov@google.com>
2023-02-08 13:14:10 +1100

    Format Java files

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/StopTimeInSequenceOrderValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/StopTimeInSequenceOrderValidator.java
@@ -31,11 +31,12 @@ import org.mobilitydata.gtfsvalidator.type.GtfsTime;
  * Validates that fields {@code arrival_time}, {@code departure_time} and {@code
  * shape_dist_traveled} are non-decreasing along each trip.
  *
- * <p> Generated notices:
- *  <ul>
- *    <li> {@link TripWithOutOfOrderArrivalTimeNotice}
- *    <li> {@link TripWithOutOfOrderDepartureTimeNotice}
- *    <li> {@link TripWithOutOfOrderShapeDistTraveledNotice}
+ * <p>Generated notices:
+ *
+ * <ul>
+ *   <li>{@link TripWithOutOfOrderArrivalTimeNotice}
+ *   <li>{@link TripWithOutOfOrderDepartureTimeNotice}
+ *   <li>{@link TripWithOutOfOrderShapeDistTraveledNotice}
  * </ul>
  */
 @GtfsValidator
--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/StopTimeInSequenceOrderValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/StopTimeInSequenceOrderValidatorTest.java
@@ -37,7 +37,7 @@ public final class StopTimeInSequenceOrderValidatorTest {
   private static List<ValidationNotice> generateNotices(List<GtfsStopTime> stopTimes) {
     NoticeContainer noticeContainer = new NoticeContainer();
     new StopTimeInSequenceOrderValidator(
-        GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer))
+            GtfsStopTimeTableContainer.forEntities(stopTimes, noticeContainer))
         .validate(noticeContainer);
     return noticeContainer.getValidationNotices();
   }
@@ -45,14 +45,15 @@ public final class StopTimeInSequenceOrderValidatorTest {
   @Test
   public void outOfOrder_yieldsNotice() {
     ImmutableList<GtfsStopTime> stopTimes =
-        ImmutableList.of(new GtfsStopTime.Builder()
-                             .setCsvRowNumber(2)
-                             .setTripId("trip1")
-                             .setStopSequence(1)
-                             .setDepartureTime(GtfsTime.fromString("08:00:00"))
-                             .setArrivalTime(GtfsTime.fromString("08:00:00"))
-                             .setShapeDistTraveled(10.0)
-                             .build(),
+        ImmutableList.of(
+            new GtfsStopTime.Builder()
+                .setCsvRowNumber(2)
+                .setTripId("trip1")
+                .setStopSequence(1)
+                .setDepartureTime(GtfsTime.fromString("08:00:00"))
+                .setArrivalTime(GtfsTime.fromString("08:00:00"))
+                .setShapeDistTraveled(10.0)
+                .build(),
             new GtfsStopTime.Builder()
                 .setCsvRowNumber(3)
                 .setTripId("trip1")
@@ -62,7 +63,8 @@ public final class StopTimeInSequenceOrderValidatorTest {
                 .setShapeDistTraveled(5.0)
                 .build());
     assertThat(generateNotices(stopTimes))
-        .containsExactly(new TripWithOutOfOrderArrivalTimeNotice(stopTimes.get(1)),
+        .containsExactly(
+            new TripWithOutOfOrderArrivalTimeNotice(stopTimes.get(1)),
             new TripWithOutOfOrderDepartureTimeNotice(stopTimes.get(1)),
             new TripWithOutOfOrderShapeDistTraveledNotice(stopTimes.get(1)));
   }
@@ -70,14 +72,15 @@ public final class StopTimeInSequenceOrderValidatorTest {
   @Test
   public void inOrder_yieldsNoNotice() {
     ImmutableList<GtfsStopTime> stopTimes =
-        ImmutableList.of(new GtfsStopTime.Builder()
-                             .setCsvRowNumber(2)
-                             .setTripId("trip1")
-                             .setStopSequence(1)
-                             .setDepartureTime(GtfsTime.fromString("08:00:00"))
-                             .setArrivalTime(GtfsTime.fromString("08:00:00"))
-                             .setShapeDistTraveled(10.0)
-                             .build(),
+        ImmutableList.of(
+            new GtfsStopTime.Builder()
+                .setCsvRowNumber(2)
+                .setTripId("trip1")
+                .setStopSequence(1)
+                .setDepartureTime(GtfsTime.fromString("08:00:00"))
+                .setArrivalTime(GtfsTime.fromString("08:00:00"))
+                .setShapeDistTraveled(10.0)
+                .build(),
             new GtfsStopTime.Builder()
                 .setCsvRowNumber(3)
                 .setTripId("trip1")
@@ -91,24 +94,29 @@ public final class StopTimeInSequenceOrderValidatorTest {
 
   @Test
   public void skippedValues_yieldsNoNotice() {
-    ImmutableList<GtfsStopTime> stopTimes = ImmutableList.of(
-        new GtfsStopTime.Builder()
-            .setCsvRowNumber(2)
-            .setTripId("trip1")
-            .setStopSequence(1)
-            .setDepartureTime(GtfsTime.fromString("08:00:00"))
-            .setArrivalTime(GtfsTime.fromString("08:00:00"))
-            .setShapeDistTraveled(10.0)
-            .build(),
-        new GtfsStopTime.Builder().setCsvRowNumber(3).setTripId("trip1").setStopSequence(2).build(),
-        new GtfsStopTime.Builder()
-            .setCsvRowNumber(4)
-            .setTripId("trip1")
-            .setStopSequence(3)
-            .setDepartureTime(GtfsTime.fromString("09:00:00"))
-            .setArrivalTime(GtfsTime.fromString("09:00:00"))
-            .setShapeDistTraveled(20.0)
-            .build());
+    ImmutableList<GtfsStopTime> stopTimes =
+        ImmutableList.of(
+            new GtfsStopTime.Builder()
+                .setCsvRowNumber(2)
+                .setTripId("trip1")
+                .setStopSequence(1)
+                .setDepartureTime(GtfsTime.fromString("08:00:00"))
+                .setArrivalTime(GtfsTime.fromString("08:00:00"))
+                .setShapeDistTraveled(10.0)
+                .build(),
+            new GtfsStopTime.Builder()
+                .setCsvRowNumber(3)
+                .setTripId("trip1")
+                .setStopSequence(2)
+                .build(),
+            new GtfsStopTime.Builder()
+                .setCsvRowNumber(4)
+                .setTripId("trip1")
+                .setStopSequence(3)
+                .setDepartureTime(GtfsTime.fromString("09:00:00"))
+                .setArrivalTime(GtfsTime.fromString("09:00:00"))
+                .setShapeDistTraveled(20.0)
+                .build());
     assertThat(generateNotices(stopTimes)).isEmpty();
   }
 }
