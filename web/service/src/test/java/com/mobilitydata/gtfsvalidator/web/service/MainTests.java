125a30a36eff3e15a62478686387e67227da2323
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2023-03-20 15:29:53 -0400

    fix: spring boot tests running due to missing credential files

--- web/service/src/test/java/com/mobilitydata/gtfsvalidator/web/service/MainTests.java
+++ web/service/src/test/java/com/mobilitydata/gtfsvalidator/web/service/MainTests.java
@@ -2,8 +2,10 @@ package org.mobilitydata.gtfsvalidator.web.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MainTests {
 
   @Test
