09d088271efbdaf8b2aafd237b4802ebea7b57fe
Brian Ferris <bdferris+v2@google.com>
2023-03-31 06:18:21 -0700

    feat: Initial support for GTFS Fares v2 - fare media (#1305)
    * Initial entry for fare payment type schema and validation.
    
    * Update to reflect new fare_payment_options.txt naming.
    
    * Update to reflect fare payment option group discussion.
    
    * Update to match fare_media.txt proposal.
    
    * fare medium => fare media
    
    * Clean up a few last usages of `medium`.
    
    * Documentation.
    
    * Fix link in RULES.md
    
    Co-authored-by: isabelle-dr <isabelle@mobilitydata.org>
    
    * Fix link in RULES.md
    
    Co-authored-by: isabelle-dr <isabelle@mobilitydata.org>
    
    * Remove extraneous tick-tick-ticks.
    
    * Flatten notice fields.
    
    * Add new notice fields.
    
    * Update RULES.md
    
    ---------
    
    Co-authored-by: isabelle-dr <isabelle@mobilitydata.org>

---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsFareMediaTypeEnum.java
@@ -0,0 +1,11 @@
+package org.mobilitydata.gtfsvalidator.table;
+
+import org.mobilitydata.gtfsvalidator.annotation.GtfsEnumValue;
+
+/** */
+@GtfsEnumValue(name = "NONE", value = 0)
+@GtfsEnumValue(name = "PAPER_TICKET", value = 1)
+@GtfsEnumValue(name = "TRANSIT_CARD", value = 2)
+@GtfsEnumValue(name = "CONTACTLESS_EMV", value = 3)
+@GtfsEnumValue(name = "MOBILE_APP", value = 4)
+public interface GtfsFareMediaTypeEnum {}
