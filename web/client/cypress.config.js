0fbb9eccd990769bd4fe798d5c3fe5c26b1198c4
Mike Gunn <mgunn@jarv.us>
2023-04-20 16:20:05 -0400

    chore: customize cypress config

--- web/client/cypress.config.js
+++ web/client/cypress.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  watchForFileChanges: false,
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
