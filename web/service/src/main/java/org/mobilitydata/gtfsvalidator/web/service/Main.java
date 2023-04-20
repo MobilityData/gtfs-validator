c89c716166d6bb46e95c5695dac0cc0bfe667886
Kevin Clough <kevinclough@gmail.com>
2023-04-20 14:28:35 -0400

    feat: use sentry spring-boot

--- web/service/src/main/java/org/mobilitydata/gtfsvalidator/web/service/Main.java
+++ web/service/src/main/java/org/mobilitydata/gtfsvalidator/web/service/Main.java
@@ -17,24 +17,11 @@ package org.mobilitydata.gtfsvalidator.web.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @SpringBootApplication
 public class Main {
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
-  }
-
-  @Bean
-  public HandlerExceptionResolver sentryExceptionResolver() {
-    return new io.sentry.spring.SentryExceptionResolver();
-  }
-
-  @Bean
-  public ServletContextInitializer sentryServletContextInitializer() {
-    return new io.sentry.spring.SentryServletContextInitializer();
   }
 }
