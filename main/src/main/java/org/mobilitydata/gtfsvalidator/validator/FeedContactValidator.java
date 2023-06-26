be262874e2635e1212f4714eca3154766dcd9606
Jingsi Lu <jingsi@mobilitydata.org>
2023-06-26 09:50:53 -0400

    format code add hasFeedContactEmailAndUrlShouldNotGenerateNotice test method add method comments

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/FeedContactValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/FeedContactValidator.java
@@ -1,48 +1,46 @@
 package org.mobilitydata.gtfsvalidator.validator;
 
+import static org.mobilitydata.gtfsvalidator.notice.SeverityLevel.WARNING;
+
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.ValidationNotice;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfo;
 
-import static org.mobilitydata.gtfsvalidator.notice.SeverityLevel.WARNING;
-
 @GtfsValidator
-public class FeedContactValidator extends SingleEntityValidator<GtfsFeedInfo>{
+public class FeedContactValidator extends SingleEntityValidator<GtfsFeedInfo> {
 
-    @Override
-    public void validate(GtfsFeedInfo entity, NoticeContainer noticeContainer) {
-        if (!entity.hasFeedContactEmail()&& !entity.hasFeedContactUrl() || entity.feedContactEmail().isBlank() && entity.feedContactUrl().isBlank()) {
-            noticeContainer.addValidationNotice(new FeedContactNotice(
-                    entity.csvRowNumber(),
-                    entity.feedContactEmail(),
-                    entity.feedContactUrl()
-            ));
-        }
+  @Override
+  public void validate(GtfsFeedInfo entity, NoticeContainer noticeContainer) {
+    if (!entity.hasFeedContactEmail() && !entity.hasFeedContactUrl()
+        || entity.feedContactEmail().isBlank() && entity.feedContactUrl().isBlank()) {
+      noticeContainer.addValidationNotice(
+          new FeedContactNotice(
+              entity.csvRowNumber(), entity.feedContactEmail(), entity.feedContactUrl()));
     }
+  }
 
-    @GtfsValidationNotice(
-            severity = WARNING,
-            files = @GtfsValidationNotice.FileRefs(GtfsFeedInfo.class),
-            urls = {
-                    @GtfsValidationNotice.UrlRef(
-                            label = "Original Python validator implementation",
-                            url = "https://github.com/google/transitfeed")
-            })
-    static class FeedContactNotice extends ValidationNotice {
-        /** The row number of the faulty record. */
-        private final int rowNumber;
+  @GtfsValidationNotice(
+      severity = WARNING,
+      files = @GtfsValidationNotice.FileRefs(GtfsFeedInfo.class),
+      urls = {
+        @GtfsValidationNotice.UrlRef(
+            label = "Original Python validator implementation",
+            url = "https://github.com/google/transitfeed")
+      })
+  static class FeedContactNotice extends ValidationNotice {
+    /** The row number of the faulty record. */
+    private final int rowNumber;
 
-        private final String feedContactEmail;
+    private final String feedContactEmail;
 
-        private final String feedContactUrl;
+    private final String feedContactUrl;
 
-
-        FeedContactNotice(int rowNumber, String feedContactEmail, String feedContactUrl) {
-            this.rowNumber = rowNumber;
-            this.feedContactEmail = feedContactEmail;
-            this.feedContactUrl = feedContactUrl;
-        }
+    FeedContactNotice(int rowNumber, String feedContactEmail, String feedContactUrl) {
+      this.rowNumber = rowNumber;
+      this.feedContactEmail = feedContactEmail;
+      this.feedContactUrl = feedContactUrl;
     }
+  }
 }
