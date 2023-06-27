e264cdd2478ffeed5d77dc8b9a8861ed095f2c5e
Jingsi Lu <jingsi@mobilitydata.org>
2023-06-27 12:21:07 -0400

    add comments to fix org.mobilitydata.gtfsvalidator.validator.NoticeDocumentationTest errors

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/FeedContactValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/FeedContactValidator.java
@@ -11,6 +11,12 @@ import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfo;
 @GtfsValidator
 public class FeedContactValidator extends SingleEntityValidator<GtfsFeedInfo> {
 
+  /**
+   * Best Practices https://gtfs.org/schedule/best-practices/#feed_infotxt
+   * suggest providing at least one of feed_contact_email and feed_contact_url
+   * @param entity
+   * @param noticeContainer
+   */
   @Override
   public void validate(GtfsFeedInfo entity, NoticeContainer noticeContainer) {
     if (!entity.hasFeedContactEmail() && !entity.hasFeedContactUrl()
@@ -21,6 +27,9 @@ public class FeedContactValidator extends SingleEntityValidator<GtfsFeedInfo> {
     }
   }
 
+  /**
+   * Only generates a warning when both feed_contact_email and feed_contact_url are unset
+   */
   @GtfsValidationNotice(
       severity = WARNING,
       files = @GtfsValidationNotice.FileRefs(GtfsFeedInfo.class),
@@ -30,11 +39,13 @@ public class FeedContactValidator extends SingleEntityValidator<GtfsFeedInfo> {
             url = "https://github.com/google/transitfeed")
       })
   static class FeedContactNotice extends ValidationNotice {
-    /** The row number of the faulty record. */
+    /** The row number of the validated record. */
     private final int rowNumber;
 
+    /** The email contact information of a feed. */
     private final String feedContactEmail;
 
+    /** The url contact information of a feed. */
     private final String feedContactUrl;
 
     FeedContactNotice(int rowNumber, String feedContactEmail, String feedContactUrl) {
