c345acd984fb17a660723973d6851904e1991f9a
Jingsi Lu <jingsi@mobilitydata.org>
2023-07-14 11:47:41 -0400

    fix failed NoticeDocumentationTest

--- core/src/main/java/org/mobilitydata/gtfsvalidator/notice/InvalidInputFilesInSubfolderNotice.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/notice/InvalidInputFilesInSubfolderNotice.java
@@ -4,6 +4,7 @@ import static org.mobilitydata.gtfsvalidator.notice.SeverityLevel.ERROR;
 
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
 
+/** Zip files containing Gtfs files in a subfolder are invalid. */
 @GtfsValidationNotice(severity = ERROR)
 public class InvalidInputFilesInSubfolderNotice extends ValidationNotice {
   /** The error message that explains the reason for the exception. */
