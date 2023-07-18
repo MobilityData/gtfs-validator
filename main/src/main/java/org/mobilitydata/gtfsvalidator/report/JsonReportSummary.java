24c44d0775ebd260d788559c9acf078a025f82e9
jcpitre <jcpitre@mobilitydata.org>
2023-07-17 20:47:27 -0400

    Made the json file less vulnerable to unwanted changes and removed unused code.

--- main/src/main/java/org/mobilitydata/gtfsvalidator/report/JsonReportSummary.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/report/JsonReportSummary.java
@@ -1,5 +1,7 @@
 package org.mobilitydata.gtfsvalidator.report;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,32 +12,29 @@ import org.mobilitydata.gtfsvalidator.runner.ValidationRunnerConfig;
 import org.mobilitydata.gtfsvalidator.util.VersionInfo;
 
 /**
- * Used to generate the summary of json report using the built-in class serialization of GSON.
- * Fields names used here will be the same as in the generated json file.
+ * Used to generate the summary of json report using the built-in object serialization of GSON.
+ * Fields names found here will be present in the generated json file except if renamed with the
+ * {@code @SerializedName} annotation.
  */
 @SuppressWarnings("unused") // The fields of this class are only read by Gson when serializing.
 public class JsonReportSummary {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   private String validatorVersion;
   private String validatedAt;
   private String gtfsInput;
-
   private int threads;
-
   private String outputDirectory;
-
   private String systemErrorsReportName;
   private String validationReportName;
   private String htmlReportName;
   private String countryCode;
-
-  private Map<String, String> feedInfo;
-
-  private List<AgencyMetadata> agencies;
-
+  private JsonReportFeedInfo feedInfo;
+  private List<JsonReportAgencyMetadata> agencies;
   private Set<String> files;
 
-  private Map<String, Integer> counts;
+  @SerializedName("counts")
+  private JsonReportCounts jsonReportCounts;
 
   private List<String> gtfsComponents;
 
@@ -56,13 +55,38 @@ public class JsonReportSummary {
       this.validationReportName = config.validationReportFileName();
       this.htmlReportName = config.htmlReportFileName();
       this.countryCode = config.countryCode().getCountryCode();
+    } else {
+      logger.atSevere().log(
+          "No validation configuration for JSON report, there will be missing data in the report.");
     }
 
     if (feedMetadata != null) {
-      this.feedInfo = feedMetadata.feedInfo;
-      this.agencies = feedMetadata.agencies;
+      if (feedMetadata.feedInfo != null) {
+        this.feedInfo = new JsonReportFeedInfo(feedMetadata.feedInfo);
+      } else {
+        logger.atSevere().log(
+            "No feed info for feed "
+                + this.gtfsInput
+                + ", there will be missing data in the report.");
+      }
+
+      if (feedMetadata.agencies != null) {
+        this.agencies =
+            feedMetadata.agencies.stream()
+                .map(JsonReportAgencyMetadata::new)
+                .collect(Collectors.toList());
+      }
+
       this.files = feedMetadata.getFilenames();
-      this.counts = feedMetadata.counts;
+      if (feedMetadata.counts != null) {
+        jsonReportCounts = new JsonReportCounts(feedMetadata.counts);
+      } else {
+        logger.atSevere().log(
+            "Counts are not available for "
+                + this.gtfsInput
+                + ", there will be missing data in the report.");
+      }
+
       this.gtfsComponents =
           feedMetadata.specFeatures == null
               ? null
@@ -70,6 +94,74 @@ public class JsonReportSummary {
                   .filter(Map.Entry::getValue)
                   .map(Map.Entry::getKey)
                   .collect(Collectors.toList());
+    } else {
+      logger.atSevere().log(
+          "No feed metadata for " + this.gtfsInput + ", there will be missing data in the report.");
     }
+  }
+
+  /**
+   * This class is used as a container of data to be converted to json using the builtin object
+   * serialization of GSON. We use this class instead of the Map in FeedMetadata directly because
+   * the schema of the resulting json file has to be stable and controlled as it becomes part of the
+   * exported API.
+   */
+  private static class JsonReportFeedInfo {
+    public JsonReportFeedInfo(Map<String, String> feedInfo) {
+      publisherName = feedInfo.get(FeedMetadata.FEED_INFO_PUBLISHER_NAME);
+      publisherUrl = feedInfo.get(FeedMetadata.FEED_INFO_PUBLISHER_URL);
+      feedLanguage = feedInfo.get(FeedMetadata.FEED_INFO_FEED_LANGUAGE);
+      feedStartDate = feedInfo.get(FeedMetadata.FEED_INFO_FEED_START_DATE);
+      feedEndDate = feedInfo.get(FeedMetadata.FEED_INFO_FEED_END_DATE);
+    }
+
+    String publisherName;
+    String publisherUrl;
+    String feedLanguage;
+    String feedStartDate;
+    String feedEndDate;
+  }
+
+  private static class JsonReportAgencyMetadata {
+    private final String name;
+    private final String url;
+    private final String phone;
+    private final String email;
+
+    public JsonReportAgencyMetadata(AgencyMetadata agencyMetadata) {
+      name = agencyMetadata.name;
+      url = agencyMetadata.url;
+      phone = agencyMetadata.phone;
+      email = agencyMetadata.email;
+    }
+  }
+
+  private static class JsonReportCounts {
+    public JsonReportCounts(Map<String, Integer> counts) {
+      shapes = counts.get(FeedMetadata.COUNTS_SHAPES);
+      stops = counts.get(FeedMetadata.COUNTS_STOPS);
+      routes = counts.get(FeedMetadata.COUNTS_ROUTES);
+      trips = counts.get(FeedMetadata.COUNTS_TRIPS);
+      agencies = counts.get(FeedMetadata.COUNTS_AGENCIES);
+      blocks = counts.get(FeedMetadata.COUNTS_BLOCKS);
+    }
+
+    @SerializedName("Shapes")
+    Integer shapes;
+
+    @SerializedName("Stops")
+    Integer stops;
+
+    @SerializedName("Routes")
+    Integer routes;
+
+    @SerializedName("Trips")
+    Integer trips;
+
+    @SerializedName("Agencies")
+    Integer agencies;
+
+    @SerializedName("Blocks")
+    Integer blocks;
   }
 }
--- main/src/test/java/org/mobilitydata/gtfsvalidator/report/model/JsonReportSummaryTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/report/model/JsonReportSummaryTest.java
@@ -17,7 +17,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mobilitydata.gtfsvalidator.input.CountryCode;
-import org.mobilitydata.gtfsvalidator.report.JsonReportGenerator;
 import org.mobilitydata.gtfsvalidator.report.JsonReportSummary;
 import org.mobilitydata.gtfsvalidator.runner.ValidationRunnerConfig;
 import org.mobilitydata.gtfsvalidator.util.VersionInfo;
@@ -29,27 +28,6 @@ public class JsonReportSummaryTest {
       VersionInfo.create(Optional.of("1.0"), Optional.of("1.1"));
 
   private static Gson gson = new Gson();
-
-  private static JsonReportSummary generateJsonReportSummary() throws Exception {
-    JsonReportGenerator reportGenerator = new JsonReportGenerator();
-
-    FeedMetadata feedMetaData;
-    ValidationRunnerConfig.Builder builder = ValidationRunnerConfig.builder();
-    builder.setCountryCode(CountryCode.forStringOrUnknown("GB"));
-    builder.setGtfsSource(new URI("some_dataset_filename"));
-    builder.setHtmlReportFileName("some_html_filename");
-    builder.setOutputDirectory(Path.of("some_output_directory"));
-    builder.setNumThreads(1);
-    builder.setPrettyJson(true);
-    builder.setSystemErrorsReportFileName("some_error_filename");
-    builder.setValidationReportFileName("some_report_filename");
-
-    ValidationRunnerConfig config = builder.build();
-
-    JsonReportSummary reportSummary = new JsonReportSummary(null, config, versionInfo, "now");
-
-    return reportSummary;
-  }
 
   private static ValidationRunnerConfig generateValidationRunnerConfig() throws Exception {
 
@@ -76,8 +54,24 @@ public class JsonReportSummaryTest {
             Arrays.asList(
                 new AgencyMetadata("agency1", "some URL 1", "phone1", "email1"),
                 new AgencyMetadata("agency1", "some URL 1", "phone1", "email1")));
-    feedMetadata.feedInfo = Map.of("key1", "value1", "key2", "value2");
-    feedMetadata.counts = Map.of("count1", 1, "count2", 2);
+    feedMetadata.feedInfo =
+        Map.of(
+            FeedMetadata.FEED_INFO_PUBLISHER_NAME,
+            "value1",
+            FeedMetadata.FEED_INFO_PUBLISHER_URL,
+            "value2",
+            "Illegal Key",
+            "Some Value" // Should not be present in the resulting GSON
+            );
+    feedMetadata.counts =
+        Map.of(
+            FeedMetadata.COUNTS_SHAPES,
+            1,
+            FeedMetadata.COUNTS_TRIPS,
+            2,
+            "Illegal Key",
+            3 // Should not be present in the resulting GSON
+            );
     feedMetadata.specFeatures = Map.of("Feature1", false, "Feature2", true);
     return feedMetadata;
   }
@@ -89,8 +83,7 @@ public class JsonReportSummaryTest {
         new JsonReportSummary(null, generateValidationRunnerConfig(), versionInfo, "now");
 
     String expected =
-        "{"
-            + "\"validatorVersion\":\"1.0\","
+        "{\"validatorVersion\":\"1.0\","
             + "\"validatedAt\":\"now\","
             + "\"gtfsInput\":\"some_dataset_filename\","
             + "\"threads\":1,"
@@ -98,8 +91,7 @@ public class JsonReportSummaryTest {
             + "\"systemErrorsReportName\":\"some_error_filename\","
             + "\"validationReportName\":\"some_report_filename\","
             + "\"htmlReportName\":\"some_html_filename\","
-            + "\"countryCode\":\"GB\""
-            + "}";
+            + "\"countryCode\":\"GB\"}";
 
     assertEquals(JsonParser.parseString(expected), gson.toJsonTree(reportSummary));
   }
@@ -111,7 +103,6 @@ public class JsonReportSummaryTest {
     JsonReportSummary reportSummary =
         new JsonReportSummary(feedMetadata, generateValidationRunnerConfig(), versionInfo, "now");
 
-    String qaz = gson.toJson(reportSummary);
     String expected =
         "{\"validatorVersion\":\"1.0\","
             + "\"validatedAt\":\"now\","
@@ -122,12 +113,12 @@ public class JsonReportSummaryTest {
             + "\"validationReportName\":\"some_report_filename\","
             + "\"htmlReportName\":\"some_html_filename\","
             + "\"countryCode\":\"GB\","
-            + "\"feedInfo\":{\"key1\":\"value1\",\"key2\":\"value2\"},"
+            + "\"feedInfo\":{\"publisherName\":\"value1\",\"publisherUrl\":\"value2\"},"
             + "\"agencies\":["
             + "{\"name\":\"agency1\",\"url\":\"some URL 1\",\"phone\":\"phone1\",\"email\":\"email1\"},"
             + "{\"name\":\"agency1\",\"url\":\"some URL 1\",\"phone\":\"phone1\",\"email\":\"email1\"}],"
             + "\"files\":[\"file1\",\"file2\"],"
-            + "\"counts\":{\"count1\":1,\"count2\":2},"
+            + "\"counts\":{\"Shapes\":1,\"Trips\":2},"
             + "\"gtfsComponents\":[\"Feature2\"]}";
 
     assertEquals(JsonParser.parseString(expected), gson.toJsonTree(reportSummary));
@@ -138,17 +129,17 @@ public class JsonReportSummaryTest {
 
     FeedMetadata feedMetadata = generateFeedMetaData();
     JsonReportSummary reportSummary = new JsonReportSummary(feedMetadata, null, versionInfo, "now");
-    String qaz = gson.toJson(reportSummary);
+
     String expected =
         "{\"validatorVersion\":\"1.0\","
             + "\"validatedAt\":\"now\","
             + "\"threads\":0,"
-            + "\"feedInfo\":{\"key1\":\"value1\",\"key2\":\"value2\"},"
+            + "\"feedInfo\":{\"publisherName\":\"value1\",\"publisherUrl\":\"value2\"},"
             + "\"agencies\":["
             + "{\"name\":\"agency1\",\"url\":\"some URL 1\",\"phone\":\"phone1\",\"email\":\"email1\"},"
             + "{\"name\":\"agency1\",\"url\":\"some URL 1\",\"phone\":\"phone1\",\"email\":\"email1\"}],"
             + "\"files\":[\"file1\",\"file2\"],"
-            + "\"counts\":{\"count1\":1,\"count2\":2},"
+            + "\"counts\":{\"Shapes\":1,\"Trips\":2},"
             + "\"gtfsComponents\":[\"Feature2\"]}";
 
     assertEquals(JsonParser.parseString(expected), gson.toJsonTree(reportSummary));
