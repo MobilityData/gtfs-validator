7264f750dde47566e7ca3b23d730ab4c0745cea8
Brian Ferris <bdferris+v2@google.com>
2023-09-20 06:45:33 -0700

    feat: Add method to GtfsFeedLoader to access GtfsTableDescriptors. (#1573)

--- core/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsFeedLoader.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsFeedLoader.java
@@ -20,6 +20,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.flogger.FluentLogger;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -70,6 +71,10 @@ public class GtfsFeedLoader {
       }
       tableDescriptors.put(descriptor.gtfsFilename(), descriptor);
     }
+  }
+
+  public Collection<GtfsTableDescriptor<?>> getTableDescriptors() {
+    return Collections.unmodifiableCollection(tableDescriptors.values());
   }
 
   public String listTableDescriptors() {
