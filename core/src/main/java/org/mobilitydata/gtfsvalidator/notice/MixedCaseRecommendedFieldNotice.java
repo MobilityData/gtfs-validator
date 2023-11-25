6e643f5e2cb0e79af46afb8c1a3e0a7cd3e24d4b
Emma Jae Blue <emma@mobilitydata.org>
2023-11-25 11:48:04 -0500

    format fix

--- core/src/main/java/org/mobilitydata/gtfsvalidator/notice/MixedCaseRecommendedFieldNotice.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/notice/MixedCaseRecommendedFieldNotice.java
@@ -22,8 +22,7 @@ import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice.UrlRef;
     urls = {
       @UrlRef(
           label = "Best Practices for All Files",
-          url =
-              "https://gtfs.org/schedule/reference/#file-requirements")
+          url = "https://gtfs.org/schedule/reference/#file-requirements")
     })
 public class MixedCaseRecommendedFieldNotice extends ValidationNotice {
 
