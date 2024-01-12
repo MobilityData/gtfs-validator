b497fc668e53588476cbb3eff15fd5b089395aea
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2024-01-12 13:42:45 -0500

    fix formatting

--- core/src/main/java/org/mobilitydata/gtfsvalidator/util/HttpGetUtil.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/util/HttpGetUtil.java
@@ -16,52 +16,50 @@
 
 package org.mobilitydata.gtfsvalidator.util;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
-import java.net.URL;
-
 public class HttpGetUtil {
 
-    public static final String USER_AGENT_PREFIX = "MobilityData GTFS-Validator";
+  public static final String USER_AGENT_PREFIX = "MobilityData GTFS-Validator";
 
-    /**
-     * @param validatorVersion version of the validator
-     * @return the user agent string in the format: "MobilityData GTFS-Validator/{validatorVersion}
-     *     (Java {java version})"
-     */
-    public static String getUserAgent(String validatorVersion) {
-        return USER_AGENT_PREFIX
-                + "/"
-                + (validatorVersion != null ? validatorVersion : "")
-                + " (Java "
-                + System.getProperty("java.version")
-                + ")";
-    }
+  /**
+   * @param validatorVersion version of the validator
+   * @return the user agent string in the format: "MobilityData GTFS-Validator/{validatorVersion}
+   *     (Java {java version})"
+   */
+  public static String getUserAgent(String validatorVersion) {
+    return USER_AGENT_PREFIX
+        + "/"
+        + (validatorVersion != null ? validatorVersion : "")
+        + " (Java "
+        + System.getProperty("java.version")
+        + ")";
+  }
 
-    /**
-     * Downloads data from network.
-     *
-     * @param sourceUrl the fully qualified URL
-     * @param outputStream the output stream
-     * @param validatorVersion the version of the validator
-     * @throws IOException if no file could not be found at the specified location
-     * @throws URISyntaxException if URL is malformed
-     */
-    public static void loadFromUrl(URL sourceUrl, OutputStream outputStream, String validatorVersion)
-            throws IOException, URISyntaxException {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet(sourceUrl.toURI());
-            httpGet.setHeader("User-Agent", getUserAgent(validatorVersion));
-            try (CloseableHttpResponse httpResponse = httpClient.execute(httpGet)) {
-                httpResponse.getEntity().writeTo(outputStream);
-            }
-        }
+  /**
+   * Downloads data from network.
+   *
+   * @param sourceUrl the fully qualified URL
+   * @param outputStream the output stream
+   * @param validatorVersion the version of the validator
+   * @throws IOException if no file could not be found at the specified location
+   * @throws URISyntaxException if URL is malformed
+   */
+  public static void loadFromUrl(URL sourceUrl, OutputStream outputStream, String validatorVersion)
+      throws IOException, URISyntaxException {
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet httpGet = new HttpGet(sourceUrl.toURI());
+      httpGet.setHeader("User-Agent", getUserAgent(validatorVersion));
+      try (CloseableHttpResponse httpResponse = httpClient.execute(httpGet)) {
+        httpResponse.getEntity().writeTo(outputStream);
+      }
     }
+  }
 }
