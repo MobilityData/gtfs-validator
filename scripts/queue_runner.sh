1461d4a993af1b8f15570cdffde15719f659bc28
cka-y <chaimaa@mobilitydata.org>
2024-03-04 12:00:41 -0500

    fix: removing arg from master jar

--- scripts/queue_runner.sh
+++ scripts/queue_runner.sh
@@ -17,7 +17,7 @@ do
    java -Xmx10G -Xms8G -jar gtfs-validator-snapshot/gtfs-validator*.jar --url $URL --output_base $OUTPUT_BASE/output/$path_name --validation_report_name latest.json --system_errors_report_name latest_errors.json --skip_validator_update
    if [ "$master" = "--include-master" ];
    then
-      java -Xmx10G -Xms8G -jar gtfs-validator-master/gtfs-validator*.jar --url $URL --output_base $OUTPUT_BASE/output/$path_name --validation_report_name reference.json --system_errors_report_name reference_errors.json --skip_validator_update
+      java -Xmx10G -Xms8G -jar gtfs-validator-master/gtfs-validator*.jar --url $URL --output_base $OUTPUT_BASE/output/$path_name --validation_report_name reference.json --system_errors_report_name reference_errors.json
    fi;
    wait
 done
