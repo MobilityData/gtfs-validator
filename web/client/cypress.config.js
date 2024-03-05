46d643b1ff2003827508353ab52265f7c0583eef
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2024-03-05 15:09:07 -0500

    changing cypress localhost address

--- web/client/cypress.config.js
+++ web/client/cypress.config.js
@@ -5,13 +5,13 @@ export default defineConfig({
   defaultCommandTimeout: 10000,
   video: true,
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://127.0.0.1:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   env: {
-    PUBLIC_CLIENT_API_ROOT: "http://localhost:8080",
-    PUBLIC_CLIENT_REPORTS_ROOT: "http://localhost:8080/reports"
+    PUBLIC_CLIENT_API_ROOT: "http://127.0.0.1:8080",
+    PUBLIC_CLIENT_REPORTS_ROOT: "http://127.0.0.1:8080/reports"
   },
 });
