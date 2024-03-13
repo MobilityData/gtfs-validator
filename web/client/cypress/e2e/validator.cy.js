dc0a9ba5b055c0a0cd88fac690d688b3598afce3
David Gamez <1192523+davidgamez@users.noreply.github.com>
2024-03-13 12:36:39 -0400

    feat: 1689 web validator app consumes execution results json (#1712)
    * consumed the execution_results.json on the web app
    
    * deployed to staging - run cypress test
    
    * added TODO comments
    
    * change it back to master
    
    * fixed broken cypress test
    
    * changed back
    
    * resolved PR comments
    
    * Intercepted HEAD request to execution_results.json
    
    * update execution file path
    
    * removed env code
    
    * refactor generation process and update cypress tests
    
    * revert workflow change
    
    ---------
    
    Co-authored-by: Jingsi Lu <jingsi@mobilitydata.org>

--- web/client/cypress/e2e/validator.cy.js
+++ web/client/cypress/e2e/validator.cy.js
@@ -2,9 +2,11 @@
 
 const url = 'https://developers.google.com/static/transit/gtfs/examples/sample-feed.zip';
 const jobId = '8f6be6fb-1fee-41f8-b401-b2b4b552e177-sample';
+const execution_result_json = "execution_result.json"
 
 context('GTFS Validator - Core Workflow', () => {
   let numReportCalls = 0;
+  let numExecutionJsonCalls = 0;
 
   it('Validate GTFS .zip file', () => {
     // Setup intercept aliases
@@ -57,6 +59,42 @@ context('GTFS Validator - Core Workflow', () => {
         numReportCalls++;
       });
 
+    cy.intercept('HEAD',
+    `${Cypress.env("PUBLIC_CLIENT_REPORTS_ROOT")}/*/${execution_result_json}`,
+    (req) => {
+      if (numExecutionJsonCalls > 0) {
+        // return 200 code
+        req.alias = 'awaitExecutionJson200'
+        req.reply({
+          statusCode: 200,
+          headers: {
+            "access-control-allow-origin": '*',
+            "Access-Control-Allow-Credentials": "true",              
+            "x-number-of-calls": `${numExecutionJsonCalls}`
+          }
+        });
+      } else {
+        // return 404 code
+        req.alias = 'awaitExecutionJson404'
+        req.reply({
+          statusCode: 404,
+          headers: {
+            "access-control-allow-origin": '*',
+            "Access-Control-Allow-Credentials": "true",              
+            "x-number-of-calls": `${numExecutionJsonCalls}`
+          }
+        });
+      }
+      numExecutionJsonCalls++;
+    });
+
+    cy.intercept('GET', `${Cypress.env("PUBLIC_CLIENT_REPORTS_ROOT")}/*/${execution_result_json}`, {
+      statusCode: 200,
+      body: {
+        status: 'success',
+      },
+    })
+
     cy.visit('/')
     
     // Enter URL to .zip file
@@ -71,9 +109,14 @@ context('GTFS Validator - Core Workflow', () => {
     cy.get('button[type=submit]')
       .click();
 
-    // Wait for 404
+    // Wait for 404 while head request for execution_results.json
+    cy.wait('@awaitExecutionJson404').its('response.statusCode').should('equal', 404);
+    // Wait for 200 while head request for execution_results.json
+    cy.wait('@awaitExecutionJson200').its('response.statusCode').should('equal', 200);
+
+    // Wait for 404 while head request for reports.html
     cy.wait('@awaitReport404').its('response.statusCode').should('equal', 404);
-    // Wait for 200
+    // Wait for 200 while head request for reports.html
     cy.wait('@awaitReport200').its('response.statusCode').should('equal', 200);
   
    // Confirm "report ready"
