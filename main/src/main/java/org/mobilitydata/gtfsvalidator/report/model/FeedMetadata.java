2c81b7aa006a97a03d9aa9a046c71c08af8b5190
Jingsi Lu <jingsi@mobilitydata.org>
2024-08-14 12:36:08 -0400

    fix: Display empty values for feed_start_date and feed_end_date if there's… (#1798)
    * Display empty values for feed_start_date and feed_end_date if there's no data for these two fields
    
    * formatted code
    
    * displayed N/A instead of empty string
    
    * formatted code
    
    * used default feed start/end date

--- main/src/main/java/org/mobilitydata/gtfsvalidator/report/model/FeedMetadata.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/report/model/FeedMetadata.java
@@ -3,6 +3,7 @@ package org.mobilitydata.gtfsvalidator.report.model;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
 import com.vladsch.flexmark.util.misc.Pair;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.function.Function;
 import org.mobilitydata.gtfsvalidator.table.*;
@@ -309,14 +310,16 @@ public class FeedMetadata {
     feedInfo.put(
         FEED_INFO_FEED_LANGUAGE, info == null ? "N/A" : info.feedLang().getDisplayLanguage());
     if (feedTable.hasColumn(GtfsFeedInfo.FEED_START_DATE_FIELD_NAME)) {
-      feedInfo.put(
-          FEED_INFO_FEED_START_DATE,
-          info == null ? "N/A" : info.feedStartDate().getLocalDate().toString());
+      LocalDate localDate = info.feedStartDate().getLocalDate();
+      String displayDate =
+          localDate.equals(GtfsFeedInfo.DEFAULT_FEED_START_DATE) ? "N/A" : localDate.toString();
+      feedInfo.put(FEED_INFO_FEED_START_DATE, info == null ? "N/A" : displayDate);
     }
     if (feedTable.hasColumn(GtfsFeedInfo.FEED_END_DATE_FIELD_NAME)) {
-      feedInfo.put(
-          FEED_INFO_FEED_END_DATE,
-          info == null ? "N/A" : info.feedEndDate().getLocalDate().toString());
+      LocalDate localDate = info.feedEndDate().getLocalDate();
+      String displayDate =
+          localDate.equals(GtfsFeedInfo.DEFAULT_FEED_END_DATE) ? "N/A" : localDate.toString();
+      feedInfo.put(FEED_INFO_FEED_END_DATE, info == null ? "N/A" : displayDate);
     }
   }
 
