fe8ab43fd15a1e557a91caf0cf825b439a8c4604
Jingsi Lu <jingsi@mobilitydata.org>
2024-08-28 09:37:41 -0400

    added class comments

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/MissingFeedInfoValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/MissingFeedInfoValidator.java
@@ -9,6 +9,23 @@ import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfo;
 import org.mobilitydata.gtfsvalidator.table.GtfsFeedInfoTableContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsTranslationTableContainer;
 
+/**
+ * The `MissingFeedInfoValidator` class is responsible for validating the presence of the
+ * `feed_info.txt` file in a GTFS feed. If the `feed_info.txt` file is missing, it generates
+ * appropriate validation notices.
+ *
+ * <p>The validation logic is as follows: - If the `feed_info.txt` file is missing and the
+ * `translations.txt` file is also missing, a `MissingRecommendedFileNotice` is generated. - If the
+ * `feed_info.txt` file is missing but the `translations.txt` file is present, a
+ * `MissingRequiredFileNotice` is generated.
+ *
+ * <p>This validator is part of the GTFS validation framework and is annotated with `@GtfsValidator`
+ * to indicate its role.
+ *
+ * <p>Dependencies: - `GtfsFeedInfoTableContainer`: Provides access to the `feed_info.txt` file
+ * data. - `GtfsTranslationTableContainer`: Provides access to the `translations.txt` file data. -
+ * `NoticeContainer`: Collects validation notices generated during the validation process.
+ */
 @GtfsValidator
 public class MissingFeedInfoValidator extends FileValidator {
 
