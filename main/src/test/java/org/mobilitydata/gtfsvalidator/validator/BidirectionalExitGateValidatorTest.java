0377dae4994a280be00322378375a95c26fb6139
Jingsi Lu <jingsi@mobilitydata.org>
2024-08-30 09:24:41 -0400

    formatted code

--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/BidirectionalExitGateValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/BidirectionalExitGateValidatorTest.java
@@ -1,5 +1,7 @@
 package org.mobilitydata.gtfsvalidator.validator;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -7,38 +9,34 @@ import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.table.GtfsPathway;
 import org.mobilitydata.gtfsvalidator.table.GtfsPathwayIsBidirectional;
 
-
-import static com.google.common.truth.Truth.assertThat;
-
 @RunWith(JUnit4.class)
 public class BidirectionalExitGateValidatorTest {
-  public static GtfsPathway createPathway(int csvRowNumber,  Integer pathwayMode, GtfsPathwayIsBidirectional gtfsPathwayIsBidirectional) {
+  public static GtfsPathway createPathway(
+      int csvRowNumber,
+      Integer pathwayMode,
+      GtfsPathwayIsBidirectional gtfsPathwayIsBidirectional) {
     return new GtfsPathway.Builder()
-            .setCsvRowNumber(csvRowNumber)
-            .setPathwayMode(pathwayMode)
-            .setIsBidirectional(gtfsPathwayIsBidirectional)
-            .build();
+        .setCsvRowNumber(csvRowNumber)
+        .setPathwayMode(pathwayMode)
+        .setIsBidirectional(gtfsPathwayIsBidirectional)
+        .build();
   }
 
-  /**
-   * Tests that a pathway with bidirectional exit gates generates a notice.
-   */
+  /** Tests that a pathway with bidirectional exit gates generates a notice. */
   @Test
   public void isBidirectionalExitGateShouldGenerateNotice() {
     GtfsPathway entity = createPathway(1, 7, GtfsPathwayIsBidirectional.BIDIRECTIONAL);
     NoticeContainer noticeContainer = new NoticeContainer();
-    new BidirectionalExitGateValidator()
-            .validate(entity, noticeContainer);
-    assertThat(noticeContainer.getValidationNotices()).containsExactly(
-            new BidirectionalExitGateValidator.BidirectionalExitGatesNotice(entity));
+    new BidirectionalExitGateValidator().validate(entity, noticeContainer);
+    assertThat(noticeContainer.getValidationNotices())
+        .containsExactly(new BidirectionalExitGateValidator.BidirectionalExitGatesNotice(entity));
   }
 
   @Test
   public void isNotBidirectionalExitGateShouldNotGenerateNotice() {
     GtfsPathway entity = createPathway(1, 7, GtfsPathwayIsBidirectional.UNIDIRECTIONAL);
     NoticeContainer noticeContainer = new NoticeContainer();
-    new BidirectionalExitGateValidator()
-            .validate(entity, noticeContainer);
+    new BidirectionalExitGateValidator().validate(entity, noticeContainer);
     assertThat(noticeContainer.getValidationNotices()).isEmpty();
   }
 }
