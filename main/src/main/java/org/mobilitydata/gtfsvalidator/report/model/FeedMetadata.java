f5628ba912c4c4bb0f2dbae34a324d0783e1330a
cka-y <chaimaa@mobilitydata.org>
2024-10-01 13:55:31 -0400

    feat: added support notice for flex feeds

--- main/src/main/java/org/mobilitydata/gtfsvalidator/report/model/FeedMetadata.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/report/model/FeedMetadata.java
@@ -591,4 +591,12 @@ public class FeedMetadata {
   public ImmutableSortedSet<String> getFilenames() {
     return filenames;
   }
+
+  public Boolean hasFlexFeatures() {
+    return specFeatures.keySet().stream()
+        .anyMatch(
+            feature ->
+                feature.getFeatureGroup() != null
+                    && feature.getFeatureGroup().equals("Flexible Services"));
+  }
 }
