ae1f4e2969c674a045387f99ef28acb7b6443733
cka-y <chaimaa@mobilitydata.org>
2024-10-02 14:47:09 -0400

    feat: new booking_rules.txt validation notices

--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/NoticeFieldsTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/NoticeFieldsTest.java
@@ -204,6 +204,7 @@ public class NoticeFieldsTest {
             "fieldNames",
             "priorNoticeDurationMin",
             "priorNoticeDurationMax",
+            "priorNoticeStartTime",
             "priorNoticeStartDay",
             "priorNoticeLastDay");
   }
