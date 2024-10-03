06fe749fda091729ea929a52327261c7aadbfaed
David Gamez Diaz <1192523+davidgamez@users.noreply.github.com>
2024-10-03 10:58:29 -0400

    add no references to the report

--- output-comparator/src/main/java/org/mobilitydata/gtfsvalidator/outputcomparator/io/ValidationPerformanceCollector.java
+++ output-comparator/src/main/java/org/mobilitydata/gtfsvalidator/outputcomparator/io/ValidationPerformanceCollector.java
@@ -211,26 +211,22 @@ public class ValidationPerformanceCollector {
 
     b.append("</details>\n\n");
 
-    if (datasetsIncreasedMemoryUsage.size() > 0 || datasetsDecreasedMemoryUsage.size() > 0) {
-      b.append("<details>\n");
-      b.append("<summary><strong>📜 Memory Consumption</strong></summary>\n");
-
+    b.append("<details>\n");
+    b.append("<summary><strong>📜 Memory Consumption</strong></summary>\n");
+    if (datasetsIncreasedMemoryUsage.size() > 0) {
       List<DatasetMemoryUsage> increasedMemoryUsages =
           getDatasetMemoryUsages(datasetsIncreasedMemoryUsage);
       addMemoryUsageReport(increasedMemoryUsages, "memory has increased", b);
+    }
+    if (datasetsDecreasedMemoryUsage.size() > 0) {
       List<DatasetMemoryUsage> decreasedMemoryUsages =
           getDatasetMemoryUsages(datasetsDecreasedMemoryUsage);
       addMemoryUsageReport(decreasedMemoryUsages, "memory has decreased", b);
-      datasetsMemoryUsageNoReference.sort(
-          new Comparator<DatasetMemoryUsage>() {
-            @Override
-            public int compare(DatasetMemoryUsage o1, DatasetMemoryUsage o2) {
-              return 0;
-            }
-          });
-      addMemoryUsageReport(datasetsMemoryUsageNoReference, "no reference available", b);
-      b.append("</details>\n");
     }
+    if (datasetsMemoryUsageNoReference.size() > 0) {
+      addMemoryUsageReport(datasetsMemoryUsageNoReference, "no reference available", b);
+    }
+    b.append("</details>\n");
     return b.toString();
   }
 
