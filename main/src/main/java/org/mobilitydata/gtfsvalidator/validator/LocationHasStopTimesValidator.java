3aee26f7755503660cc03a570484f53d48adc517
Jingsi Lu <jingsi@mobilitydata.org>
2024-10-10 17:42:14 -0400

    changed from and to or

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/LocationHasStopTimesValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/LocationHasStopTimesValidator.java
@@ -77,7 +77,7 @@ public class LocationHasStopTimesValidator extends FileValidator {
     for (GtfsStop stop : stopTable.getEntities()) {
       List<GtfsStopTime> stopTimes = stopTimeTable.byStopId(stop.stopId());
       if (stop.locationType().equals(GtfsLocationType.STOP)) {
-        if (stopTimes.isEmpty() && !stopIdsInStopTimesandLocationGroupStops.contains(stop.stopId())) {
+        if (stopTimes.isEmpty() || !stopIdsInStopTimesandLocationGroupStops.contains(stop.stopId())) {
           noticeContainer.addValidationNotice(new StopWithoutStopTimeNotice(stop));
         }
       } else if (!stopTimes.isEmpty()) {
