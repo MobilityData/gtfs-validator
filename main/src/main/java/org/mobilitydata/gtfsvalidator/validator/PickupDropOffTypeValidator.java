b25c08600fcc9a348c03cf36733bab43fc7ad9cf
Jingsi Lu <jingsi@mobilitydata.org>
2025-01-07 09:39:59 -0500

    added ForbiddenDropOffTypeNotice

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/PickupDropOffTypeValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/PickupDropOffTypeValidator.java
@@ -20,6 +20,15 @@ public class PickupDropOffTypeValidator extends SingleEntityValidator<GtfsStopTi
                       entity.startPickupDropOffWindow(),
                       entity.endPickupDropOffWindow()));
     }
+
+    if ((entity.hasStartPickupDropOffWindow() || entity.hasEndPickupDropOffWindow()) &&
+            entity.dropOffType().equals(GtfsPickupDropOff.ALLOWED)) {
+      noticeContainer.addValidationNotice(
+              new ForbiddenDropOffTypeNotice(
+                      entity.csvRowNumber(),
+                      entity.startPickupDropOffWindow(),
+                      entity.endPickupDropOffWindow()));
+    }
   }
 
   /**
@@ -37,6 +46,27 @@ public class PickupDropOffTypeValidator extends SingleEntityValidator<GtfsStopTi
     private final GtfsTime endPickupDropOffWindow;
 
     public ForbiddenPickupTypeNotice(int csvRowNumber, GtfsTime startPickupDropOffWindow, GtfsTime endPickupDropOffWindow) {
+      this.csvRowNumber = csvRowNumber;
+      this.startPickupDropOffWindow = startPickupDropOffWindow;
+      this.endPickupDropOffWindow = endPickupDropOffWindow;
+    }
+  }
+
+  /**
+   * pickup_drop_off_window fields are forbidden when the drop_off_type is regularly scheduled (0).
+   */
+  @GtfsValidationNotice(severity = ERROR)
+  public static class ForbiddenDropOffTypeNotice extends ValidationNotice {
+    /** The row of the faulty record. */
+    private final int csvRowNumber;
+
+    /** The start pickup drop off window of the faulty record. */
+    private final GtfsTime startPickupDropOffWindow;
+
+    /** The end pickup drop off window of the faulty record. */
+    private final GtfsTime endPickupDropOffWindow;
+
+    public ForbiddenDropOffTypeNotice(int csvRowNumber, GtfsTime startPickupDropOffWindow, GtfsTime endPickupDropOffWindow) {
       this.csvRowNumber = csvRowNumber;
       this.startPickupDropOffWindow = startPickupDropOffWindow;
       this.endPickupDropOffWindow = endPickupDropOffWindow;
