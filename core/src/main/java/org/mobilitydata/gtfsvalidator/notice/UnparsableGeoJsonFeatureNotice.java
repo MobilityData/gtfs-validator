687ab27793955c0afda4a91fb9792b1536897e7a
Jingsi Lu <jingsi@mobilitydata.org>
2025-01-27 13:15:09 -0500

    added a UnparsableGeoJsonFeatureNotice to cover all failed cases

---
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/notice/UnparsableGeoJsonFeatureNotice.java
@@ -0,0 +1,20 @@
+package org.mobilitydata.gtfsvalidator.notice;
+
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
+
+import static org.mobilitydata.gtfsvalidator.notice.SeverityLevel.ERROR;
+
+/** A GeoJSON feature of locations.geojson could not be parsed. */
+@GtfsValidationNotice(severity = ERROR)
+public class UnparsableGeoJsonFeatureNotice extends ValidationNotice {
+  /** The name of the faulty file. */
+  private final String filename;
+
+  /** The detailed message describing the error. */
+  private final String message;
+
+  public UnparsableGeoJsonFeatureNotice(String filename, String message) {
+    this.filename = filename;
+    this.message = message;
+  }
+}
