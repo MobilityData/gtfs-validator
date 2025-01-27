20a89469078f25a230d864499764797df75988fe
cka-y <chaimaa@mobilitydata.org>
2025-01-27 14:49:31 -0500

    fix: clean up

--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/LocationIdForeignKeyValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/LocationIdForeignKeyValidatorTest.java
@@ -4,7 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import java.util.List;
-import java.util.function.Consumer; // <-- Added for the Consumer
+import java.util.function.Consumer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
