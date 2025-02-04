18fcb1a1c08a20f1757080323df4774d8d4335d4
cka-y <chaimaa@mobilitydata.org>
2025-02-04 15:08:24 -0500

    feat: added `missing_pickup_booking_rule_id` notice

---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/PickupBookingRuleIdValidator.java
@@ -0,0 +1,54 @@
+package org.mobilitydata.gtfsvalidator.validator;
+
+import javax.annotation.Nullable;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice.FileRefs;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.notice.SeverityLevel;
+import org.mobilitydata.gtfsvalidator.notice.ValidationNotice;
+import org.mobilitydata.gtfsvalidator.table.*;
+
+@GtfsValidator
+public class PickupBookingRuleIdValidator extends SingleEntityValidator<GtfsStopTime> {
+
+  @Override
+  public void validate(GtfsStopTime entity, NoticeContainer noticeContainer) {
+    if (entity.hasPickupType()
+        && entity.pickupType() == GtfsPickupDropOff.MUST_PHONE
+        && !entity.hasPickupBookingRuleId()) {
+      noticeContainer.addValidationNotice(
+          new MissingPickupBookingRuleIdNotice(
+              entity.csvRowNumber(),
+              entity.pickupType(),
+              entity.hasDropOffType() ? entity.dropOffType() : null));
+    }
+  }
+
+  @Override
+  public boolean shouldCallValidate(ColumnInspector header) {
+    return header.hasColumn(GtfsStopTime.PICKUP_TYPE_FIELD_NAME);
+  }
+
+  /** `pickup_booking_rule_id` is recommended when `pickup_type=2`. */
+  @GtfsValidationNotice(
+      severity = SeverityLevel.WARNING,
+      files = @FileRefs(GtfsStopTimeSchema.class))
+  static class MissingPickupBookingRuleIdNotice extends ValidationNotice {
+    /** The row number of the faulty record in `stop_times.txt` */
+    private final int csvRowNumber;
+
+    /** The pickup type of the faulty record. */
+    private final GtfsPickupDropOff pickupType;
+
+    /** The drop-off type of the faulty record. */
+    @Nullable private final GtfsPickupDropOff dropOffType;
+
+    public MissingPickupBookingRuleIdNotice(
+        int csvRowNumber, GtfsPickupDropOff pickupType, GtfsPickupDropOff dropOffType) {
+      this.csvRowNumber = csvRowNumber;
+      this.pickupType = pickupType;
+      this.dropOffType = dropOffType;
+    }
+  }
+}
