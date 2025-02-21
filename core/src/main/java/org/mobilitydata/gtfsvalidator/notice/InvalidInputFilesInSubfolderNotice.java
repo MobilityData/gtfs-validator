f2730d0a5451ab4d0a616e316102484980d6066c
Simon Richard <sylvansson@gmail.com>
2025-02-20 23:18:45 -0500

    Update docs for invalid_input_files_in_subfolder notice

--- core/src/main/java/org/mobilitydata/gtfsvalidator/notice/InvalidInputFilesInSubfolderNotice.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/notice/InvalidInputFilesInSubfolderNotice.java
@@ -8,6 +8,13 @@ import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
  * At least 1 GTFS file is in a subfolder.
  *
  * <p>All GTFS files must reside at the root level directly.
+ * There are two common cases that trigger this error:
+ *   1) The root folder (e.g., a folder called "GTFS 2020") was zipped instead of the individual
+ *      files within the folder (e.g., `calendar.txt`, `agency.txt`, etc.). This can be fixed
+ *      by zipping the files directly instead.
+ *   2) A file <a href="https://gtfs.org/documentation/schedule/reference/#dataset-files">associated
+ *      with GTFS</a> is in a subfolder rather than the root folder of the dataset, and needs to be
+ *      removed if not needed or moved to the root level.
  */
 @GtfsValidationNotice(severity = ERROR)
 public class InvalidInputFilesInSubfolderNotice extends ValidationNotice {
