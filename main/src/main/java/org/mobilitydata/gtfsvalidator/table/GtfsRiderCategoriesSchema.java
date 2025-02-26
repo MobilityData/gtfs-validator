2d9fdee91cff9c30d80239f5c51aa250e3b68f78
Jingsi Lu <jingsi@mobilitydata.org>
2025-02-26 10:39:50 -0500

    added GtfsRiderCategoriesSchema

---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsRiderCategoriesSchema.java
@@ -0,0 +1,20 @@
+package org.mobilitydata.gtfsvalidator.table;
+
+import org.mobilitydata.gtfsvalidator.annotation.*;
+
+@GtfsTable("rider_categories.txt")
+public interface GtfsRiderCategoriesSchema extends GtfsEntity {
+  @FieldType(FieldTypeEnum.ID)
+  @PrimaryKey
+  @Required
+  String riderCategoryId();
+
+  @Required
+  String riderCategoryName();
+
+  @Required
+  GtfsRiderCategory isDefaultRiderCategory();
+
+  @FieldType(FieldTypeEnum.URL)
+  String eligibilityUrl();
+}
---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsRiderCategoryEnum.java
@@ -0,0 +1,7 @@
+package org.mobilitydata.gtfsvalidator.table;
+
+import org.mobilitydata.gtfsvalidator.annotation.GtfsEnumValue;
+
+@GtfsEnumValue(name = "NOT_DEFAULT", value = 0)
+@GtfsEnumValue(name = "IS_DEFAULT", value = 1)
+public interface GtfsRiderCategoryEnum {}
