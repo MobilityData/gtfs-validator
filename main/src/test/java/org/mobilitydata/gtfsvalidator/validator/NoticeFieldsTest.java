7bc3c7403059186e75d9dc3bdbd123e62486b108
Jingsi Lu <jingsi@mobilitydata.org>
2025-03-04 11:21:23 -0500

    feat: 1976 modifying notice description for invalid currency amount (#1988)

--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/NoticeFieldsTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/NoticeFieldsTest.java
@@ -227,7 +227,8 @@ public class NoticeFieldsTest {
             "validator",
             "value",
             "duplicatedElement",
-            "unknownElement");
+            "unknownElement",
+            "currencyCode");
   }
 
   private static List<String> discoverValidationNoticeFieldNames() {
