0ed6f04399bbf02edcc82370b4478ed603fa93ce
Jingsi Lu <jingsi@mobilitydata.org>
2026-01-20 08:00:22 -0500

    cemv_support - update schema

---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/RouteCemvSupportValidator.java
@@ -0,0 +1,42 @@
+package org.mobilitydata.gtfsvalidator.validator;
+
+
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.notice.UnexpectedEnumValueNotice;
+import org.mobilitydata.gtfsvalidator.table.GtfsCemvSupport;
+import org.mobilitydata.gtfsvalidator.table.GtfsRoute;
+import org.mobilitydata.gtfsvalidator.table.GtfsRouteTableContainer;
+
+import javax.inject.Inject;
+
+@GtfsValidator
+public class RouteCemvSupportValidator extends SingleEntityValidator<GtfsRoute> {
+  private final GtfsRouteTableContainer routeTable;
+
+  @Inject
+  RouteCemvSupportValidator(GtfsRouteTableContainer routeTable) {
+    this.routeTable = routeTable;
+  }
+
+  @Override
+  public boolean shouldCallValidate(ColumnInspector header) {
+    return routeTable != null && !routeTable.isMissingFile()
+            && header.hasColumn(GtfsRoute.CEMV_SUPPORT_FIELD_NAME);
+  }
+
+  @Override
+  public void validate(GtfsRoute route, NoticeContainer noticeContainer) {
+    GtfsCemvSupport cemvSupport = route.cemvSupport();
+    if (cemvSupport != GtfsCemvSupport.NO_INFORMATION
+            && cemvSupport != GtfsCemvSupport.SUPPORTED
+            && cemvSupport != GtfsCemvSupport.NOT_SUPPORTED) {
+      noticeContainer.addValidationNotice(
+              new UnexpectedEnumValueNotice(
+                      routeTable.gtfsFilename(),
+                      route.csvRowNumber(),
+                      cemvSupport.name(),
+                      cemvSupport.getNumber()));
+    }
+  }
+}
