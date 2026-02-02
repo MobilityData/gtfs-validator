6ff298eba78e10aef1c0678870d7b446b94b61c2
Jingsi Lu <jingsi@mobilitydata.org>
2026-02-02 11:26:21 -0500

    more logic and test

--- main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsStopAccessEnum.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/table/GtfsStopAccessEnum.java
@@ -3,5 +3,5 @@ package org.mobilitydata.gtfsvalidator.table;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsEnumValue;
 
 @GtfsEnumValue(name = "EMPTY", value = 0)
-@GtfsEnumValue(name = "NOT_EMPTY", value = 1)
+@GtfsEnumValue(name = "NON_EMPTY", value = 1)
 public interface GtfsStopAccessEnum {}
---
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/StopAccessValidatorTest.java
@@ -0,0 +1,73 @@
+package org.mobilitydata.gtfsvalidator.validator;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.notice.ValidationNotice;
+import org.mobilitydata.gtfsvalidator.table.GtfsLocationType;
+import org.mobilitydata.gtfsvalidator.table.GtfsStop;
+import org.mobilitydata.gtfsvalidator.table.GtfsStopAccess;
+
+@RunWith(JUnit4.class)
+public class StopAccessValidatorTest {
+  private static List<ValidationNotice> generateNotices(GtfsStop stop) {
+    NoticeContainer noticeContainer = new NoticeContainer();
+    new StopAccessValidator().validate(stop, noticeContainer);
+    return noticeContainer.getValidationNotices();
+  }
+
+  @Test
+  public void emptyStopAccess_noNotice() {
+    assertThat(
+            generateNotices(
+                new GtfsStop.Builder()
+                    .setCsvRowNumber(4)
+                    .setStopId("S0")
+                    .setLocationType(GtfsLocationType.STOP)
+                    .setStopName("stop name")
+                    .setStopAccess(GtfsStopAccess.EMPTY)
+                    .build()))
+        .isEmpty();
+  }
+
+  @Test
+  public void stopLocationWithoutParentStation_generatesNotice() {
+    GtfsStop stop =
+        new GtfsStop.Builder()
+            .setCsvRowNumber(7)
+            .setStopId("S1")
+            .setLocationType(GtfsLocationType.STOP)
+            .setStopName("Stop 1")
+            // specify a non-empty stop_access to trigger validation
+            .setStopAccess(GtfsStopAccess.NON_EMPTY)
+            // do not set parent station -> hasParentStation() should be false
+            .build();
+
+    assertThat(generateNotices(stop))
+        .containsExactly(
+            new StopAccessValidator.StopAccessSpecifiedForStopWithNoParentStation(
+                7, "S1", "Stop 1", GtfsStopAccess.NON_EMPTY, GtfsLocationType.STOP));
+  }
+
+  @Test
+  public void nonStopLocationWithStopAccess_generatesIncorrectLocationNotice() {
+    GtfsStop stop =
+        new GtfsStop.Builder()
+            .setCsvRowNumber(9)
+            .setStopId("S2")
+            .setLocationType(GtfsLocationType.STATION)
+            .setStopName("Stop 2")
+            // specify a non-empty stop_access to trigger validation
+            .setStopAccess(GtfsStopAccess.NON_EMPTY)
+            .build();
+
+    assertThat(generateNotices(stop))
+        .containsExactly(
+            new StopAccessValidator.StopAccessSpecifiedForIncorrectLocation(
+                9, "S2", "Stop 2", GtfsStopAccess.NON_EMPTY, GtfsLocationType.STATION));
+  }
+}
