d9ad56a8d15237f509295f237e7d7a9099fac3c5
Jingsi Lu <jingsi@mobilitydata.org>
2026-02-02 10:12:55 -0500

    fix: Fixed the description for "overlapping_frequency" and "timeframe_overlap" (#2084)
    * Fixed the description for "overlapping_frequency" and "timeframe_overlap"

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TimeframeOverlapValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/TimeframeOverlapValidator.java
@@ -78,8 +78,8 @@ public class TimeframeOverlapValidator extends FileValidator {
    * overlapping time intervals.
    *
    * <p>Timeframes with the same group and service dates must not overlap in time. Two entries X and
-   * Y are considered to directly overlap if `X.start_time &lt;= Y.start_time` and `Y.start_time
-   * &lt; X.end_time`.
+   * Y are considered to directly overlap if X.start_time is less than or equal to Y.start_time and
+   * Y.start_time is less than X.end_time.
    */
   @GtfsValidationNotice(severity = ERROR, files = @FileRefs(GtfsFrequencySchema.class))
   static class TimeframeOverlapNotice extends ValidationNotice {
