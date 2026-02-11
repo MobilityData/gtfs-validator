420f448c5c8d624f8893ba8ba3e6d920026f774b
Jingsi Lu <jingsi@mobilitydata.org>
2026-02-11 13:22:40 -0500

    cleaned up

--- web/service/src/main/java/org/mobilitydata/gtfsvalidator/web/service/util/ValidationHandler.java
+++ web/service/src/main/java/org/mobilitydata/gtfsvalidator/web/service/util/ValidationHandler.java
@@ -32,10 +32,7 @@ public class ValidationHandler {
     var configBuilder =
         ValidationRunnerConfig.builder()
             .setGtfsSource(feedFile.toURI())
-            .setOutputDirectory(outputPath)
-            .setSkipValidatorUpdate(
-                true); // skipValidatorUpdate is true to prevent remote version checks and forces
-    // use of the JAR manifest version.
+            .setOutputDirectory(outputPath);
     if (!countryCode.isEmpty()) {
       var country = CountryCode.forStringOrUnknown(countryCode);
       logger.debug("setting country code: {}", country.getCountryCode());
