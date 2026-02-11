f12e2a84f866e410f443defd70d27bc85fc53266
cka-y <chaimaa@mobilitydata.org>
2026-02-11 08:02:17 -0500

    feat: route type consistency validators

---
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/InconsistentRouteTypeForBlockIdValidator.java
@@ -0,0 +1,76 @@
+package org.mobilitydata.gtfsvalidator.validator;
+
+import static org.mobilitydata.gtfsvalidator.notice.SeverityLevel.ERROR;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
+import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
+import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
+import org.mobilitydata.gtfsvalidator.notice.ValidationNotice;
+import org.mobilitydata.gtfsvalidator.table.*;
+
+@GtfsValidator
+public class InconsistentRouteTypeForBlockIdValidator extends FileValidator {
+  private final GtfsTripTableContainer tripTable;
+  private final GtfsRouteTableContainer routeTable;
+
+  @Inject
+  InconsistentRouteTypeForBlockIdValidator(
+      GtfsTripTableContainer tripTable, GtfsRouteTableContainer routeTable) {
+    this.tripTable = tripTable;
+    this.routeTable = routeTable;
+  }
+
+  @Override
+  public void validate(NoticeContainer noticeContainer) {
+    // for each block id in trip get the route type of the trip and check if all route types for the
+    // block id are the same
+    tripTable
+        .byBlockIdMap()
+        .asMap()
+        .forEach(
+            (blockId, trips) -> {
+              if (blockId != null) {
+                List<GtfsRouteType> routeTypes =
+                    trips.stream()
+                        .map(trip -> routeTable.byRouteId(trip.routeId()))
+                        .filter(Optional::isPresent)
+                        .map(route -> route.get().routeType())
+                        .distinct()
+                        .collect(Collectors.toList());
+                if (routeTypes.size() > 1) {
+                  List<String> routeIds =
+                      trips.stream().map(GtfsTrip::routeId).distinct().collect(Collectors.toList());
+                  noticeContainer.addValidationNotice(
+                      new InconsistentRouteTypeForBlockIdNotice(blockId, routeIds, routeTypes));
+                }
+              }
+            });
+  }
+
+  /** A block should have the same route mode. */
+  @GtfsValidationNotice(
+      severity = ERROR,
+      files = @GtfsValidationNotice.FileRefs({GtfsTripSchema.class, GtfsRouteSchema.class}))
+  static class InconsistentRouteTypeForBlockIdNotice extends ValidationNotice {
+    /** The block_id value of the faulty trip. */
+    private final String blockId;
+
+    /** The route_id values associated with the block_id comma separated. */
+    private final String routeIds;
+
+    /** The route_type values associated with the block_id comma separated. */
+    private final String routeTypes;
+
+    public InconsistentRouteTypeForBlockIdNotice(
+        String blockId, List<String> routeIds, List<GtfsRouteType> routeTypes) {
+      this.blockId = blockId;
+      this.routeIds = String.join(", ", routeIds);
+      this.routeTypes =
+          routeTypes.stream().map(String::valueOf).reduce((a, b) -> a + ", " + b).orElse("");
+    }
+  }
+}
