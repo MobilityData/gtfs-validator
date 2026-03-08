e457ddc84e4c66787d9c0bb41ee9821638da0b8a
Charlotte Wilson <251223230+cswilson252@users.noreply.github.com>
2026-03-08 19:54:23 -0400

    assertThat?

--- main/src/test/java/org/mobilitydata/gtfsvalidator/validator/MissingShapesFileValidatorTest.java
+++ main/src/test/java/org/mobilitydata/gtfsvalidator/validator/MissingShapesFileValidatorTest.java
@@ -64,7 +64,7 @@ public class MissingShapesFileValidatorTest {
             .anyMatch(
                 notice ->
                     notice instanceof MissingShapesFileValidator.MissingRecommendedFileNotice);
-    assertTrue(found);
+    assertThat(found);
   }
 
   @Test
@@ -79,7 +79,7 @@ public class MissingShapesFileValidatorTest {
             .anyMatch(
                 notice ->
                     notice instanceof MissingShapesFileValidator.MissingRecommendedFileNotice);
-    assertTrue(found);
+    assertThat(found);
   }
 
   @Test
@@ -94,7 +94,7 @@ public class MissingShapesFileValidatorTest {
             .anyMatch(
                 notice ->
                     notice instanceof MissingShapesFileValidator.MissingRecommendedFileNotice);
-    assertTrue(found);
+    assertThat(found);
   }
 
   private static List<ValidationNotice> generateNotices(
