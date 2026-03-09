af13b31ee4ef0cfa0332279ef65ef543ac036786
jcpitre <106176106+jcpitre@users.noreply.github.com>
2026-03-09 10:58:30 -0400

    Added ServiceExtendsFarInTheFutureNotice (#2127)

--- main/src/main/java/org/mobilitydata/gtfsvalidator/validator/ServiceSpreadValidator.java
+++ main/src/main/java/org/mobilitydata/gtfsvalidator/validator/ServiceSpreadValidator.java
@@ -23,6 +23,7 @@ import javax.inject.Inject;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidationNotice.FileRefs;
 import org.mobilitydata.gtfsvalidator.annotation.GtfsValidator;
+import org.mobilitydata.gtfsvalidator.input.DateForValidation;
 import org.mobilitydata.gtfsvalidator.notice.NoticeContainer;
 import org.mobilitydata.gtfsvalidator.notice.ValidationNotice;
 import org.mobilitydata.gtfsvalidator.table.GtfsCalendarDateSchema;
@@ -34,29 +35,35 @@ import org.mobilitydata.gtfsvalidator.util.ServiceInterval;
 import org.mobilitydata.gtfsvalidator.util.ServiceIntervalCache;
 
 /**
- * Validates that no service has a gap of more than 13 days between active service dates.
+ * Validates data related to the calendar spread of a service. Checks that no service has a gap of
+ * more than 13 days between active service dates. Also checks the end date of a service is too far
+ * in the future.
  *
  * <p>A gap is defined as a period of inactivity between two consecutive active intervals within the
  * overall service range. Dates before the first active day or after the last are not considered
  * gaps.
  *
- * <p>Generated notice: {@link BigGapInServiceNotice}.
+ * <p>Generated notices: {@link BigGapInServiceNotice}, {@link ServiceExtendsFarInTheFutureNotice}
  */
 @GtfsValidator
-public class ServiceGapValidator extends FileValidator {
+public class ServiceSpreadValidator extends FileValidator {
 
   private static final int MAX_GAP_DAYS = 13;
+  private static final int MAX_FUTURE_EXTENT_DAYS = 2 * 365;
 
   private final ServiceIntervalCache serviceIntervalCache;
+  private final DateForValidation dateForValidation;
   private final GtfsCalendarTableContainer calendarTableContainer;
   private final GtfsCalendarDateTableContainer calendarDateTableContainer;
 
   @Inject
-  ServiceGapValidator(
+  ServiceSpreadValidator(
       ServiceIntervalCache serviceIntervalCache,
+      DateForValidation dateForValidation,
       GtfsCalendarTableContainer calendarTableContainer,
       GtfsCalendarDateTableContainer calendarDateTableContainer) {
     this.serviceIntervalCache = serviceIntervalCache;
+    this.dateForValidation = dateForValidation;
     this.calendarTableContainer = calendarTableContainer;
     this.calendarDateTableContainer = calendarDateTableContainer;
   }
@@ -83,6 +90,13 @@ public class ServiceGapValidator extends FileValidator {
                           gap.end().plusDays(1), // First active day after the gap
                           gap.lengthInDays()));
                 }
+              }
+              LocalDate lastActiveDate = intervals.lastActiveDate();
+              LocalDate now = dateForValidation.getDate();
+              int diff = (int) (lastActiveDate.toEpochDay() - now.toEpochDay());
+              if (diff > MAX_FUTURE_EXTENT_DAYS) {
+                noticeContainer.addValidationNotice(
+                    new ServiceExtendsFarInTheFutureNotice(serviceId, lastActiveDate));
               }
             });
   }
@@ -111,6 +125,24 @@ public class ServiceGapValidator extends FileValidator {
       this.gapStartDate = gapStartDate.toString();
       this.gapEndDate = gapEndDate.toString();
       this.gapDurationDays = gapDurationDays;
+    }
+  }
+
+  /** A service end date is more than 2 years in the future. */
+  @GtfsValidationNotice(
+      severity = INFO,
+      files = @FileRefs({GtfsCalendarSchema.class, GtfsCalendarDateSchema.class}))
+  static class ServiceExtendsFarInTheFutureNotice extends ValidationNotice {
+
+    /** The service_id that ends far in the future. */
+    private final String serviceId;
+
+    /** The end date of the service (YYYY-MM-DD format). */
+    private final String serviceWindowEndDate;
+
+    ServiceExtendsFarInTheFutureNotice(String serviceId, LocalDate serviceWindowEndDate) {
+      this.serviceId = serviceId;
+      this.serviceWindowEndDate = serviceWindowEndDate.toString();
     }
   }
 }
