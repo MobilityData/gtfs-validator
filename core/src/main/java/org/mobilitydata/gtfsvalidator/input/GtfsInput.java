9822b6665a19a4073f7a1374a8bfcfe76b425380
Charlotte Wilson <251223230+cswilson252@users.noreply.github.com>
2026-03-09 17:03:52 -0400

    fix return type

--- core/src/main/java/org/mobilitydata/gtfsvalidator/input/GtfsInput.java
+++ core/src/main/java/org/mobilitydata/gtfsvalidator/input/GtfsInput.java
@@ -179,7 +179,7 @@ public abstract class GtfsInput implements Closeable {
       }
 
       return new GtfsZipFileInput(
-          new ZipFile(new SeekableInMemoryByteChannel(outputStream.toByteArray())), fileName);
+          new File(new SeekableInMemoryByteChannel(outputStream.toByteArray())), fileName);
     }
   }
 
